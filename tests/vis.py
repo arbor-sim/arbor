fix area units in unit tests for mc models

tests/vis.py
@@ -5,8 +5,8 @@ data = np.loadtxt('v.dat')
 t = data[:,0]
 v = data[:,1]
 
-
 pyplot.plot(t, v)
+pyplot.ylim(-80, 50)
 pyplot.xlabel('time (ms)')
 pyplot.ylabel('mV')
 pyplot.show()