a3c35136d7c17ab8dd8c92be1dfa62a0629bda83
Sam Yates <halfflat@gmail.com>
2016-06-29 12:55:50 +0200

    Fix ytpo in coment

--- src/util/debug.cpp
+++ src/util/debug.cpp
@@ -7,7 +7,7 @@ bool nest::mc::util::failed_assertion(const char *assertion, const char *file,
                                       int line, const char *func)
 {
     // Explicit flush, as we can't assume default buffering semantics on stderr/cerr,
-    // and abort() mignt not flush streams.
+    // and abort() might not flush streams.
 
     std::cerr << file << ':' << line << " " << func
               << ": Assertion `" << assertion << "' failed." << std::endl;
