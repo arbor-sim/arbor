fac70f4a5db768a62040e9a6ae80336cd46eb3e4
Ben Cumming <bcumming@cscs.ch>
2016-08-05 15:56:10 +0200

    turn on generation of optimized mech kernels

--- mechanisms/generate.sh
+++
@@ -1,8 +0,0 @@
-#flags="-t cpu -O"
-flags="-t cpu"
-
-for mech in pas hh expsyn
-do
-    echo ../external/modparser/bin/modcc ${flags} -o ../include/mechanisms/$mech.hpp ./mod/$mech.mod
-    ../external/modparser/bin/modcc ${flags} -o ../include/mechanisms/$mech.hpp ./mod/$mech.mod
-done
