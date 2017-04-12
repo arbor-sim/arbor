e2f48e198995a8557547aa869e668723b41d2572
Sam Yates <yates@cscs.ch>
2017-04-12 18:06:49 +0200

    Move string-printf utility from lmorpho into util. (#228)
    Move string-printf utility from lmorpho into util.
    
    * Add `util::strprintf` printf-alike function that returns a `std::string` result.
    * Include simple adaptors for `std::string` and standard smart pointer arguments fot `util::strprintf` (i.e. `std::string` arguments can be used with `%s`, smart pointers with `%p`).
    * Add unit tests to suit.

---
+++ tests/unit/test_strprintf.cpp
@@ -0,0 +1,63 @@
+#include <cstdio>
+#include <memory>
+#include <string>
+
+#include "../gtest.h"
+#include <util/strprintf.hpp>
+
+using namespace nest::mc::util;
+
+
+TEST(strprintf, simple) {
+    char buf[200];
+
+    const char* fmt1 = " %% %04d % 3.2f %#016x %c";
+    sprintf(buf, fmt1, 3, 7.1e-3, 0x1234ul, 'x');
+    auto result = strprintf(fmt1, 3, 7.1e-3, 0x1234ul, 'x');
+    EXPECT_EQ(std::string(buf), result);
+
+    const char* fmt2 = "%5s %3s";
+    sprintf(buf, fmt2, "bear", "pear");
+    result = strprintf(fmt2, "bear", "pear");
+    EXPECT_EQ(std::string(buf), result);
+}
+
+TEST(strprintf, longstr) {
+    std::string aaaa(2000, 'a');
+    std::string bbbb(2000, 'b');
+    std::string x;
+
+    x = aaaa;
+    ASSERT_EQ(x, strprintf("%s", x.c_str()));
+
+    x += bbbb+x;
+    ASSERT_EQ(x, strprintf("%s", x.c_str()));
+
+    x += bbbb+x;
+    ASSERT_EQ(x, strprintf("%s", x.c_str()));
+
+    x += bbbb+x;
+    ASSERT_EQ(x, strprintf("%s", x.c_str()));
+
+    x += bbbb+x;
+    ASSERT_EQ(x, strprintf("%s", x.c_str()));
+}
+
+TEST(strprintf, wrappers) {
+    // can we print strings and smart-pointers directly?
+
+    char buf[200];
+
+    auto uptr = std::unique_ptr<int>{new int(17)};
+    sprintf(buf, "uptr %p", uptr.get());
+
+    EXPECT_EQ(std::string(buf), strprintf("uptr %p", uptr));
+
+    auto sptr = std::shared_ptr<double>{new double(19.)};
+    sprintf(buf, "sptr %p", sptr.get());
+
+    EXPECT_EQ(std::string(buf), strprintf("sptr %p", sptr));
+
+    EXPECT_EQ(std::string("fish"), strprintf("fi%s", std::string("sh")));
+}
+
