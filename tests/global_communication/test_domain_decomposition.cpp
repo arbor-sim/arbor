c2633503b9434f8faee681b0bfca7eedcb8fdcc3
Ben Cumming <louncharf@gmail.com>
2017-05-08 12:37:21 +0200

    Implement `util::any`
    Partial implementation of `std::any` from C++17 standard. See: http://en.cppreference.com/w/cpp/utility/any
    
    The implementation is in the `util` library as `util::any`.
    
    Deviations from the standards description of `std::any`:
      * Does not avoid dynamic allocation of small objects.
      * Does not implement the `in_place_type<T>` constructors from the standard.
      * Does not implement the `emplace` modifier from the standard.

--- tests/global_communication/test_domain_decomposition.cpp
+++ tests/global_communication/test_domain_decomposition.cpp
@@ -13,15 +13,15 @@ using namespace nest::mc;
 
 using communicator_type = communication::communicator<communication::global_policy>;
 
-static bool is_dry_run() {
+inline bool is_dry_run() {
     return communication::global_policy::kind() ==
         communication::global_policy_kind::dryrun;
 }
 
 TEST(domain_decomp, basic) {
+/*
     using policy = communication::global_policy;
 
-/*
     const auto num_domains = policy::size();
     const auto rank = policy::id();
 */
