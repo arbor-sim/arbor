42ea5964b11c6efa81e66f2ca174802912282e1b
klijn1 <nonoice@gmail.com>
2017-05-18 18:00:39 +0200

    Small textual issues fixed after self review

--- tests/regression/scripts/compare.py
+++ tests/regression/scripts/compare.py
@@ -1,8 +1,8 @@
+#!/usr/bin/env python
 from __future__ import print_function
 import os
 import sys
 import collections
-
 
 def usage(default_delta):
     print ("""
@@ -14,7 +14,6 @@ def usage(default_delta):
 
     """.format(default_delta))
 
-
 def parse_file(path):
     """
     Parse a spikes file: fail hard! If parsing does not work,
@@ -22,12 +21,9 @@ def parse_file(path):
 
     returns dict keyed on gid.
         each value is a list of (spiketimes,  the line number, line )
-
     """
-
     fp = open(path, "r")
     parsed_data = collections.defaultdict(list)
-
 
     line_idx = 0
     for line in fp.readlines():
@@ -40,11 +36,10 @@ def parse_file(path):
             print ("Could not parse a line in the file!!!! \n")
             print (" line: " , line_idx, ": ", stripped_line)
             print (path)
-
+            
             exit(1) #failure
 
         line_data = (time, line_idx, stripped_line)
-
         parsed_data[gid].append(line_data)
 
     return parsed_data
@@ -57,16 +52,14 @@ def compare(path1, data1, path2, data2, delta):
     if whole spikes are missing just exit!
 
     print all errors and then exit(1)
-
     """
-
     # First check all gids are equal
     if (data1.keys() != data2.keys()):
         print ("The encountered gids are NOT equal!")
         if (len(data1.keys()) == 0):
             print ("No spikes in",  path1)
-
             exit(1)
+            
         if (len(data2.keys()) == 0):
             print ("No spikes in",  path2)
             exit(1)
@@ -75,12 +68,11 @@ def compare(path1, data1, path2, data2, delta):
         if ( len(data1.keys()) > len(data2.keys())):
             print ("Extra gid encountered in:", path1)
             print (set(data1.keys()) - set(data2.keys()) )
-
             exit(1)
+            
         elif ( len(data2.keys()) > len(data1.keys())):
             print ("Extra gid encountered in:", path2)
             print (set(data2.keys()) - set(data1.keys()) )
-
             exit(1)
 
         print ("first difference in GIDs in the two files: ")
@@ -92,7 +84,6 @@ def compare(path1, data1, path2, data2, delta):
         # should not happen but still
         print ("encountered unknown difference in files!")
         exit(1)
-
 
     different_spikes = []
     for gid in data1.keys():
@@ -108,28 +99,22 @@ def compare(path1, data1, path2, data2, delta):
                     # IF there are ALSO spike time differences these are NOT reported
                     exit(1)
 
-
         for  time1, time2 in zip( gid_list1, gid_list2):
             if abs(time1[0] - time2[0]) > delta:
                 different_spikes.append((time1, time2))
 
-
     if len(different_spikes) != 0:
         print ("Found difference in spike times, displaying first 10:")
-
         for idx, (time1, time2) in enumerate(different_spikes):
             if idx == 10:
                 break
 
             print ("Spike #", idx, ": ", time1, " != ", time2)
-
         exit(1)
 
     # we compared the file, no errors found.
     # exit with 0 code!
     exit(0)
-
-
 
 if __name__ == "__main__":
 
@@ -149,4 +134,3 @@ if __name__ == "__main__":
         delta = default_delta
 
     compare(path1, data, path2, data2, delta )
-
