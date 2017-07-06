3fc41826f88ef307fe525c772d458f161286f36a
klijn1 <nonoice@gmail.com>
2017-07-06 14:54:47 +0200

    Use itterator ans std:: find_if based spike emit

--- src/dss_cell.hpp
+++ src/dss_cell.hpp
@@ -18,13 +18,10 @@ public:
         std::vector<time_type> spike_times;
 
         /// The description needs a vector of doubles for the description
-
         dss_cell_description(std::vector<time_type> &  spike_times):
             spike_times(spike_times)
         {}
     };
-
-    /// Construct a dss cell from its description
 
     dss_cell(dss_cell_description descr){
         spike_times_.reserve(descr.spike_times.size());
@@ -34,18 +31,20 @@ public:
         // are doing is correct and if this sort is a bottle neck we might have
         // other problems.
         std::sort(spike_times_.begin(), spike_times_.end());
+
+
+        not_emit_it = spike_times_.begin();
     }
 
     /// Return the kind of cell, used for grouping into cell_groups
-
     cell_kind  get_cell_kind() const  {
         return cell_kind::data_spike_source;
     }
 
     /// Get  all spikes until tfinal.
-
     // updates the internal time state to tfinal as a side effect
     std::vector<time_type> spikes_until(time_type tfinal) {
+        /*
         std::vector<time_type> spikes_this_period;
 
         // Collect spikes in this period
@@ -63,11 +62,19 @@ public:
         not_emit_idx = spike_idx;
 
         return spikes_this_period;
+        */
+        auto first = not_emit_it;
+        not_emit_it = std::find_if(
+            first, spike_times_.end(),
+            [tfinal](time_type t) {return t >= tfinal; }
+        );
+
+        return { first, not_emit_it };
     }
 
-    /// reset internal time to 0.0
+    /// reset internal point to non emitted spike to the first in the storage
     void reset() {
-        not_emit_idx = 0;
+        not_emit_it = spike_times_.begin();
     }
 
 private:
@@ -75,7 +82,7 @@ private:
     std::vector<time_type> spike_times_;
 
     // Index to the first spike not yet emitted
-    unsigned not_emit_idx = 0;
+    std::vector<time_type>::iterator not_emit_it;
 };
 
 } // namespace mc
