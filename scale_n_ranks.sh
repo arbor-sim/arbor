47b400d1ae2f50591e765e9bf86aa07be4fed2cd
Marko Kabic <kabicm@Markos-MacBook-Pro.local>
2017-07-13 12:52:19 +0200

    scaling experiment modified

--- scale_n_ranks.sh
+++ scale_n_ranks.sh
@@ -1,16 +1,16 @@
-# PARAMETERS OF THE SIMULATION
-n_exc=100         # exc population size
-n_inh=$((n_exc/4))       # inh popoulation size
+# Parameters of the simulation.
+n_exc=100           # exc population size
+n_inh=$((n_exc/4))  # inh popoulation size
 prop=0.05           # prop of connections from each population
 weight=1.2          # exc connections weight
 rel_inh_strength=1  # relative strength of inhibitory connections
 delay=0.1           # delay of all connections
 rate=1              # rate of Poisson neruons
-time=10           # simulation time
+time=10             # simulation time
 dt=1                # timestep (ignored)
-group_size=10       # size of cell groups
+group_size=50       # size of cell groups
 
-# MULTICORE PARAMETERS
+# Multicore parameters.
 n_ranks=(1 2 4 9 18 36)
 n_cores=1
 nmc_num_threads=2
@@ -19,29 +19,44 @@ nmc_num_threads=2
 run() {
     n_rank=$1
     n_core=$2
-    srun -n $n_rank -c $n_core ./build/miniapp/brunel/brunel_miniapp.exe -n $n_exc -m $n_inh -p $prop -w $weight -d $delay -g $rel_inh_strength -r $rate -t $time -s $dt -G $group_size
+    srun -n $n_rank -c $n_core --hint=nomultithread ./build/miniapp/brunel/brunel_miniapp.exe -n $n_exc -m $n_inh -p $prop -w $weight -d $delay -g $rel_inh_strength -r $rate -t $time -s $dt -G $group_size
 }
 
 run_locally() {
-    NMC_NUM_THREADS=2 ./build/miniapp/brunel/brunel_miniapp.exe -n $n_exc -m $n_inh -p $prop -w $weight -d $delay -g $rel_inh_strength -r $rate -t $time -s $dt -G $group_size
+    NMC_NUM_THREADS=$nmc_num_threads ./build/miniapp/brunel/brunel_miniapp.exe -n $n_exc -m $n_inh -p $prop -w $weight -d $delay -g $rel_inh_strength -r $rate -t $time -s $dt -G $group_size -f
 }
 
 # Preserve the newline characters by setting this empty (field splitting).
 IFS=
 
-rm scaling_ranks.txt
+cd build && make -j && cd ..
 
-for n_rank in ${n_ranks[@]}
+vary_n_exc=(100 1000 10000)
+
+for n_exc in ${vary_n_exc[@]}
 do
-    # Take the output of the simulation.
-    output=$(run n_rank n_cores)
-    #output=$(run_locally)
+    echo "Setting n_exc = "$n_exc"..."
 
-    # Find the duration of the simulation from stdout.
-    duration=$(echo $output | awk '/model-simulate/ {print $2}')
+    file="scaling_ranks_"$n_exc".txt"
 
-    echo $n_rank" "$duration
+    [ -e $file_name ] && rm $file
+    #[-e profile* ] rm profile*
 
-    # Output n_rank and the duration to a file.
-    echo $n_rank" "$duration >> "scaling_ranks.txt"
+    for n_rank in ${n_ranks[@]}
+    do
+        echo "  setting n_rank = "$n_rank"..."
+        # Take the output of the simulation.
+        #output=$(run $n_rank $n_cores)
+        output=$(run_locally)
+        #echo "  "$output
+        # Find the duration of the simulation from stdout.
+        setup=$(echo $output | awk '/setup/ {print $2}')
+        model_init=$(echo $output | awk '/model-init/ {print $2}')
+        model_simulate=$(echo $output | awk '/model-simulate/ {print $2}')
+
+        echo "  "$n_rank" "$setup" "$model_init" "$model_simulate
+
+        # Output n_rank and the duration to a file.
+        echo $n_rank" "$setup" "$model_init" "$model_simulate >> $file
+    done
 done
