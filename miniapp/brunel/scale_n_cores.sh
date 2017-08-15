0fd626ad5ceead36717f70c844fae26c5ca2e59f
Marko Kabic <kabicm@Markos-MacBook-Pro.local>
2017-08-15 12:06:11 +0200

    Benchmarking optimised Brunel:

--- miniapp/brunel/scale_n_cores.sh
+++ miniapp/brunel/scale_n_cores.sh
@@ -2,14 +2,14 @@
 n_exc=100           # exc population size
 n_inh=$((n_exc/4))  # inh popoulation size
 n_ext=100           # poisson population size
-prop=1            # prop of connections from each population
+prop=0.5            # prop of connections from each population
 weight=1            # exc connections weight
-rel_inh_strength=1  # relative strength of inhibitory connections
-delay=0.1           # delay of all connections
+rel_inh_strength=0.5# relative strength of inhibitory connections
+delay=1             # delay of all connections
 rate=1              # rate of Poisson neruons
 time=100            # simulation time
 dt=1                # timestep (ignored)
-group_size=10       # size of cell groups
+group_size=50       # size of cell groups
 
 # Multicore parameters.
 n_ranks=1
@@ -19,13 +19,23 @@ n_cores=(1 2 4 9 18 36)
 run() {
     n_rank=$1
     n_core=$2
+    optimised=$3
 
     # Use multithreading for 36 cores and otherwise no.
     if [ $n_core -eq 36 ]
     then
-        NMC_NUM_THREADS=$n_core srun -n $n_rank -c $n_core ../../build/miniapp/brunel/brunel_miniapp.exe -n $n_exc -m $n_inh -e $n_ext -p $prop -w $weight -d $delay -g $rel_inh_strength -r $rate -t $time -s $dt -G $group_size
+        if [ $optimised ]
+        then
+            NMC_NUM_THREADS=$n_core srun -n $n_rank -c $n_core ../../build/miniapp/brunel/brunel_miniapp.exe -n $n_exc -m $n_inh -e $n_ext -p $prop -w $weight -d $delay -g $rel_inh_strength -r $rate -t $time -s $dt -G $group_size -o
+        else
+            NMC_NUM_THREADS=$n_core srun -n $n_rank -c $n_core ../../build/miniapp/brunel/brunel_miniapp.exe -n $n_exc -m $n_inh -e $n_ext -p $prop -w $weight -d $delay -g $rel_inh_strength -r $rate -t $time -s $dt -G $group_size
+        fi
     else
-        NMC_NUM_THREADS=$n_core srun -n $n_rank -c $n_core --hint=nomultithread ../../build/miniapp/brunel/brunel_miniapp.exe -n $n_exc -m $n_inh -e $n_ext -p $prop -w $weight -d $delay -g $rel_inh_strength -r $rate -t $time -s $dt -G $group_size
+        if [ $optimised ]
+            NMC_NUM_THREADS=$n_core srun -n $n_rank -c $n_core --hint=nomultithread ../../build/miniapp/brunel/brunel_miniapp.exe -n $n_exc -m $n_inh -e $n_ext -p $prop -w $weight -d $delay -g $rel_inh_strength -r $rate -t $time -s $dt -G $group_size -o
+        else
+            NMC_NUM_THREADS=$n_core srun -n $n_rank -c $n_core --hint=nomultithread ../../build/miniapp/brunel/brunel_miniapp.exe -n $n_exc -m $n_inh -e $n_ext -p $prop -w $weight -d $delay -g $rel_inh_strength -r $rate -t $time -s $dt -G $group_size
+        fi
     fi
 }
 
@@ -36,9 +46,12 @@ run_locally() {
 # Preserve the newline characters by setting this empty (field splitting).
 IFS=
 
-cd ../../build && make -j && cd ../miniapp/brunel
+cd $SCRATCH/nestmc-proto/build
+make -j
+cd ../miniapp/brunel
 
 vary_n_exc=(100 1000 10000)
+optimised=false
 
 for n_exc in ${vary_n_exc[@]}
 do
@@ -46,18 +59,16 @@ do
 
     file="scaling_cores_"$n_exc".txt"
 
-    #[ -e $file_name ] && rm $file
-    #[-e profile* ] rm profile*
+    rm scaling_cores_*
 
     n_inh=$((n_exc/4))
     n_ext=$((n_exc))
-    prop=$(echo "$prop/10.0" | bc -l)
 
     for n_core in ${n_cores[@]}
     do
         echo "  setting n_core = "$n_core"..."
         # Take the output of the simulation.
-        output=$(run $n_ranks $n_core)
+        output=$(run $n_ranks $n_core $optimised)
         #output=$(run_locally)
         #echo "  "$output
         # Find the duration of the simulation from stdout.
@@ -70,6 +81,8 @@ do
         # Output n_core and the duration to a file.
         echo $n_core" "$setup" "$model_init" "$model_simulate >> $file
     done
+
+    prop=$(echo "$prop/10.0" | bc -l)
 done
 
 
