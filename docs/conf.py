ea2dd568e58e6479cd1dcb21755ad1614cfdb766
bcumming <bcumming@cscs.ch>
2017-08-18 13:56:56 +0200

    remove warnings from sphinx build

--- docs/conf.py
+++ docs/conf.py
@@ -71,7 +71,7 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['build', 'Thumbs.db', '.DS_Store', 'rtd_theme']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
@@ -84,14 +84,14 @@ todo_include_todos = True
 
 # Use the ReadTheDocs theme
 html_theme = "sphinx_rtd_theme"
-html_theme_path = ["sphinx_rtd_theme", ]
+html_theme_path = ["rtd_theme", ]
 
 # html_theme_options = {}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['static']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
@@ -164,6 +164,3 @@ texinfo_documents = [
      author, 'Arbor', 'One line description of project.',
      'Miscellaneous'),
 ]
-
-
-
