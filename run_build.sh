5e230f105d02b6be53c86029b45dde179e7c7150
bcumming <bcumming@cscs.ch>
2017-08-29 17:59:49 +0200

    wip

--- run_build.sh
+++ run_build.sh
@@ -1,8 +1,16 @@
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+CLEAR='\033[0m'
+
+error() {>&2 echo -e "${RED}ERROR${CLEAR}: $1"}
+progress() { echo; echo -e "${BLUE}STATUS${CLEAR}: $1"; echo}
+
 export CC=`which gcc-6`
 export CXX=`which g++-6`
 
-$(CC) --version
-$(CXX) --version
+${CC} --version
+${CXX} --version
 cmake --version
 
 # make build path
@@ -10,25 +18,25 @@ mkdir -p build
 cd build
 
 # run cmake
-echo "configuring with cmake"
+progress "configuring with cmake"
 cmake ..
 if [ $? -ne 0 ]; then
-    echo "Error: unable to configure with cmake"
+    error "unable to configure with cmake"
     exit 2;
 fi
 
 # make the tests
-echo "running make"
+progress "running make"
 make test.exe -j8
 if [ $? -ne 0 ]; then
-    echo "Error: unable to build unit tests"
+    error "unable to build unit tests"
     exit 3;
 fi
 
 # run tests
-echo "running tests"
+progress "running tests"
 ./tests/test.exe
 if [ $? -ne 0 ]; then
-    echo "Error: some unit tests did not pass"
+    error "some unit tests did not pass"
     exit 4;
 fi
