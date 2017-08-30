c18aef497ab11adbea60fe8e6e13583af8e4d3e1
bcumming <bcumming@cscs.ch>
2017-08-30 12:40:29 +0200

    wip

--- scripts/travis/run_build.sh
+++ scripts/travis/run_build.sh
@@ -30,27 +30,29 @@ cd $build_path
 # run cmake
 progress "configuring with cmake"
 
-cmake_flags="-DNMC_THREADING_MODEL=${WITH_THREAD}"
+cmake_flags="-DNMC_WITH_ASSERTIONS=on"
+cmake_flags="$cmake_flags -DNMC_THREADING_MODEL=${WITH_THREAD}"
 cmake .. ${cmake_flags}
 if [ $? -ne 0 ]; then
     error "unable to configure with cmake ${cmake_flags}"
     exit 2;
 fi
 
-# make the tests
-progress "running make"
-make test.exe -j4
-if [ $? -ne 0 ]; then
-    error "unable to build unit tests"
-    exit 3;
-fi
+for test_case in "test.exe" "global_communication.exe"
+do
+    progress "making $test_case"
+    NUM_NUM_THREADS=2 make ${test_case} -j4 > /dev/null
+    if [ $? -ne 0 ]; then
+        error "unable to build ${test_case}"
+        exit 3;
+    fi
 
-# run tests
-progress "running tests"
-./tests/test.exe
-if [ $? -ne 0 ]; then
-    error "some unit tests did not pass"
-    exit 4;
-fi
+    progress "running $test_case"
+    NUM_NUM_THREADS=2 ./tests/${test_case}
+    if [ $? -ne 0 ]; then
+        error "some tests did not pass"
+        exit 4;
+    fi
+done
 
 cd $base_path
