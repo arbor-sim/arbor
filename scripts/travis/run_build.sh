058890af44c307cc988b61fe0f5cfe4d597d1d80
bcumming <bcumming@cscs.ch>
2017-09-01 13:54:11 +0200

    wip

--- scripts/travis/run_build.sh
+++ scripts/travis/run_build.sh
@@ -1,10 +1,8 @@
 RED='\033[0;31m'
-GREEN='\033[0;32m'
-BLUE='\033[0;34m'
 YELLOW='\033[0;33m'
 CLEAR='\033[0m'
 
-error() {>&2 echo -e "${RED}ERROR${CLEAR}: $1"; exit 1;}
+error()    {>&2 echo -e "${RED}ERROR${CLEAR}: $1"; exit 1;}
 progress() { echo; echo -e "${YELLOW}STATUS${CLEAR}: $1"; echo;}
 
 base_path=`pwd`
@@ -13,24 +11,26 @@ build_path=build-${BUILD_NAME}
 #
 # print build-specific and useful information
 #
-progress "build environment information"
+progress "build environment"
 
 compiler_version=`${CXX} -dumpversion`
 cmake_version=`cmake --version | grep version | awk '{print $3}'`
 
-echo "compiler   : ${compiler_version}"
+echo "compiler   : ${CXX} ${compiler_version}"
 echo "cmake      : ${cmake_version}"
 echo "build path : ${build_path}"
 echo "base path  : ${base_path}"
 
 if [[ "${WITH_DISTRIBUTED}" = "mpi" ]]; then
-    echo "mpi        : enabled"
+    echo "mpi        : on"
     export OMPI_CC=${CC}
     export OMPI_CXX=${CXX}
-    #CXX_FLAGS="-DCMAKE_CXX_FLAGS=-cxx=${CXX}"
     CC="mpicc"
     CXX="mpicxx"
     launch="mpiexec -n 4"
+else
+    echo "mpi        : off"
+    launch=""
 fi
 
 #
@@ -51,15 +51,15 @@ cmake .. ${cmake_flags} || error "unable to configure cmake"
 export NMC_NUM_THREADS=2
 
 progress "Unit tests"
-make test.exe -j4  || error "errors building unit tests"
-./tests/test.exe || error "errors running unit tests"
+make test.exe -j4  || error "building unit tests"
+./tests/test.exe --gtest_color=no || error "running unit tests"
 
 progress "Global communication tests"
-make global_communication.exe -j4          || error "errors building global communication tests"
-${launch} ./tests/global_communication.exe || error "errors running global communication tests"
+make global_communication.exe -j4          || error "building global communication tests"
+${launch} ./tests/global_communication.exe || error "running global communication tests"
 
 progress "Miniapp spike comparison test"
-make miniapp.exe -j4                        || error "errors building miniapp"
-${launch} ./miniapp/miniapp.exe -n 10 -t 10 || error "errors running miniapp"
+make miniapp.exe -j4                         || error "building miniapp"
+${launch} ./miniapp/miniapp.exe -n 20 -t 100 || error "running miniapp"
 
 cd $base_path
