d3dc6dc8a02856eb68405d96eee7ec99a7de53df
Yachao Shao <y.shao@fz-juelich.de>
2017-10-23 15:04:45 +0200

    This is a simple test file of Arbor.

--- python/cynestmc/arbor_test.py
+++ python/cynestmc/arbor_test.py
@@ -1,6 +1,6 @@
 #!/bin/bash/python3
 
-#import py_arbor
+import py_arbor
 
 import sys
 import getopt
@@ -23,9 +23,31 @@ def main(argv):
                 input_para = json.load(parameter)
                 num_cell = input_para["cells"]
                 print(num_cell)
-
-
-
+                # py_arbor(input_para)
+                recipe = py_arbor.py_recipe(input_para)
+                decomp = py_arbor.domain_decomposition(recipe, rules)
+                model = py_arbor.py_model(recipe, decomp)
+                dt = input_para["dt"]
+                tfinal = input_para["tfinal"]
+                model.run(dt,tfinal)
+                print("Number spikes: ", model.num_spikes())
+        else:
+            print("No input json file, use default settings instead.")
+            default_input ={
+                "cells": 1000,
+                "synapses": 500,
+                "compartments": 50,
+                "dt": 0.02,
+                "tfinal": 100.0,
+                "all_to_all": False
+            }
+            recipe = py_arbor.py_recipe(default_input)
+            decomp = py_arbor.domain_decomposition(recipe, rules)
+            model = py_arbor.py_model(recipe, decomp)
+            dt = default_input["dt"]
+            tfinal = default_input["tfinal"]
+            model.run(dt, tfinal)
+            print("Number spikes: ", model.num_spikes())
 
 if __name__ == '__main__':
     main(sys.argv[1:])
