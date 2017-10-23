39254e57ffd7e4517336b2dc4156426967b4c514
Alexander Peyser <a.peyser@fz-juelich.de>
2017-10-23 15:24:46 +0200

    Add yachao cynestmc

---
+++ nestmc-proto/python/cynestmc/py_arbor.pyx
@@ -0,0 +1,28 @@
+cdef class py_recipe:
+    cdef recipe* c_recipe
+    def __cinit__(self, ):
+        self.c_recipe = new recipe()
+    def __dealloc__(self):
+        del self.c_recipe
+
+cdef class py_model:
+    cdef model* c_model
+
+    def __init__(self, py_recipe recipe, domain_decomposition decomp):
+        self.c_model = new module(recipe, decomp)
+
+    def __dealloc__(self):
+        del self.c_model
+
+    def reset(self):
+      self.c_model.reset()
+
+    def run(self, float tfinal, float dt):
+      return self.c_model.run(tfinal, dt)
+
+    def num_spikes(self):
+      return self.c_model.num_spikes()
+    def num_groups(self):
+      return self.c_model.num_groups()
+    def num_cells(self):
+      return self.c_model.num_cells()
