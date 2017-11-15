a3e115888ebcf7c16f74fb358ca2ecb4ba285643
Sam Yates <halfflat@gmail.com>
2017-11-15 20:09:33 +0100

    Add custom streambuf for indented output.
    These classes aim to provide an alternative to `TextBuffer` in `modcc` printers.
    
    * Implement a streambuf wrapper `prefixbuf` that prepends a prefix string to each line.
    * Adds stream manipulators `setprefix`, `indent`, `popindent`, `settab` for manipulating the prefix in a `prefixbuf`.
    * Provide a wrapper stream `pfxstringstream` around `std::stringbuf` with `std::ostringstream` semantics.

---
+++ src/util/prefixbuf.hpp
@@ -0,0 +1,109 @@
+#pragma once
+
+// Output-only stream buffer that prepends a prefix to each line of output,
+// together with stream manipulators for setting the prefix and managing
+// an indentation level.
+
+#include <ostream>
+#include <sstream>
+#include <string>
+
+namespace arb {
+namespace util {
+
+class prefixbuf: public std::streambuf {
+public:
+    explicit prefixbuf(std::streambuf* inner): inner_(inner) {}
+
+    prefixbuf(prefixbuf&&) = default;
+    prefixbuf(const prefixbuf&) = delete;
+
+    prefixbuf& operator=(prefixbuf&&) = default;
+    prefixbuf& operator=(const prefixbuf&) = delete;
+
+    std::streambuf* inner() { return inner_; }
+    std::string prefix;
+
+protected:
+    std::streambuf* inner_;
+    bool bol_ = true;
+
+    std::streamsize xsputn(const char_type* s, std::streamsize count) override;
+    int_type overflow(int_type ch) override;
+};
+
+// Manipulators:
+//
+// setprefix(s):   explicitly set prefix string on corresponding prefixbuf.
+// indent:         increase indentation level by one tab width.
+// indent(n):      increase indentation level by n tab widths.
+// popindent:      undo last `indent` operation.
+// popindent(n)    undo last n `indent` operations.
+// settab(w):      set tab width to w (default is 4).
+
+class setprefix {
+public:
+    explicit setprefix(std::string prefix): prefix_(std::move(prefix)) {}
+
+    friend std::ostream& operator<<(std::ostream& os, const setprefix& sp);
+
+private:
+    std::string prefix_;
+};
+
+struct indent_manip {
+    enum action_enum {push, pop, settab};
+
+    explicit constexpr indent_manip(action_enum action, unsigned value=0):
+        action_(action), value_(value)
+    {}
+
+    // convenience interface: allows using both `indent` and `indent(n)`
+    // as stream manipulators.
+    indent_manip operator()(unsigned n) const {
+        return indent_manip(action_, n);
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, indent_manip in);
+
+private:
+    static constexpr unsigned default_tabwidth = 4;
+    static int xindex();
+
+    action_enum action_;
+    unsigned value_;
+};
+
+constexpr indent_manip indent{indent_manip::push, 1u};
+constexpr indent_manip popindent{indent_manip::pop, 1u};
+
+inline indent_manip settab(unsigned w) {
+    return indent_manip{indent_manip::settab, w};
+}
+
+// Wrap an ostringstream with a prefixbuf.
+
+class pfxstringstream: public std::ostream {
+public:
+    pfxstringstream():
+        std::ostream(&pbuf_),
+        sbuf_(std::ios_base::out),
+        pbuf_(&sbuf_)
+    {}
+
+    pfxstringstream(pfxstringstream&&) = default;
+    pfxstringstream& operator=(pfxstringstream&&) = default;
+
+    std::string str() const { return sbuf_.str(); }
+    void str(const std::string& s) { sbuf_.str(s); }
+
+    prefixbuf* rdbuf() { return &pbuf_; }
+
+private:
+    std::stringbuf sbuf_;
+    prefixbuf pbuf_;
+};
+
+
+} // namespace util
+} // namespace arb
---
+++ tests/unit/test_prefixbuf.cpp
@@ -0,0 +1,113 @@
+#include "../gtest.h"
+
+#include <cstring>
+#include <iomanip>
+#include <string>
+#include <sstream>
+
+#include <util/prefixbuf.hpp>
+
+using namespace arb::util;
+
+TEST(prefixbuf, prefix) {
+    auto write_sputn = [](std::streambuf& b, const char* c) {
+        b.sputn(c, std::strlen(c));
+    };
+
+    auto write_sputc = [](std::streambuf& b, const char* c) {
+        while (*c) {
+            b.sputc(*c++);
+        }
+    };
+
+    std::stringbuf s;
+    write_sputn(s, "starting text\n");
+
+    prefixbuf p(&s);
+    p.prefix = ":) ";
+
+    write_sputn(p, "foo\nbar ");
+    write_sputc(p, "quux\nbaz ");
+    p.prefix = "^^ ";
+    write_sputn(p, "xyzzy\nplugh\n");
+
+    std::string expected =
+        "starting text\n"
+        ":) foo\n"
+        ":) bar quux\n"
+        ":) baz xyzzy\n"
+        "^^ plugh\n";
+
+    EXPECT_EQ(expected, s.str());
+}
+
+TEST(prefixbuf, pfxstringstream) {
+    pfxstringstream p;
+
+    p.rdbuf()->prefix = "...";
+
+    p << "_foo_ " << std::setw(5) << 123 << "\n";
+    p << std::showbase << std::hex << 42;
+
+    std::string expected =
+        "..._foo_   123\n"
+        "...0x2a";
+
+    EXPECT_EQ(expected, p.str());
+}
+
+TEST(prefixbuf, pfxstringstream_str) {
+    pfxstringstream p;
+    p.str("0123456789");
+    p.rdbuf()->prefix = "__";
+    p << "a\nb";
+
+    std::string expected = "__a\n__b789";
+    EXPECT_EQ(expected, p.str());
+}
+
+TEST(prefixbuf, indent_manip) {
+    pfxstringstream p;
+
+    p << settab(2);
+    p << "0\n"
+      << indent
+      << "1\n"
+      << indent(2)
+      << "3\n"
+      << indent
+      << "4\n"
+      << popindent(2)
+      << "1\n"
+      << popindent
+      << "0";
+
+    std::string expected =
+        "0\n"
+        "  1\n"
+        "      3\n"
+        "        4\n"
+        "  1\n"
+        "0";
+
+    EXPECT_EQ(expected, p.str());
+}
+
+TEST(prefixbuf, setprefix) {
+    pfxstringstream p;
+
+    p << indent << "one\ntwo ";
+    p << setprefix("--->"); // override prefix
+    p << "three\nfour ";
+    p << indent(0)         // restore indentation
+      << "five\nsix";
+
+    std::string expected =
+        "    one\n"
+        "    two three\n"
+        "--->four five\n"
+        "    six";
+
+    EXPECT_EQ(expected, p.str());
+}
+
