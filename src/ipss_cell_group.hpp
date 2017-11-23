36e4a569b6c9e3a7379481eae23030a7a3d7fb05
klijn1 <nonoice@gmail.com>
2017-11-23 15:26:50 +0100

    Moved all state to the cell_info struct Add test for ranges Add test for reset

--- src/ipss_cell_group.hpp
+++ src/ipss_cell_group.hpp
@@ -19,25 +19,13 @@ namespace arb {
 
 
 class ipss_cell_group: public cell_group {
-struct ipss_info;
-using cell_gen_time = std::tuple<std::vector<ipss_info>::const_iterator,
-                                 std::vector<std::mt19937>::iterator,
-                                 std::vector<time_type>::iterator>;
-
 public:
     ipss_cell_group(std::vector<cell_gid_type> gids, const recipe& rec) {
         cells_.reserve(gids.size());
         for (auto gid: gids) {
             cells_.emplace_back(
-                util::any_cast<ipss_cell>(rec.get_cell_description(gid)),
-                gid);
-
-            random_generators.push_back(std::mt19937(gid));
-            times_.push_back(0.0);
+                util::any_cast<ipss_cell>(rec.get_cell_description(gid)), gid);
         }
-
-
-
         reset();
     }
 
@@ -47,10 +35,12 @@ public:
 
     void reset() override {
         clear_spikes();
-        for (auto &time: times_) {
-            time = 0.0;
-        }
+        for (auto &cell: cells_) {
+            cell.time = 0.0;
 
+            // Reset the random number generator!
+            cell.generator = std::mt19937(cell.gid);
+        }
     }
 
     void set_binning_policy(binning_kind policy, time_type bin_interval) override {}
@@ -58,17 +48,11 @@ public:
     void advance(epoch ep, time_type dt, const event_lane_subrange& events) override {
         // We only know the dt at this stage.
         auto distribution = std::uniform_real_distribution<float>(0.f, 1.0f);
-        for (cell_gen_time cell_gen_time_it(cells_.cbegin(), random_generators.begin(), times_.begin());
-            std::get<0>(cell_gen_time_it) != cells_.cend();
-            ++std::get<0>(cell_gen_time_it), ++std::get<1>(cell_gen_time_it), ++std::get<2>(cell_gen_time_it))
-
+        for (auto& cell: cells_)
         {
-            auto cell = *std::get<0>(cell_gen_time_it);
-            auto time = *std::get<2>(cell_gen_time_it);
-
             // Get begin and start range, depending on the cell config and
             // end the epoch ranges
-            auto t = std::max(cell.start_time, time);
+            auto t = std::max(cell.start_time, cell.time);
             auto t_end = std::min(cell.stop_time, ep.tfinal);
 
             // if cell does not fire in current advance step continue
@@ -80,19 +64,18 @@ public:
             // First convert the rate from spikes/sec to ms
             time_type prob_per_dt = (cell.rate / 1000.0) * cell.sample_delta;
 
-            //while (t < t_end) does not work
-            // after x floating points this might result in t-small delta where
-            // you would expect it to be t_end. This while works as expected
+            // Float noise might result in a final step larger then t_end.
             while (t < t_end)
             {
                 // roll a dice between 0 and 1, if below prop we have a spike
-                if (distribution(*std::get<1>(cell_gen_time_it)) < prob_per_dt) {
+                if (distribution(cell.generator) < prob_per_dt) {
                     spikes_.push_back({ { cell.gid, 0 }, t });
                 }
                 t += cell.sample_delta;
             }
+
             // Save the cell specific time
-            *std::get<2>(cell_gen_time_it) = t;
+            cell.time = t;
         }
     }
 
@@ -116,23 +99,23 @@ private:
     // RSS description plus gid for each RSS cell.
     struct ipss_info: public ipss_cell {
         ipss_info(ipss_cell desc, cell_gid_type gid):
-            ipss_cell(std::move(desc)), gid(gid)
+            ipss_cell(std::move(desc)),
+            gid(gid),
+            time(0.0),
+            generator(gid)
         {}
 
         cell_gid_type gid;
+        time_type time;
+        std::mt19937 generator;
+
     };
 
     // RSS cell descriptions.
     std::vector<ipss_info> cells_;
 
-    // Simulation time for all RSS cells in the group.
-    std::vector<time_type> times_;
-
     // Spikes that are generated.
     std::vector<spike> spikes_;
-
-    // Random number generator
-    std::vector<std::mt19937> random_generators;
 };
 
 } // namespace arb
--- tests/unit/test_ipss_cell.cpp
+++ tests/unit/test_ipss_cell.cpp
@@ -51,16 +51,15 @@ TEST(ipss_cell_group, basic_usage)
 
     }
 
+    // Check the output of the cell
     EXPECT_EQ(spikes.size(), spikes_from_cell.size());
-
-
     for (auto s1 = spikes.begin(), s2 = spikes_from_cell.begin(); s1 < spikes.end(); ++s1, ++s2) {
         ASSERT_FLOAT_EQ(s1->time, s2->time);
     }
 }
 
 
-TEST(ipss_cell_group, later_start)
+TEST(ipss_cell_group, test_reset)
 {
     // Create an array of spike times for 1000 ms of time using the same
     // seed for the random number generator (in one go)
@@ -88,10 +87,64 @@ TEST(ipss_cell_group, later_start)
     // Create the cell_group
     ipss_cell desc{ begin, end, rate, sample_delta };
     ipss_cell_group sut({ 0 }, ipss_recipe(1u, desc));
+
+    // Run the cell_group for some time
+    epoch ep(0, 10);
+    sut.advance(ep, dt, {});
+
+    // Reset the cell to zero
+    sut.reset();
+
+    // And now generate the same spikes as the basic test
     std::vector<spike> spikes_from_cell;
     for (int idx = 0; idx < 10; ++idx) {
         epoch ep(100.0 * idx, 100.0 * idx + 100.0);
         sut.advance(ep, dt, {});
+        spikes_from_cell.insert(spikes_from_cell.end(), sut.spikes().begin(), sut.spikes().end());
+        sut.clear_spikes();
+
+    }
+
+    // Check the output of the cell
+    EXPECT_EQ(spikes.size(), spikes_from_cell.size());
+    for (auto s1 = spikes.begin(), s2 = spikes_from_cell.begin(); s1 < spikes.end(); ++s1, ++s2) {
+        ASSERT_FLOAT_EQ(s1->time, s2->time);
+    }
+}
+
+
+TEST(ipss_cell_group, start_end_different_then_zero)
+{
+    // Create an array of spike times for 1000 ms of time using the same
+    // seed for the random number generator (in one go)
+    // Then let the cell_group generate the spikes, they should be the same
+    time_type begin = 50;
+    time_type end = 500.0;
+    time_type rate = 20;  // Hz
+    time_type sample_delta = 0.1; // 0.1 ms
+
+    constexpr time_type dt = 0.01; // dt is ignored by ipss_cell_group::advance().
+
+                                   // Create the generator
+    std::mt19937 gen(0);
+    auto distribution = std::uniform_real_distribution<float>(0.f, 1.0f);
+    auto prop_per_time_step = (rate / 1000.0) * sample_delta;
+
+    // Create the spikes and store in the correct way
+    std::vector<spike> spikes;
+    for (time_type t = begin; t < end; t += sample_delta) {
+        if (distribution(gen) < prop_per_time_step) {
+            spikes.push_back({ { 0, 0 }, t });
+        }
+    }
+
+    // Create the cell_group
+    ipss_cell desc{ begin, end, rate, sample_delta };
+    ipss_cell_group sut({ 0 }, ipss_recipe(1u, desc));
+    std::vector<spike> spikes_from_cell;
+    for (int idx = 0; idx < 10; ++idx) {
+        epoch ep(100.0 * idx, 100.0 * idx + 100.0);
+        sut.advance(ep, dt, {});  // We advance past the end time
         spikes_from_cell.insert(spikes_from_cell.end(), sut.spikes().begin(), sut.spikes().end());
         sut.clear_spikes();
 
