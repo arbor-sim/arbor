719120383c30eb1248ba37e122bb1029a856f233
klijn1 <nonoice@gmail.com>
2017-11-24 13:50:56 +0100

    Documentation and code cleanup of ipss_cell.hpp

--- src/ipss_cell.hpp
+++ src/ipss_cell.hpp
@@ -3,21 +3,20 @@
 #include <utility>
 #include <random>
 
-
 #include <cell_group.hpp>
 #include <recipe.hpp>
 #include <ipss_cell_description.hpp>
 #include <util/unique_any.hpp>
 
-
-#include <iostream>
-
 namespace arb {
 
-    /// Cell group implementing RSS cells.
+/// Cell implementation of inhomogeneous Poisson spike generator.
+/// Spikes are generated at a configurable sample rate. With a rate that can be
+/// varied across time. Rate can be piece wise constant or linearly interpolated
+/// per sample time step.
 class ipss_cell : public ipss_cell_description {
 public:
-
+    /// Constructor
     ipss_cell(ipss_cell_description desc, cell_gid_type gid) :
         ipss_cell_description(std::move(desc)), gid_(gid), time_(0.0),
         generator_(gid), prob_(0.0), prob_dt_(0.0) {
@@ -26,22 +25,39 @@ public:
         // next rate change.
         rates_per_time.push_back({ stop_time + sample_delta, rates_per_time.back().second });
 
-
-        initialize_cell_state();
+        reset();
     }
 
     cell_kind get_cell_kind() const {
         return cell_kind::inhomogeneous_poisson_spike_source;
     }
 
-
+    /// reset internal variables based on the rate vector supplied add construction
+    ///   - Find the first valid rate depending on the start time_
+    ///   - Set the rates and rate change per step
+    ///   - Set the iterator to the next rate change
+    ///   - time zero
+    ///   - reseed rng
     void reset() {
         time_ = 0.0;
 
         // Reset the random number generator!
         generator_.seed(gid_);
 
-        initialize_cell_state();
+        // Sanity check: we need a rate at the start of the neuron time_
+        if (rates_per_time.cbegin()->first > start_time) {
+            throw std::logic_error("The start time of the neuron is before the first time/rate pair");
+        }
+
+        it_next_rate_ = rates_per_time.cbegin();
+        prob_ = it_next_rate_->second;
+
+        // loop over the entries until we have the last change before
+        // the start time_ of the cell
+        while (it_next_rate_->first <= start_time) {
+            rate_change_step();
+        }
+
     }
 
     /// Advance the Poisson generator until end of ep
@@ -68,8 +84,7 @@ public:
             double t_end_step = it_next_rate_->first < t_end ?
                 it_next_rate_->first : t_end;
 
-            while (t < t_end_step)
-            {
+            while (t < t_end_step) {
                 // roll a dice between 0 and 1, if below prop we have a spike
                 if (distribution_(generator_) < prob) {
                     spikes_.push_back({ { gid_, 0 }, t });
@@ -84,32 +99,11 @@ public:
                 rate_change_step();
             }
         }
+        // Store for next epoch
         time_ = t;
         prob_ = prob;
     }
-
-
 private:
-    /// Set internal variables based on the rate vector supplied add construction
-    ///   - Find the first valid rate depending on the start time_
-    ///   - Set the rates and rate change per step
-    ///   - Set the iterator to the next rate change
-    void initialize_cell_state() {
-        // Sanity check: we need a rate at the start of the neuron time_
-        if (rates_per_time.cbegin()->first > start_time) {
-            throw std::logic_error("The start time of the neuron is before the first time/rate pair");
-        }
-
-        it_next_rate_ = rates_per_time.cbegin();
-        prob_ = it_next_rate_->second;
-
-        // loop over the entries until we have the last change before
-        // the start time_ of the cell
-        while (it_next_rate_->first <= start_time) {
-            rate_change_step();
-        }
-    }
-
     /// We need to do some book keeping when we have a rate_change step
     ///   - Set the new base rate based on the rate vector
     ///   - Update the next rate change it.
@@ -120,7 +114,6 @@ private:
         auto start_time = it_next_rate_->first;
         prob_ = (it_next_rate_->second / 1000.0) * sample_delta;
 
-        // Increase the iterator
         it_next_rate_++;
 
         // If we interpolate we calculate the rate_change per sample_delta step
@@ -148,7 +141,7 @@ private:
     // interpolation.
     double prob_dt_;
 
-    // pointer into a vector of time-rate pairs when to change to new rate
+    // iterator in the vector of time-rate pairs when to change to new rate
     std::vector<std::pair<time_type, double>>::const_iterator it_next_rate_;
 
     // Distribution for Poisson generation
