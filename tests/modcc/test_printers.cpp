410da95f60cc888a191e410c60a68315e04551da
Sam Yates <halfflat@gmail.com>
2017-11-24 20:15:44 +0100

    Test expected generated code in `test_printers.cpp`

--- tests/modcc/test_printers.cpp
+++ tests/modcc/test_printers.cpp
@@ -1,17 +1,30 @@
+#include <regex>
+#include <string>
+
 #include "test.hpp"
 
 #include "cprinter.hpp"
+#include "expression.hpp"
 
-using scope_type = Scope<Symbol>;
-using symbol_map = scope_type::symbol_map;
-using symbol_ptr = Scope<Symbol>::symbol_ptr;
+//using scope_type = Scope<Symbol>;
+//using symbol_map = scope_type::symbol_map;
+//using symbol_ptr = Scope<Symbol>::symbol_ptr;
+
+struct testcase {
+    const char* source;
+    const char* expected;
+};
+
+static std::string strip(std::string text) {
+    static std::regex rx(R"(\s)");
+    return std::regex_replace(text, rx, "");
+}
 
 TEST(CPrinter, statement) {
-    std::vector<const char*> expressions =
-    {
-"y=x+3",
-"y=y^z",
-"y=exp(x/2 + 3)",
+    std::vector<testcase> testcases = {
+        {"y=x+3",            "y=x+3"},
+        {"y=y^z",            "y=std::pow(y,z)"},
+        {"y=exp((x/2) + 3)", "y=exp(x/2+3)"}
     };
 
     // create a scope that contains the symbols used in the tests
@@ -22,12 +35,9 @@ TEST(CPrinter, statement) {
     scope->add_local_symbol("y", make_symbol<LocalVariable>(Location(), "y", localVariableKind::local));
     scope->add_local_symbol("z", make_symbol<LocalVariable>(Location(), "z", localVariableKind::local));
 
-    for(auto const& expression : expressions) {
-        auto e = parse_line_expression(expression);
-
-        // sanity check the compiler
-        EXPECT_NE(e, nullptr);
-        if( e==nullptr ) continue;
+    for (const auto& tc: testcases) {
+        auto e = parse_line_expression(tc.source);
+        ASSERT_TRUE(e);
 
         e->semantic(scope);
         auto v = make_unique<CPrinter>();
@@ -35,19 +45,30 @@ TEST(CPrinter, statement) {
 
         verbose_print(e->to_string());
         verbose_print(" :--: ", v->text());
+
+        EXPECT_EQ(strip(tc.expected), strip(v->text()));
     }
 }
 
 TEST(CPrinter, proc) {
-    std::vector<const char*> expressions =
-    {
-"PROCEDURE trates(v) {\n"
-"    LOCAL k\n"
-"    minf=1-1/(1+exp((v-k)/k))\n"
-"    hinf=1/(1+exp((v-k)/k))\n"
-"    mtau = 0.6\n"
-"    htau = 1500\n"
-"}"
+    std::vector<testcase> testcases = {
+        {
+            "PROCEDURE trates(v) {\n"
+            "    LOCAL k\n"
+            "    minf=1-1/(1+exp((v-k)/k))\n"
+            "    hinf=1/(1+exp((v-k)/k))\n"
+            "    mtau = 0.6\n"
+            "    htau = 1500\n"
+            "}"
+            ,
+            "void trates(int i_, value_type v) {\n"
+            "value_type k;\n"
+            "minf[i_] = 1-1/(1+exp((v-k)/k));\n"
+            "hinf[i_] = 1/(1+exp((v-k)/k));\n"
+            "mtau[i_] = 0.6;\n"
+            "htau[i_] = 1500;\n"
+            "}"
+        }
     };
 
     // create a scope that contains the symbols used in the tests
@@ -58,8 +79,8 @@ TEST(CPrinter, proc) {
     globals["htau"] = make_symbol<VariableExpression>(Location(), "htau");
     globals["v"]    = make_symbol<VariableExpression>(Location(), "v");
 
-    for(auto const& expression : expressions) {
-        expression_ptr e = parse_procedure(expression);
+    for (const auto& tc: testcases) {
+        expression_ptr e = parse_procedure(tc.source);
         ASSERT_TRUE(e->is_symbol());
 
         auto procname = e->is_symbol()->name();
@@ -71,5 +92,7 @@ TEST(CPrinter, proc) {
 
         verbose_print(proc->to_string());
         verbose_print(" :--: ", v->text());
+
+        EXPECT_EQ(strip(tc.expected), strip(v->text()));
     }
 }
