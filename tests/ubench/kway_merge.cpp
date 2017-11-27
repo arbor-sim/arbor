169927a0db042f0d2ec2beea9f8c5d6f51881cba
bcumming <bcumming@cscs.ch>
2017-11-27 09:57:03 +0100

    add tournament tree to kway ubench

--- tests/ubench/kway_merge.cpp
+++ tests/ubench/kway_merge.cpp
@@ -13,8 +13,139 @@
 #include <algorithm>
 #include <random>
 #include <vector>
+#include <iostream>
 
 #include <benchmark/benchmark.h>
+
+unsigned next_power_2(unsigned x) {
+    unsigned n = 1;
+    while (n<x) n<<=1;
+    return n;
+}
+
+template <typename T>
+struct tourney_tree {
+    using iterator = typename std::vector<T>::const_iterator;
+    using value_type = T;
+    using key_val = std::pair<unsigned, iterator>;
+
+    tourney_tree(const std::vector<std::vector<T>>& input):
+        input_(input),
+        n_lanes_(input_.size())
+    {
+        // must have at least 1 queue
+        assert(n_lanes_!=0);
+        // maximum value in unsigned limits how many queues we can have
+        assert(n_lanes_<(1u<<(sizeof(unsigned)*8-1)));
+
+        leaves_ = next_power_2(n_lanes_);
+        nodes_ = 2*(leaves_-1)+1; // 2*l-1 witho overflow protection
+
+        heap_.resize(nodes_);
+
+        for (auto i=0u; i<leaves_; ++i) {
+            heap_[leaf(i)] = i<n_lanes_?
+                key_val(i, input[i].begin()):
+                key_val(n_lanes_, iterator()); // invalid node
+        }
+
+        setup(0);
+    }
+
+    void setup(unsigned i) {
+        if (is_leaf(i)) return;
+        setup(left(i));
+        setup(right(i));
+        merge_up(i);
+    };
+
+    void merge_up(unsigned i) {
+        const auto l = left(i);
+        const auto r = right(i);
+        heap_[i] = compare(l, r)? heap_[l]: heap_[r];
+    }
+
+    void update_lane(unsigned lane) {
+        unsigned i = leaf(lane);
+
+        // update the iterator and test if the lane is empty
+        ++heap_[i].second;
+        if (heap_[i].second==input_[lane].end()) {
+            heap_[i].first = leaves_; // mark lane as empty
+        }
+
+        // walk from leaf to root
+        while ((i=parent(i))) {
+            merge_up(i);
+        }
+        merge_up(0); // handle to root
+    }
+
+    bool compare(unsigned l, unsigned r) {
+        if (!is_valid(l)) {
+            return false;
+        }
+        if (!is_valid(r)) {
+            return true;
+        }
+        return *(heap_[l].second)<*(heap_[r].second);
+    }
+
+    unsigned parent(unsigned i) {
+        return (i-1)>>1;
+    }
+    unsigned left(unsigned i) {
+        return (i<<1) + 1;
+    }
+    unsigned right(unsigned i) {
+        return left(i)+1;
+    }
+    unsigned leaf(unsigned i) {
+        return i+leaves_-1;
+    }
+    bool is_leaf(unsigned i) {
+        return i>=leaves_-1;
+    }
+    bool is_in_range(unsigned i) {
+        return i<leaf(n_lanes_);
+    }
+    bool is_valid(unsigned i) {
+        return heap_[i].first<n_lanes_;
+        //auto lane = heap_[i].first;
+        //return lane<n_lanes_ && heap_[i].second!=input_[lane].end();
+    }
+
+    void print() {
+        for (auto i=0; i<nodes_; ++i) {
+            if (is_valid(i))
+                std::cout << *(heap_[i].second) << " ";
+            else
+                std::cout << "* ";
+        }
+        std::cout << "\n";
+    }
+
+
+    bool empty() {
+        return !is_valid(0);
+    }
+
+    const T& head() {
+        return *(heap_[0].second);
+    }
+
+    void pop() {
+        auto lane = heap_[0].first;
+        update_lane(lane);
+    }
+
+    std::vector<key_val> heap_;
+    const std::vector<std::vector<T>>& input_;
+    unsigned leaves_;
+    unsigned nodes_;
+    unsigned n_lanes_;
+};
+
 
 std::vector<float> kway_dumb(const std::vector<std::vector<float>>& lanes) {
     auto n = 0u;
@@ -58,6 +189,23 @@ std::vector<float> kway_naive(const std::vector<std::vector<float>>& lanes) {
         if (it->second==lanes[it->first].end()) {
             tops.erase(it);
         }
+    }
+
+    return m;
+}
+
+std::vector<float> kway_tourney(const std::vector<std::vector<float>>& lanes) {
+    auto n = 0u;
+    for (auto& l: lanes) n+=l.size();
+
+    std::vector<float> m;
+    m.reserve(n);
+
+    tourney_tree<float> tree(lanes);
+
+    while (!tree.empty()) {
+        m.push_back(tree.head());
+        tree.pop();
     }
 
     return m;
@@ -111,16 +259,18 @@ std::vector<float> kway_heap(const std::vector<std::vector<float>>& lanes) {
 template <typename F>
 void test(benchmark::State& state, F f) {
     const unsigned num_lanes = state.range(0);
-    const unsigned n = state.range(1);
+    unsigned n = state.range(1);
 
     // generate input information
     std::vector<std::vector<float>> lanes(num_lanes);
     auto seed = 0u;
     for (auto& l: lanes) {
-        l.reserve(n);
         std::mt19937 gen(seed++);
+        std::uniform_int_distribution<unsigned> dis_n(n/2, n);
+        auto m = dis_n(gen);
+        l.reserve(m);
         std::uniform_real_distribution<> dis(0, 1e6);
-        for (auto i=0u; i<n; ++i) {
+        for (auto i=0u; i<m; ++i) {
             l.push_back(dis(gen));
         }
         std::sort(l.begin(), l.end());
@@ -136,18 +286,20 @@ void test(benchmark::State& state, F f) {
 void test_dumb (benchmark::State& state) { test(state, kway_dumb);  }
 void test_naive(benchmark::State& state) { test(state, kway_naive); }
 void test_heap (benchmark::State& state) { test(state, kway_heap);  }
+void test_tourney (benchmark::State& state) { test(state, kway_tourney);  }
 
 
 void run_custom_arguments(benchmark::internal::Benchmark* b) {
-    for (auto num_lanes: {3, 10, 20, 100}) {
-        for (auto n: {100, 1000, 10000}) {
+    for (auto num_lanes: {2, 3, 5, 7, 10, 100, 1000}) {
+        for (auto n: {100, 1000, 10000, 100000}) {
             b->Args({num_lanes, n});
         }
     }
 }
 
-BENCHMARK(test_dumb)->Apply(run_custom_arguments);
+//BENCHMARK(test_dumb)->Apply(run_custom_arguments);
 BENCHMARK(test_naive)->Apply(run_custom_arguments);
 BENCHMARK(test_heap)->Apply(run_custom_arguments);
+BENCHMARK(test_tourney)->Apply(run_custom_arguments);
 
 BENCHMARK_MAIN();
