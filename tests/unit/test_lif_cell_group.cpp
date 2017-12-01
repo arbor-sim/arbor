4234dba81c4aeddb4cca4df56f9bf2603d0639ff
Marko Kabic <marko.kabic@cscs.ch>
2017-12-01 14:17:06 +0100

    Added tests for LIF (multicore) cells

--- tests/unit/test_lif_cell_group.cpp
+++ tests/unit/test_lif_cell_group.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <lif_cell_description.hpp>
 #include <lif_cell_group_mc.hpp>
+#include <load_balance.hpp>
 #include <model.hpp>
 #include <recipe.hpp>
 #include <rss_cell.hpp>
@@ -55,7 +56,7 @@ public:
             return lif_cell_description();
         }
         // Produces just a single spike at time 0ms.
-        auto rs = rss_cell::rss_cell();
+        auto rs = arb::rss_cell();
         rs.start_time = 0;
         rs.period = 1;
         rs.stop_time = 0.5;
@@ -91,7 +92,6 @@ public:
         return ncells_;
     }
 
-    // LIF neurons have gid in range [1..ncells_-1] whereas fake cell is numbered with 0.
     cell_kind get_cell_kind(cell_gid_type gid) const override {
         return cell_kind::lif_neuron;
     }
@@ -141,16 +141,15 @@ TEST(lif_cell_group_mc, recipe)
     EXPECT_EQ(99, rr.connections_on(0u)[0].source.gid);
 }
 
-TEST(lif_cell_group_mc, cell_group_factory) {
-    std::vector<util::unique_any> cells;
-    cells.emplace_back(lif_cell_description());
-    cells.emplace_back(lif_cell_description());
+TEST(lif_cell_group_mc, spikes) {
+    // make two lif cells
+    path_recipe recipe(2, 1000, 0.1);
 
-    cell_group_ptr group = cell_group_factory(
-        cell_kind::lif_neuron,
-        0,
-        cells,
-        backend_policy::use_multicore);
+    hw::node_info nd;
+    nd.num_cpu_cores = threading::num_threads();
+
+    auto decomp = partition_load_balance(recipe, nd);
+    model m(recipe, decomp);
 
     std::vector<postsynaptic_spike_event> events;
 
@@ -165,63 +164,25 @@ TEST(lif_cell_group_mc, cell_group_factory) {
     // event, should thus trigger new spike (first neuron).
     events.push_back({{0, 0}, 50, 1000});
 
-    // This is event to the second neuron.
-    events.push_back({{1, 0}, 1, 1000});
+    m.inject_events(events);
 
-    group->enqueue_events(events);
-    group->advance(100, 0.01);
-    std::vector<spike> spikes = group->spikes();
-    EXPECT_EQ(3, spikes.size());
+    time_type tfinal = 100;
+    time_type dt = 0.01;
+    m.run(tfinal, dt);
+
+    // we expect 4 spikes: 2 by both neurons
+    EXPECT_EQ(4, m.num_spikes());
 }
 
-TEST(lif_cell_group_mc, spikes_testing) {
-    std::vector<util::unique_any> cells;
-    cells.emplace_back(lif_cell_description());
-
-    auto gr = cell_group_factory(cell_kind::lif_neuron, 0, cells, backend_policy::use_multicore);
-    auto group = dynamic_cast<lif_cell_group_mc*>(gr.get());
-
-    std::vector<postsynaptic_spike_event> events;
-    std::vector<time_type> incoming_spikes;
-    time_type simulation_end = 50;
-
-    // Add events at times i for the first 80% time of the simulation.
-    for (int i = 1; i < (int) (0.8 * simulation_end); i++) {
-        // last parameter is the weight
-        events.push_back({{0, 0}, static_cast<time_type>(i), 100});
-        incoming_spikes.push_back(i);
-    }
-
-    group->enqueue_events(events);
-
-    // second parameter is dt, but is ignored
-    group->advance(simulation_end, 0.01);
-    std::vector<spike> spikes = group->spikes();
-
-    std::ofstream in_spikes_file;
-    in_spikes_file.open("../../tests/unit/lif_neuron_input_spikes.txt");
-
-    std::ofstream out_spikes_file;
-    out_spikes_file.open("../../tests/unit/lif_neuron_output_spikes.txt");
-
-    for (auto& in_spike : incoming_spikes) {
-        in_spikes_file << in_spike << std::endl;
-    }
-
-    for (auto& out_spike : spikes) {
-        out_spikes_file << out_spike.time << std::endl;
-    }
-
-    in_spikes_file.close();
-    out_spikes_file.close();
-}
-
-TEST(lif_cell_group_mc, domain_decomposition)
+TEST(lif_cell_group_mc, ring)
 {
     // Total number of cells.
     int num_cells = 99;
     double weight = 1000;
     double delay = 1;
+
+    hw::node_info nd;
+    nd.num_cpu_cores = threading::num_threads();
 
     // Total simulation time.
     time_type simulation_time = 100;
@@ -230,10 +191,7 @@ TEST(lif_cell_group_mc, domain_decomposition)
     cell_size_type group_size = 10;
 
     auto recipe = ring_recipe(num_cells, weight, delay);
-    // Group rules specifies the number of cells in each cell group
-    // and the backend policy.
-    group_rules rules{group_size, backend_policy::use_multicore};
-    domain_decomposition decomp(recipe, rules);
+    auto decomp = partition_load_balance(recipe, nd);
 
     // Creates a model with a ring recipe of lif neurons
     model mod(recipe, decomp);
@@ -248,8 +206,6 @@ TEST(lif_cell_group_mc, domain_decomposition)
 
     // Runs the simulation for simulation_time with given timestep
     mod.run(simulation_time, 0.01);
-    // The number of cell groups.
-    EXPECT_EQ(11, mod.num_groups());
     // The total number of cells in all the cell groups.
     EXPECT_EQ((num_cells + 1), recipe.num_cells());
 
