4747bab1944425290e4a2c9610bb6178d76c628f
Sam Yates <halfflat@gmail.com>
2017-12-21 01:37:15 +0100

    Remove errant commented-out code.

--- tests/unit/test_fvm_multi.cpp
+++ tests/unit/test_fvm_multi.cpp
@@ -856,10 +856,6 @@ TEST(fvm_multi, ion_weights) {
     double con_int = 80;
     double con_ext = 120;
 
-    //std::vector<std::vector<fvm_value_type>> expected_wght = {
-    //    {1./3}, {1./3, 1./2, 0.}, {1./4, 0., 0.}, {0., 0., 0., 0.},
-    //};
-
     auto construct_cell = [](cell& c) {
         c.add_soma(5);
 
