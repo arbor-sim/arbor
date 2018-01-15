de57bae9db69b3678eebf4f2776707df71d8bd3b
Marko Kabic <marko.kabic@cscs.ch>
2018-01-15 15:50:19 +0100

    Small changes

--- src/common_types.hpp
+++ src/common_types.hpp
@@ -71,7 +71,6 @@ enum cell_kind {
     cable1d_neuron,           // Our own special mc neuron
     lif_neuron,               // Leaky-integrate and fire neuron
     regular_spike_source,     // Regular spiking source
-    poisson_spike_source,     // Poisson spiking source
     data_spike_source,        // Spike source from values inserted via description
 };
 
