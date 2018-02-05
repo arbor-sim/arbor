aeaace5a9069030fc5a3acbe5fda9e75e511d6ae
bcumming <bcumming@cscs.ch>
2018-02-05 14:53:18 +0100

    add threading benchmark

---
+++ example/threadbench/bench.cpp
@@ -0,0 +1,68 @@
+#include <chrono>
+#include <iostream>
+#include <thread>
+
+#include <threading/threading.hpp>
+
+using namespace arb;
+
+void run(unsigned long us_per_task, unsigned tasks) {
+    auto duration = std::chrono::microseconds(us_per_task);
+    arb::threading::parallel_for::apply(
+        0, tasks,
+        [&](unsigned i){std::this_thread::sleep_for(duration);});
+}
+
+
+int main() {
+    const auto nthreads = threading::num_threads();
+    std::cout << "thread benchmark: " << threading::description() << " with " << nthreads << " threads\n";
+
+    const unsigned long us_per_sec = 1000000;
+
+    threading::timer timer;
+
+    // Do a dummy run to initialize the threading backend
+    run(1000, 1000); // 1000 * 1 ms tasks
+
+    // pre-compute the output strings
+    const std::vector<unsigned long> tasks_per_second_per_thread{1, 10, 100, 1000, 10000, 10000};
+    const auto nruns = tasks_per_second_per_thread.size();
+
+    std::cout << "\ngathering results..." << std::endl;
+    std::vector<double> t_run;
+    t_run.reserve(nruns);
+    for (auto tpspt: tasks_per_second_per_thread) {
+        auto us_per_task = us_per_sec/tpspt;
+        std::cout << "  μs per task: " << us_per_task << "\n";
+        const auto start = timer.tic();
+        run(us_per_task, tpspt*nthreads);
+        t_run.push_back(timer.toc(start));
+    }
+
+    // Perform one thread's worth of work in a for loop.
+    // The time taken to perform this work can be used to
+    // determine the best case performance for the multithreaded
+    // runs.
+    std::cout << "gathering baseline..." << std::endl;
+    std::vector<double> t_baseline;
+    t_baseline.reserve(nruns);
+    for (auto tpspt: tasks_per_second_per_thread) {
+        auto us_per_task = us_per_sec/tpspt;
+        std::cout << "  μs per task: " << us_per_task << "\n";
+        auto duration = std::chrono::microseconds(us_per_task);
+        const auto start = timer.tic();
+        for (auto j=0u; j<tpspt; ++j) {
+            std::this_thread::sleep_for(duration);
+        }
+        t_baseline.push_back(timer.toc(start));
+    }
+
+    std::printf("\n%12s%12s%12s%12s\n", "us-per-task", "baseline", "run", "efficiency");
+    for (auto i=0u; i<nruns; ++i) {
+        const auto tb = t_baseline[i];
+        const auto tr = t_run[i];
+        unsigned us_per_task = us_per_sec / tasks_per_second_per_thread[i];
+        std::printf("%12u%12.3f%12.3f%12.1f\n", us_per_task, float(tr), float(tb), float(tb/tr*100.));
+    }
+}
