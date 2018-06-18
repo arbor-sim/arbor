20fbad271e8e12c5653d80b009c29e413b6af195
Sam Yates <halfflat@gmail.com>
2018-06-18 18:45:09 +0200

    WIP docs/html

--- cmake/ErrorTarget.cmake
+++ cmake/ErrorTarget.cmake
@@ -3,11 +3,21 @@
 #   comment : the COMMENT string for the real target, e.g. "Building the Sphinx documentation"
 #   message : the error message
 
-function(add_error_target name comment message)
-    if (NOT TARGET ${name})
-        add_custom_target(${name}
-            COMMAND echo "  Error: ${message}."
+function(add_error_target target comment error_message)
+    if(NOT TARGET "${target}")
+        add_custom_target("${target}"
+            COMMAND echo "  Error: ${error_message}."
             COMMAND exit 1
             COMMENT "${comment}")
     endif()
 endfunction()
+
+macro(add_target_if condition target comment error_message)
+    if(${condition})
+        add_custom_target("${target}"
+            COMMAND true
+            COMMENT "${comment}")
+    else()
+        add_error_target("${target}" "${comment}" "${error_message}")
+    endif()
+endmacro()
