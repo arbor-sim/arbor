7b1d8eff97f56cd18ef45b45c6dd5ca3947349b0
abiakarn <nora.abiakar@gmail.com>
2018-07-13 14:40:38 +0200

    fixes to test_thread

--- test/unit/test_thread.cpp
+++ test/unit/test_thread.cpp
@@ -145,7 +145,7 @@ TEST(task_group, individual_tasks) {
     g.wait();
 }
 
-TEST(task_group, parallel_for_tasks) {
+TEST(task_group, parallel_for_sleep) {
     // Simple check for deadlock for nested parallelism
     arb::threading::task_group g;
     auto num_threads = arb::num_threads();
@@ -159,11 +159,7 @@ TEST(task_group, parallel_for_tasks) {
 
 TEST(task_group, parallel_for) {
 
-    int a = -1;
-    arb::threading::parallel_for::apply(0, 0, [&](int i) {a = i;});
-    EXPECT_EQ(-1, a);
-
-    for (int n = 1; n < 100000; n*=2) {
+    for (int n = 0; n < 10000; n=!n?1:2*n) {
         std::vector<int> v(n, -1);
         arb::threading::parallel_for::apply(0, n, [&](int i) {v[i] = i;});
         for (int i = 0; i< n; i++) {
@@ -174,21 +170,17 @@ TEST(task_group, parallel_for) {
 
 TEST(task_group, nested_parallel_for) {
 
-    int a = -1;
-    arb::threading::parallel_for::apply(0, 0, [&](int i) {
-        arb::threading::parallel_for::apply(0, 0, [&](int i){a = i;});
-    });
-    EXPECT_EQ(-1, a);
-
-
-    for (int n = 1; n < 500; n*=2) {
-        std::vector<std::vector<int>> v(n, std::vector<int>(n, -1));
-        arb::threading::parallel_for::apply(0, n, [&](int i) {
-            arb::threading::parallel_for::apply(0, n, [&](int j){v[i][j] = i*n + j;});
-        });
-        for (int j = 0; j < n; j++) {
+    for (int m = 1; m < 512; m*=2) {
+        for (int n = 0; n < 1000; n=!n?1:2*n) {
+            std::vector<std::vector<int>> v(n, std::vector<int>(m, -1));
+            arb::threading::parallel_for::apply(0, n, [&](int i) {
+                auto &w = v[i];
+                arb::threading::parallel_for::apply(0, m, [&](int j) { w[j] = i + j; });
+            });
             for (int i = 0; i < n; i++) {
-                EXPECT_EQ(i*n + j, v[i][j]);
+                for (int j = 0; j < m; j++) {
+                    EXPECT_EQ(i + j, v[i][j]);
+                }
             }
         }
     }
