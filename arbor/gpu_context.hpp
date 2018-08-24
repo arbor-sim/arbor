2c29767f090cc8b0c60462fe8d2b1a58f47506df
bcumming <bcumming@cscs.ch>
2018-08-24 11:15:24 +0200

    remove ARB_HAVE_GPU from header file

--- arbor/gpu_context.hpp
+++ arbor/gpu_context.hpp
@@ -1,21 +1,6 @@
-#include <memory>
-
-#ifdef ARB_HAVE_GPU
-#include <cuda.h>
-#include <cuda_runtime.h>
-#endif
+#pragma once
 
 namespace arb {
-
-#ifndef ARB_HAVE_GPU
-struct gpu_context {
-    bool has_gpu_;
-    size_t attributes_;
-
-    gpu_context(): has_gpu_(false), attributes_(0) {}
-};
-
-#else
 
 enum gpu_flags {
     has_concurrent_managed_access = 1,
@@ -26,32 +11,11 @@ struct gpu_context {
     bool has_gpu_;
     size_t attributes_;
 
-    gpu_context() : has_gpu_(true) {
-        cudaDeviceProp prop;
-        cudaGetDeviceProperties(&prop, 0);
-        attributes_ = 0;
-        if (prop.concurrentManagedAccess) {
-            attributes_ |= gpu_flags::has_concurrent_managed_access;
-        }
-        if (prop.major*100 + prop.minor >= 600) {
-            attributes_ |= gpu_flags::has_atomic_double;
-        }
-    };
+    gpu_context();
 
-    bool has_concurrent_managed_access() {
-        return attributes_ & gpu_flags::has_concurrent_managed_access;
-    }
-
-    bool has_atomic_double() {
-        return attributes_ & gpu_flags::has_atomic_double;
-    }
-
-    void synchronize_for_managed_access() {
-        if(!has_concurrent_managed_access()) {
-            cudaDeviceSynchronize();
-        }
-    }
+    bool has_concurrent_managed_access() const;
+    bool has_atomic_double() const;
+    void synchronize_for_managed_access() const;
 };
 
-#endif
 }
