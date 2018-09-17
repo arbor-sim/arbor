a2b39382abc272d989e36c9bc85575601e2e2878
noraabiakar <nora.abiakar@gmail.com>
2018-09-17 11:33:28 +0200

    Dry-run mode  (#582)
    Dry-run mode: 
    * An implementation of distributed_context that is used to mimic the performance of running an MPI distributed simulation with n ranks.
    * Verifiable against an MPI run with the same parameters. 
    
    Implementation: 
    * Describe the model on a single domain (tile) and translate it to however many domains we want to mimic using arb::tile and arb::symmetric_recipe. This allows us to know the exact behavior of the entire system by only running the simulation on a single node.
    * Mimic communication between domains using arb::dry_run_context
    
    Example: 
    * dryrun in example/ is a verifiable example of using dry-run mode with mc_cells
    
    Other:
    * Documentation of dry-run mode 
    * unit test for dry_run_context

--- arbor/execution_context.cpp
+++ arbor/execution_context.cpp
@@ -48,6 +48,24 @@ context make_context<MPI_Comm>(const proc_allocation& p, MPI_Comm comm) {
     return context(new execution_context(p, comm), [](execution_context* p){delete p;});
 }
 #endif
+template <>
+execution_context::execution_context<dry_run_info>(
+        const proc_allocation& resources,
+        dry_run_info d):
+        distributed(make_dry_run_context(d.num_ranks, d.num_cells_per_rank)),
+        thread_pool(std::make_shared<threading::task_system>(resources.num_threads)),
+        gpu(resources.has_gpu()? std::make_shared<gpu_context>(resources.gpu_id)
+                               : std::make_shared<gpu_context>())
+{}
+
+template <>
+context make_context(const proc_allocation& p, dry_run_info d) {
+    return context(new execution_context(p, d), [](execution_context* p){delete p;});
+}
+
+std::string distribution_type(const context& ctx) {
+    return ctx->distributed->name();
+}
 
 bool has_gpu(const context& ctx) {
     return ctx->gpu->has_gpu();
