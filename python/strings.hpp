2022b3a1574d7c71a11da2eb964cadd46be57fb0
Anne Kuesters <a.kuesters@fz-juelich.de>
2019-01-30 15:26:14 +0100

    deleted commented lines.

--- python/strings.hpp
+++ python/strings.hpp
@@ -6,23 +6,11 @@
 
 #include <string>
 
-//#include <arbor/common_types.hpp>
 #include <arbor/context.hpp>
-//#include <arbor/domain_decomposition.hpp>
-//#include <arbor/mc_cell.hpp>
-//#include <arbor/recipe.hpp>
-//#include <arbor/spike.hpp>
 
 namespace pyarb {
 
-//std::string cell_string(const arb::mc_cell&);
-//std::string cell_member_string(const arb::cell_member_type&);
-//std::string connection_string(const arb::cell_connection&);
 std::string context_string(const arb::context&);
-//std::string group_description_string(const arb::group_description&);
 std::string proc_allocation_string(const arb::proc_allocation&);
-//std::string proc_allocation_string(const arb::proc_allocation&);
-//std::string segment_location_string(const arb::segment_location&);
-//std::string spike_string(const arb::spike&);
 
 } // namespace pyarb
