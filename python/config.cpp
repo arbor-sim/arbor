246c2a1d96b546649636a12c3a5733016053eb8b
Anne Kuesters <a.kuesters@fz-juelich.de>
2019-02-28 16:42:37 +0100

    added configuration features (config(), print_config(dict))

---
+++ python/config.cpp
@@ -0,0 +1,13 @@
+#include <pybind11/pybind11.h>
+
+#include "config.hpp"
+
+namespace pyarb {
+
+void register_config(pybind11::module &m) {
+
+    m
+        .def("config", &config, "Get Arbor's configuration.")
+        .def("print_config", [](const pybind11::dict& d){return print_config(d);}, "Print Arbor's configuration.");
+}
+} // namespace pyarb
--- python/pyarb.cpp
+++ python/pyarb.cpp
@@ -6,18 +6,19 @@
 // types and functions to be exposed to Python.
 namespace pyarb {
 
+void register_config(pybind11::module& m);
 void register_contexts(pybind11::module& m);
 void register_queryenvvars(pybind11::module& m);
 #ifdef ARB_MPI_ENABLED
 void register_mpi(pybind11::module& m);
 #endif
-
 }
 
 PYBIND11_MODULE(arbor, m) {
     m.doc() = "arbor: Python bindings for Arbor.";
     m.attr("__version__") = ARB_VERSION;
 
+    pyarb::register_config(m);
     pyarb::register_contexts(m);
     pyarb::register_queryenvvars(m);
     #ifdef ARB_MPI_ENABLED
