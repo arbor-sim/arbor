0f1fa0724a515ed6dd92ac6dfc2d5952fee3fec6
Sam Yates <halfflat@gmail.com>
2019-05-17 14:57:23 +0200

    Workaround for Juwels link error.

--- arbor/include/arbor/sampling.hpp
+++ arbor/include/arbor/sampling.hpp
@@ -12,7 +12,7 @@ using cell_member_predicate = std::function<bool (cell_member_type)>;
 
 static cell_member_predicate all_probes = [](cell_member_type pid) { return true; };
 
-static inline cell_member_predicate one_probe(cell_member_type pid) {
+inline cell_member_predicate one_probe(cell_member_type pid) {
     return [pid](cell_member_type x) { return pid==x; };
 }
 
