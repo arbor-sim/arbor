66b04c127b20783ad29799d3d7aea49e22df69ec
akuesters <42005107+akuesters@users.noreply.github.com>
2019-06-11 14:09:54 +0200

    Py feature simulation (#778)
    Wrap `arb::simulation` type and interface for running `run()`, resetting `reset()`, and `set_binning_policy(policy, bin_interval)`. 
    
    Wrap `enum binning_kind`, used to set event binning on `simulation` API.
    
    Fixes #763

--- python/pyarb.cpp
+++ python/pyarb.cpp
@@ -13,6 +13,7 @@ void register_event_generators(pybind11::module& m);
 void register_identifiers(pybind11::module& m);
 void register_recipe(pybind11::module& m);
 void register_schedules(pybind11::module& m);
+void register_simulation(pybind11::module& m);
 
 #ifdef ARB_MPI_ENABLED
 void register_mpi(pybind11::module& m);
@@ -33,4 +34,5 @@ PYBIND11_MODULE(arbor, m) {
     #endif
     pyarb::register_recipe(m);
     pyarb::register_schedules(m);
+    pyarb::register_simulation(m);
 }
