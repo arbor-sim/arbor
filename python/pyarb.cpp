92620aa3ebfd92a805498ce8c37582d9334cd518
akuesters <42005107+akuesters@users.noreply.github.com>
2019-07-03 12:34:57 +0200

    Python wrapper: profiler (#796)
    Wraps the `meter_manager` with
    - the constructor
    - `start(ctx)`
    - `checkpoint(name, ctx)`
    - `checkpoint_names`
    - `times`
    
    and the `meter_report` with
    - `make_meter_report(meter_manager, ctx)`
    
    Fixes #765

--- python/pyarb.cpp
+++ python/pyarb.cpp
@@ -12,6 +12,7 @@ void register_contexts(pybind11::module& m);
 void register_domain_decomposition(pybind11::module& m);
 void register_event_generators(pybind11::module& m);
 void register_identifiers(pybind11::module& m);
+void register_profiler(pybind11::module& m);
 void register_recipe(pybind11::module& m);
 void register_schedules(pybind11::module& m);
 void register_simulation(pybind11::module& m);
@@ -32,11 +33,13 @@ PYBIND11_MODULE(arbor, m) {
     pyarb::register_domain_decomposition(m);
     pyarb::register_event_generators(m);
     pyarb::register_identifiers(m);
-    #ifdef ARB_MPI_ENABLED
-    pyarb::register_mpi(m);
-    #endif
+    pyarb::register_profiler(m);
     pyarb::register_recipe(m);
     pyarb::register_schedules(m);
     pyarb::register_simulation(m);
     pyarb::register_spike_handling(m);
+    
+    #ifdef ARB_MPI_ENABLED
+    pyarb::register_mpi(m);
+    #endif
 }
