d9c93856acf35cd4982147b5b7e124959a8b8f61
Alexander Peyser <a.peyser@fz-juelich.de>
2019-09-24 14:30:09 +0200

    extend comments, add auto return to apply

--- arbor/communication/communicator.hpp
+++ arbor/communication/communicator.hpp
@@ -83,7 +83,8 @@ private:
 
     // forward declaration of prefetching types for prefetch.hpp
     using prefetched_connections = prefetch::elements<
-        std::vector<pse_vector>::iterator, // the queue* that we went to prefetch
+        prefetch::write,
+        std::vector<pse_vector>::iterator,  // the queue* that we went to prefetch
         std::vector<spike>::const_iterator, // spike* or begin()
         std::vector<spike>::const_iterator, // nothing or end()
         std::vector<connection>::iterator   // connection*
--- arbor/util/prefetch.hpp
+++ arbor/util/prefetch.hpp
@@ -12,32 +12,52 @@ auto get_pointer(P& prefetched) {
     return &*prefetched;
 }
 
+/////////////////////////////
+// Prefetch as read or write
+// constants for __builtin_prefetch
+enum prefetch_mode {
+    read = 0,
+    write = 1
+};
+
+///////////////////////////////////////////////////////////////////
+// element<prefetch_mode, prefetch-pointer, payload-pointers...> //
+///////////////////////////////////////////////////////////////////
+//
 // pass only pointer-like things in here!
 // Makes templates much simpler -- we're just storing a cut
 // through arrays
-template<typename P, typename ... Types>
+//
+// Internal element of elements
+//
+template<prefetch_mode M, typename P, typename ... Types>
 class element: public std::tuple<P, Types...> {
 public:
     using parent = std::tuple<P, Types...>;
     static constexpr std::size_t size = std::tuple_size<parent>();
+    static constexpr prefetch_mode m = M;
     
     element(P p, Types... args): parent{p, args...} {prefetch();}
     element() = default;
 
     template<typename F>
-    void apply(F f) {
-        apply(std::forward<F>(f), std::make_index_sequence<size>{});
+    auto apply(F f) {
+        return apply(std::forward<F>(f), std::make_index_sequence<size>{});
     }
 
 private:
-    void prefetch() {__builtin_prefetch(get_pointer(std::get<0>(*this)), 1);}
+    void prefetch() {__builtin_prefetch(get_pointer(std::get<0>(*this)), m);}
 
     template<typename F, size_t... I>
-    void apply(F f, std::index_sequence<I...>) {
-        std::forward<F>(f)(std::get<I>(*this)...);
+    auto apply(F f, std::index_sequence<I...>) {
+        return std::forward<F>(f)(std::get<I>(*this)...);
     }
 };
 
+//////////////////////////////////////////////////////////////////////////////////////
+//////// elements<prefetch_mode, prefetch-pointer, payload-pointers...> //////////////
+//////////////////////////////////////////////////////////////////////////////////////
+//
 // a list of addresses to prefetch, and associated address
 // the concept is that you continously `add` prefetch address from
 // an array and their cuts through other arrays until fill
@@ -57,10 +77,26 @@ private:
 //   e.process([] (auto&& a_, auto&& b_, auto&& c_) { /* handle left over */
 //       a->do_domething(b_, c_);
 //   });
+//
+// Prefetching is actually called when the `element` is constructed from the add arguments, where the
+// first argument has prefetch::get_pointer applied giving an address to call
+// on __builtin_prefetch
+//
+// The "hard bit" is determining the range of good sizes for the capacity.
+// How many elements should we add before calling e.process?
+// Not too many or we will be pushing things out of the cache
+// and not too few or we'll hit the function application before
+// the data has arrived
 
-template<typename P, typename ... Types> 
-struct elements: public std::vector<element<P, Types...>> {
-    using element_type = element<P, Types...>;
+//
+// prefetch_mode M = read or write, do we prefetch with a read or write expection?
+// P = type of prefetch (pointer-like object)
+// Types... =  payload types for calling functions on (pointer-like objects)
+// process is applied on f: f(P&&, Types&&...)
+//
+template<prefetch_mode M, typename P, typename ... Types> 
+struct elements: public std::vector<element<M, P, Types...>> {
+    using element_type = element<M, P, Types...>;
     using parent = std::vector<element_type>;
 
     const std::size_t n;
@@ -82,13 +118,8 @@ struct elements: public std::vector<element<P, Types...>> {
         clear();
     }
 
-    bool not_full() const {
-        return size() < n;
-    }
-
-    bool full() const {
-        return ! not_full();
-    }
+    bool not_full() const {return size() < n;}
+    bool full() const {return ! not_full();}
 
     using parent::reserve;
     using parent::clear;
