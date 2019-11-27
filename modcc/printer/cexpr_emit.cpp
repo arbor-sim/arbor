860f15abfc3adc84bd9c7fe7b558d630ae381bb0
Nora Abi Akar <nora.abiakar@gmail.com>
2019-11-27 17:53:27 +0100

    Modcc: parse `fabs` instead of `abs` (#907)
    Also emit "abs" in cexpr_emit.cpp because the simd backend implements abs
    
    Fixes #887

--- modcc/printer/cexpr_emit.cpp
+++ modcc/printer/cexpr_emit.cpp
@@ -52,7 +52,7 @@ void CExprEmitter::visit(UnaryExpression* e) {
         {tok::cos,     "cos"},
         {tok::sin,     "sin"},
         {tok::log,     "log"},
-        {tok::abs,     "fabs"},
+        {tok::abs,     "abs"},
         {tok::exprelr, "exprelr"}
     };
 
