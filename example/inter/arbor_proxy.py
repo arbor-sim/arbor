3a68ffe64a9447b303c7bc25e264a7446d92a94b
Wouter Klijn <w.klijn@fz-juelich.de>
2019-12-05 12:21:54 +0100

    Slowly move to comm_info implementation

--- example/inter/arbor_proxy.py
+++ example/inter/arbor_proxy.py
@@ -32,24 +32,6 @@ def print_spike_array_d (to_print , force = False):
     sys.stdout.flush() 
 
 
-#####################################################################
-# MPI configuration
-world = MPI.COMM_WORLD
-rank = world.Get_rank()
-
-# Need to formalize this nicer, currently the coupling is hardcoded
-comm = MPI.COMM_WORLD.Split(1) 
-
-
-########################################################################
-# Config
-num_arbor_cells = 100;
-min_delay = 10;
-duration = 100;
-
-arbor_root = 0
-nest_root = 1
-
 # we are debuggin MPI code, force a print after each print statement
 # Gather function
 def gather_spikes(spikes, comm):
@@ -76,16 +58,79 @@ def gather_spikes(spikes, comm):
 
     return receive_spikes_array.view('uint32,uint32, float32')
     
+class comm_information(): 
+    def __init__(self, is_arbor):
+        self.is_arbor = is_arbor
+        self.is_nest = not is_arbor
+
+        self.global_rank = MPI.COMM_WORLD.rank
+        self.global_size = MPI.COMM_WORLD.size
+
+        # split MPI_COMM_WORLD: all arbor go into split 1
+        # TODO: with N>2 simulators self whole function needs to be cleaned up
+        color = 1 if is_arbor else 0
+        self.world = MPI.COMM_WORLD
+        self.comm = self.world.Split(color)
+
+        local_size  = self.comm.size
+        self.local_rank = self.comm.rank;
+
+        self.arbor_size = local_size if self.is_arbor else self.global_size - local_size
+        self.nest_size = self.global_size - self.arbor_size
+
+        input = np.array([self.global_rank], dtype=np.int32) 
+        local_ranks = np.zeros(local_size, dtype=np.int32)
+       
+        self.comm.Allgather(input, local_ranks)
+        local_ranks.sort()
+
+        # look for first non concecutive entry. self would occur if we create the ranks interleaved
+        def first_missing(np_array):
+            for idx in range(np_array.size-1):
+                
+                if not (np_array[idx+1] - np_array[idx] is 1):
+                    return np_array[idx] + 1
+            # Default the last rank plus one
+            return np_array[-1]+1
+
+        if (self.is_arbor):
+            self.arbor_root = local_ranks[0]
+            self.nest_root = first_missing(local_ranks) if self.arbor_root is 0 else 0
+        else: 
+            self.nest_root = local_ranks[0]
+            self.arbor_root = first_missing(local_ranks) if self.nest_root is 0 else 0
+
+
+    def __str__(self):
+        return str("global ( rank: " + str(self.global_rank) + ", size: " + str(self.global_size) + "\n" +
+             "local rank " + str(self.local_rank) + "\n" +
+             "self is arbor\n" if self.is_arbor else "self is nest\n" +
+             "arbor (root: " + str(self.arbor_root) + ", size: " + str(self.arbor_size) + ")\n" +
+             +"nest (root: " + str(self.nest_root) + ", size: " + str(self.nest_size) + ")\n")
+
+#####################################################################
+# MPI configuration
+comm_info = comm_information(True)
+
+########################################################################
+# Config
+num_arbor_cells = 100;
+min_delay = 10;
+duration = 100;
+
+arbor_root = 0
+nest_root = 1
+
 
 ########################################################################
 # handshake #1: communicate the number of cells between arbor and nest
 # send nr of arbor cells
 data_array = np.array([num_arbor_cells],dtype=np.int32)  
-world.Bcast(data_array, arbor_root) 
+comm_info.world.Bcast(data_array, arbor_root) 
 
 #Receive nest cell_nr
 data_array = np.array([0],dtype=np.int32)  
-world.Bcast(data_array, root=nest_root) #Use Bcast allows setting of received size
+comm_info.world.Bcast(data_array, root=nest_root) #Use Bcast allows setting of received size
 
 num_nest_cells = data_array[0] 
 num_total_cells = num_nest_cells + num_arbor_cells
@@ -100,11 +145,11 @@ print_d("num_arbor_cells: " + str(num_arbor_cells) + " " +
 # first send the arbor delays
 arb_com_time = min_delay / 2.0
 data_array = np.array([arb_com_time],dtype=np.float32)  
-world.Bcast(data_array, arbor_root)
+comm_info.world.Bcast(data_array, arbor_root)
 
 # receive the nest delays 
 data_array = np.array([0],dtype=np.float32)  
-world.Bcast(data_array, nest_root)
+comm_info.world.Bcast(data_array, nest_root)
 nest_com_time = data_array[0]
 print_d("nest_com_time: " + str(nest_com_time))
 
@@ -123,7 +168,7 @@ if (steps * delta < duration):
 ###############################################################
 # Handshake #3: steps
 data_array = np.array([steps], dtype=np.int32)  
-world.Bcast(data_array, arbor_root)
+comm_info.world.Bcast(data_array, arbor_root)
 
 print_d("delta: " + str(delta) + ", " +
         "sim_duration: " + str(duration) + ", " +
@@ -136,7 +181,7 @@ for step in range(steps+1):
 
     # We are sending no spikes from arbor to nest. Create a array with size zero with correct type
     data_array = np.zeros(0, dtype='uint32, uint32, float32')  
-    gather_spikes(data_array, world)
+    gather_spikes(data_array, comm_info.world)
 
 print_d("Reached arbor_proxy.py end")
 
