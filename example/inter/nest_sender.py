a2c541f823b3ea67f16be289b768bd35856d3844
Wouter Klijn <w.klijn@fz-juelich.de>
2019-12-06 12:53:55 +0100

    Incorrectly refactored the python wrapper. Tested and runs correct now

--- example/inter/nest_sender.py
+++ example/inter/nest_sender.py
@@ -12,30 +12,26 @@ comm = MPI.COMM_WORLD.Split(0) # is nest
 
 print("Getting nest")
 import nest
+
+
 nest.set_communicator(comm)
 nest.SetKernelStatus({'recording_backends': {'arbor':{}}})
 
 print("Building network")
-
-# Create a spike generator
 pg = nest.Create('poisson_generator', params={'rate': 10.0})
 
-# Poisson_generators are special, we need a parrot to forward the spikes to be able
-# to record
-g = nest.Create('poisson_generator', params={'rate': 10.0})
+# We cannot directly record from poisson_generator due to implementation details
+# Create a parrot and connect the recorder to that
+parrots = nest.Create('parrot_neuron', 100)
 nest.Connect(pg, parrots)
 
-# can now record from the parrots.
 sd2 = nest.Create('spike_detector', params={"record_to": "arbor"})			  
 nest.Connect(parrots, sd2)
 
 status = nest.GetKernelStatus()
 print('min_delay: ', status['min_delay'], ", max_delay: ", status['max_delay'])
-
 print("Simulate")
 sys.stdout.flush()
 
 nest.Simulate(100.0)
-
 print("Done")
-sys.stdout.flush()
