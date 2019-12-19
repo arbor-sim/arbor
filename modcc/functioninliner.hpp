a887aa5757a3face4b797536b391270cbacc974e
abiakarn <nora.abiakar@gmail.com>
2019-12-19 15:06:38 +0100

    add missing file

--- modcc/functioninliner.hpp
+++ modcc/functioninliner.hpp
@@ -35,8 +35,8 @@ public:
 private:
     std::string inlining_func_, calling_func_;
     expression_ptr lhs_;
-    std::unordered_map<std::string, expression_ptr> call_arg_map_;
-    std::unordered_map<std::string, expression_ptr> local_arg_map_;
+    std::map<std::string, expression_ptr> call_arg_map_;
+    std::map<std::string, expression_ptr> local_arg_map_;
     scope_ptr scope_;
 
     // Tracks whether the return value of a function has been set
@@ -62,48 +62,4 @@ protected:
         inlining_executed_ = false;
         BlockRewriterBase::reset();
     }
-};
-
-class VariableReplacer : public Visitor {
-
-public:
-
-    VariableReplacer(std::string const& source, std::string const& target)
-    :   source_(source),
-        target_(target)
-    {}
-
-    void visit(Expression *e)           override;
-    void visit(UnaryExpression *e)      override;
-    void visit(BinaryExpression *e)     override;
-    void visit(NumberExpression *e)     override {};
-
-    ~VariableReplacer() {}
-
-private:
-
-    std::string source_;
-    std::string target_;
-};
-
-class ValueInliner : public Visitor {
-
-public:
-
-    ValueInliner(std::string const& source, long double value)
-    :   source_(source),
-        value_(value)
-    {}
-
-    void visit(Expression *e)           override;
-    void visit(UnaryExpression *e)      override;
-    void visit(BinaryExpression *e)     override;
-    void visit(NumberExpression *e)     override {};
-
-    ~ValueInliner() {}
-
-private:
-
-    std::string source_;
-    long double value_;
 };
