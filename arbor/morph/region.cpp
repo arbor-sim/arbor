2f6e3b2d7125932a983296f5a6343ceb9640e70a
Nora Abi Akar <nora.abiakar@gmail.com>
2020-01-22 13:09:53 +0100

    Remove covered zero-regions from region intersections (#934)
    Stop region intersection from returning extra zero length cables if the location it describes is in the cover of another region in the `cable_list`.

--- arbor/morph/region.cpp
+++ arbor/morph/region.cpp
@@ -132,6 +132,44 @@ mcable_list remove_cover(mcable_list cables, const morphology& m) {
     return merge(cables);
 }
 
+// Remove zero-length regions from a sorted cable_list that are in the cover of another region in the list.
+mcable_list remove_covered_points(mcable_list cables, const morphology& m) {
+    struct branch_index_pair {
+        msize_t bid;
+        unsigned lid;
+    };
+    std::vector<branch_index_pair> end_branches;
+    std::vector<unsigned> erase_indices;
+
+    for (unsigned i = 0; i < cables.size(); i++) {
+        auto c = cables[i];
+        // Save zero length cables at the distal end of a branch
+        // Or at the proximal end of the soma
+        if ((c.prox_pos==1 && c.dist_pos==1) ||
+            (c.prox_pos==0 && c.dist_pos==0)) {
+            end_branches.push_back({c.branch, i});
+        }
+        // Look for branches that are children of the cables saved in end_branches
+        else if (c.prox_pos==0) {
+            auto parent = m.branch_parent(c.branch);
+            if (parent==mnpos) parent = 0;
+
+            auto it = std::find_if(end_branches.begin(), end_branches.end(),
+                                   [&](branch_index_pair& p) { return p.bid==parent;});
+
+            if (it!=end_branches.end()) {
+                erase_indices.push_back((*it).lid);
+                end_branches.erase(it);
+            }
+        }
+    }
+    util::sort(erase_indices);
+    for (auto it = erase_indices.rbegin(); it != erase_indices.rend(); it++) {
+        cables.erase(cables.begin() + *it);
+    }
+
+    return cables;
+}
 
 // Empty region.
 
@@ -324,7 +362,7 @@ mcable_list thingify_(const reg_and& P, const mprovider& p) {
         at_end = it.first==end.first || it.second==end.second;
     }
 
-    return remove_cover(L, m);
+    return remove_covered_points(remove_cover(L, m), m);
 }
 
 std::ostream& operator<<(std::ostream& o, const reg_and& x) {
