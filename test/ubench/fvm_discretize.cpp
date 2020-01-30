a7a6eb04f7117c05b6604b1ba0d103f0ca70af4a
Sam Yates <halfflat@gmail.com>
2020-01-30 15:02:42 +0100

    Replace unnecessary cv_policy copy in fvm_discretize.
    Also: add a couple more cases to fvm_discretize ubench.

--- test/ubench/fvm_discretize.cpp
+++ test/ubench/fvm_discretize.cpp
@@ -50,6 +50,19 @@ void run_cv_geom_every_sample(benchmark::State& state) {
     }
 }
 
+void run_cv_geom_explicit(benchmark::State& state) {
+    auto gdflt = neuron_parameter_defaults;
+
+    cable_cell c(from_swc(SWCFILE));
+
+    while (state.KeepRunning()) {
+        auto ends = cv_policy_every_sample().cv_boundary_points(c);
+        auto ends2 = cv_policy_explicit(std::move(ends)).cv_boundary_points(c);
+
+        benchmark::DoNotOptimize(cv_geometry_from_ends(c, ends2));
+    }
+}
+
 void run_discretize(benchmark::State& state) {
     auto gdflt = neuron_parameter_defaults;
     const std::size_t ncv_per_branch = state.range(0);
@@ -73,10 +86,26 @@ void run_discretize_every_sample(benchmark::State& state) {
     }
 }
 
+void run_discretize_explicit(benchmark::State& state) {
+    auto gdflt = neuron_parameter_defaults;
+
+    cable_cell c(from_swc(SWCFILE));
+
+    while (state.KeepRunning()) {
+        auto ends = cv_policy_every_sample().cv_boundary_points(c);
+        c.default_parameters.discretization = cv_policy_explicit(std::move(ends));
+
+        benchmark::DoNotOptimize(fvm_cv_discretize(c, gdflt));
+    }
+}
+
 BENCHMARK(run_cv_geom)->RangeMultiplier(2)->Range(1,32)->Unit(benchmark::kMicrosecond);
 BENCHMARK(run_discretize)->RangeMultiplier(2)->Range(1,32)->Unit(benchmark::kMicrosecond);
 
 BENCHMARK(run_cv_geom_every_sample)->Unit(benchmark::kMillisecond);
 BENCHMARK(run_discretize_every_sample)->Unit(benchmark::kMillisecond);
+
+BENCHMARK(run_cv_geom_explicit)->Unit(benchmark::kMillisecond);
+BENCHMARK(run_discretize_explicit)->Unit(benchmark::kMillisecond);
 
 BENCHMARK_MAIN();
