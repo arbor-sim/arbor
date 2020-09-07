137ba67d242b2a3315f399dd4e864940414e1550
bcumming <bcumming@cscs.ch>
2020-09-07 22:43:21 +0200

    finalise either -> variant

--- python/s_expr.cpp
+++ python/s_expr.cpp
@@ -1,5 +1,3 @@
-#include <iostream>
-
 #include <cctype>
 #include <cstring>
 #include <string>
@@ -298,32 +296,26 @@ bool test_identifier(const char* in) {
 //
 
 bool s_expr::is_atom() const {
-    std::cout << "testing atom\n";
     return state.index()==0;
 }
 
 const token& s_expr::atom() const {
-    std::cout << "testing atom\n";
     return state.get<0>();
 }
 
 const s_expr& s_expr::head() const {
-    std::cout << "getting head\n";
     return state.get<1>().head.get();
 }
 
 const s_expr& s_expr::tail() const {
-    std::cout << "getting tail\n";
     return state.get<1>().tail.get();
 }
 
 s_expr& s_expr::head() {
-    std::cout << "getting head\n";
     return state.get<1>().head.get();
 }
 
 s_expr& s_expr::tail() {
-    std::cout << "getting tail\n";
     return state.get<1>().tail.get();
 }
 
@@ -367,9 +359,7 @@ s_expr parse(lexer& L) {
     using namespace std::string_literals;
 
     s_expr node;
-    std::cout << "STARTING: " << node << std::endl;
     auto t = L.current();
-    std::cout << "da lexah is priiiiiiimed" << std::endl;
 
     if (t.kind==tok::lparen) {
         t = L.next();
@@ -403,7 +393,6 @@ s_expr parse(lexer& L) {
         return token{t.column, tok::error, "Missing opening parenthesis'('."};;
     }
 
-    std::cout << "FINISHED: " << node << std::endl;
     return node;
 }
 
