189222eb01d1887970368358e961c55886a23cdc
Brent Huisman <brent@huisman.pl>
2020-09-29 17:33:09 +0200

    * Fix assorted typos

--- python/recipe.cpp
+++ python/recipe.cpp
@@ -29,7 +29,7 @@ arb::util::unique_any py_recipe_shim::get_cell_description(arb::cell_gid_type gi
                 "Python error already thrown");
 }
 
-arb::probe_info cable_loc_probe(std::string kind, arb::cell_member_type id, arb::mlocation loc) {
+arb::probe_info cable_loc_probe(std::string kind, arb::mlocation loc) {
     if (kind == "voltage") {
         return arb::cable_probe_membrane_voltage{loc};
     }
@@ -170,9 +170,9 @@ void register_recipe(pybind11::module& m) {
 
     // Probes
     m.def("cable_probe", &cable_loc_probe,
-        "Description of a probe at a location on a cable cell with id available for monitoring data of kind "\
+        "Description of a probe at a location available for monitoring data of kind "\
         "where kind is one of 'voltage' or 'ionic current density'.",
-        "kind"_a, "id"_a, "location"_a);
+        "kind"_a, "location"_a);
 
     pybind11::class_<arb::probe_info> probe(m, "probe");
     probe
