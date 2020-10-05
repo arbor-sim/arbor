4339ffed28389106b78198e735965abafac80f39
Sam Yates <halfflat@gmail.com>
2020-10-05 08:34:50 +0200

    Tighten SWC requirements, comments. (#1170)
    * Remove contiguous numbering requirement from 'strict' mode SWC
    parsing, as it is not necessary for correct interpretation as a segment
    tree.
    * Describe in the swcio.hpp header how (strict) swc data is interpreted
    as a segment tree.
    * Demonstrate in unit tests that out-of-order and non-contiguously
    numbered records are valid for `parse_swc`.
    * Add unit test for `as_segment_tree`.

--- arbor/swcio.cpp
+++ arbor/swcio.cpp
@@ -33,10 +33,6 @@ swc_duplicate_record_id::swc_duplicate_record_id(int record_id):
     swc_error("duplicate SWC sample id", record_id)
 {}
 
-swc_irregular_id::swc_irregular_id(int record_id):
-    swc_error("SWC record id not numbered consecutively from 1", record_id)
-{}
-
 swc_spherical_soma::swc_spherical_soma(int record_id):
     swc_error("SWC with spherical somata are not supported", record_id)
 {}
@@ -92,10 +88,6 @@ static std::vector<swc_record> sort_and_validate_swc(std::vector<swc_record> rec
 
         if (r.parent_id>=r.id) {
             throw swc_record_precedes_parent(r.id);
-        }
-
-        if (mode==swc_mode::strict && r.id != (int)i+1) {
-            throw swc_irregular_id(r.id);
         }
 
         if (!seen.insert(r.id).second) {
