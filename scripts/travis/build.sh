4f0ea9f1ac53c869fb56c0e824540d99734beefd
abiakarn <nora.abiakar@gmail.com>
2020-10-06 13:10:58 +0200

    python messages

--- scripts/travis/build.sh
+++ scripts/travis/build.sh
@@ -110,6 +110,7 @@ if [[ "${WITH_PYTHON}" == "true" ]]; then
     make pyarb -j4                                                                              || error "building pyarb"
     progress "Python unit tests"
     python$PY $python_path/test/unit/runner.py -v2                                              || error "running python unit tests (serial)"
+    progress "Python examples"
     python$PY $python_path/example/network_ring.py                                              || error "running python network_ring example"
     python$PY $python_path/example/single_cell_model.py                                         || error "running python single_cell_model example"
     python$PY $python_path/example/single_cell_multi_branch.py                                  || error "running python single_cell_multi_branch example"
