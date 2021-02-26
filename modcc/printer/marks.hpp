9a93c9e9dc460c36b614f701a8602d983042db78
Thorsten Hater <24411438+thorstenhater@users.noreply.github.com>
2021-02-26 12:11:09 +0100

    Add tracing macros.

---
+++ modcc/printer/marks.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#define MARK_REGIONS 1
+#ifdef MARK_REGIONS
+#define ENTER(stream) (stream) << " /* " << __FUNCTION__ << ":enter*/ "
+#define EXIT(stream)  (stream) << " /* " << __FUNCTION__ << ":exit*/ "
+#define ENTERM(stream, m) (stream) << " /* " << __FUNCTION__ << ":" << m << ":enter */ "
+#define EXITM(stream, m)  (stream) << " /* " << __FUNCTION__ << ":" << m << ":exit */ "
+#else
+#define ENTER(stream)
+#define EXIT(stream)
+#define ENTERM(stream, m)
+#define EXITM(stream, m)
+#endif
