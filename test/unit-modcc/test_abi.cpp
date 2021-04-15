e1fa2f8ba27f30278257009b4f5a53226d70d949
Thorsten Hater <24411438+thorstenhater@users.noreply.github.com>
2021-04-15 11:17:12 +0200

    Add build script.

---
+++ test/unit-modcc/test_abi.cpp
@@ -0,0 +1,11 @@
+#include "../test/gtest.h"
+
+#include <arbor/mechanism_abi.h>
+
+#include <backends/multicore/mechanism.hpp>
+
+using namespace std::string_literals;
+
+TEST(abi, multicore_initialisation) {
+
+}
