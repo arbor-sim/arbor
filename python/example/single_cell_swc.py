e536cad4816e1c28d12d837d0d2b5ac02369b6d9
Sebastian Schmitt <sebastian.schmitt@kip.uni-heidelberg.de>
2021-04-21 15:38:52 +0200

    Update comment about example swc file (#1490)

--- python/example/single_cell_swc.py
+++ python/example/single_cell_swc.py
@@ -17,7 +17,7 @@ import pandas, seaborn
 import sys
 
 # Load a cell morphology from an swc file.
-# Example present here: morph.swc
+# Example present here: single_cell_detailed.swc
 if len(sys.argv) < 2:
     print("No SWC file passed to the program")
     sys.exit(0)
@@ -30,7 +30,7 @@ defs = {'soma': '(tag 1)',  # soma has tag 1 in swc files.
         'axon': '(tag 2)',  # axon has tag 2 in swc files.
         'dend': '(tag 3)',  # dendrites have tag 3 in swc files.
         'root': '(root)',   # the start of the soma in this morphology is at the root of the cell.
-        'stim_site': '(location 0 0.5)', # site for the stimulus, in the middle of branch 1.
+        'stim_site': '(location 0 0.5)', # site for the stimulus, in the middle of branch 0.
         'axon_end': '(restrict (terminal) (region "axon"))'} # end of the axon.
 labels = arbor.label_dict(defs)
 
