73c83d2075f66889d649ac5eab6d03f8fad74908
Thorsten Hater <24411438+thorstenhater@users.noreply.github.com>
2021-06-02 15:12:48 +0200

    Run issue through CI.

--- test/unit/test_simulation.cpp
+++ test/unit/test_simulation.cpp
@@ -41,7 +41,7 @@ static auto n_thread_context(unsigned n_thread) {
 struct null_recipe: arb::recipe {
     arb::cable_cell_global_properties properties;
     null_recipe() { properties.default_parameters = arb::neuron_parameter_defaults; }
-    arb::cell_size_type num_cells() const override { return 1; }
+    arb::cell_size_type num_cells() const override { return 20000; }
     arb::cell_kind get_cell_kind(arb::cell_gid_type) const override { return arb::cell_kind::cable; }
     arb::util::unique_any get_cell_description(arb::cell_gid_type) const override { return {arb::cable_cell{}}; }
     std::vector<arb::probe_info> get_probes(arb::cell_gid_type gid) const override { return {}; }
