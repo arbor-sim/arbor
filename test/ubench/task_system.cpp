8afe25940130246b4df3bb435e7b449bf9711760
Nora Abi Akar <nora.abiakar@gmail.com>
2021-06-21 12:10:32 +0300

    Fix stack overflow bug in the task system (#1583)
    * Turn each task queue in the task system into a finite set of queues of increasing priority. The number of queues is a compile-time constant, currently two.
    * When a task of priority higher than that of the highest priority queue is scheduled, execute it synchronously.
    * When scheduling tasks in a task group, use a priority one higher than that of the calling task; when waiting on the task group, work only on tasks with this priority or higher. This is sufficient to avoid the deep recursion issue seen in issue #1570.
    
    Fixes #1570.
    Co-authored-by: Sam Yates <yates@cscs.ch>

--- test/ubench/task_system.cpp
+++ test/ubench/task_system.cpp
@@ -21,12 +21,31 @@ void run(unsigned long us_per_task, unsigned tasks, threading::task_system* ts) 
             [&](unsigned i){std::this_thread::sleep_for(duration);});
 }
 
+void run_nested(unsigned long us_per_task, unsigned tasks, threading::task_system* ts) {
+    auto duration = std::chrono::microseconds(us_per_task);
+    arb::threading::parallel_for::apply(0, tasks, ts, [&](unsigned i) {
+       arb::threading::parallel_for::apply(0, 1, ts, [&](unsigned i) {
+          std::this_thread::sleep_for(duration);});});
+}
+
+void task_test_nested(benchmark::State& state) {
+    const unsigned us_per_task = state.range(0);
+    arb::threading::task_system ts;
+    const auto nthreads = ts.get_num_threads();
+    const unsigned total_us = 1000000;
+    const unsigned num_tasks = nthreads*total_us/us_per_task;
+
+    while (state.KeepRunning()) {
+        run_nested(us_per_task, num_tasks, &ts);
+    }
+}
+
 void task_test(benchmark::State& state) {
     const unsigned us_per_task = state.range(0);
     arb::threading::task_system ts;
     const auto nthreads = ts.get_num_threads();
-    const unsigned us_per_s = 1000000;
-    const unsigned num_tasks = nthreads*us_per_s/us_per_task;
+    const unsigned total_us = 1000000;
+    const unsigned num_tasks = nthreads*total_us/us_per_task;
 
     while (state.KeepRunning()) {
         run(us_per_task, num_tasks, &ts);
@@ -34,10 +53,11 @@ void task_test(benchmark::State& state) {
 }
 
 void us_per_task(benchmark::internal::Benchmark *b) {
-    for (auto ncomps: {100, 250, 500, 1000, 10000}) {
-        b->Args({ncomps});
+    for (auto us_per_task: {10, 100, 250, 500, 1000, 10000}) {
+        b->Args({us_per_task});
     }
 }
 
 BENCHMARK(task_test)->Apply(us_per_task);
+BENCHMARK(task_test_nested)->Apply(us_per_task);
 BENCHMARK_MAIN();
