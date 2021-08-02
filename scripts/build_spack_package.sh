28f8e413fc754f71b5d0dd69e763737fcfdbeece
Sebastian Schmitt <sebastian.schmitt@kip.uni-heidelberg.de>
2021-08-02 14:14:53 +0200

    Cache Spack for GHA

--- scripts/build_spack_package.sh
+++ scripts/build_spack_package.sh
@@ -46,11 +46,18 @@ case $SPACK_VERSION in
         echo "SPACK_VERSION" must be \"latest_release\" or \"develop\"
         exit 1
 esac
+
+mkdir ~/.spack
+cp $ARBOR_DIR/spack/config.yaml ~/.spack
+
 source $SPACK_DIR/share/spack/setup-env.sh
 spack repo create $SPACK_CUSTOM_REPO
 
 mkdir -p $SPACK_CUSTOM_REPO/packages/arbor
 spack repo add $SPACK_CUSTOM_REPO
+
+# to make use of the cached installations
+spack reindex
 
 cp $ARBOR_DIR/spack/package.py $SPACK_CUSTOM_REPO/packages/arbor
 cd $ARBOR_DIR
