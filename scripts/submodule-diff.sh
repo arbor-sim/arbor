3e08eeb90e8fac6df95fe3e0933d183798ddb054
Brent Huisman <brent@huisman.pl>
2021-10-28 12:58:51 +0200

    update check no longer destructive

--- scripts/submodule-diff.sh
+++ scripts/submodule-diff.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
 git submodule foreach 'git describe HEAD --tags' > pre.log
-git submodule update --remote
-git submodule foreach 'git checkout `git describe --abbrev=0 --tags`'
-git submodule foreach 'git describe HEAD --tags' > post.log
+git submodule foreach 'git fetch'
+git submodule foreach 'git describe `git log --branches -1 --pretty=format:"%H"` --tags --abbrev=0' > post.log
 if diff pre.log post.log ; then
     echo "No submodule updates found"
 else
     diff pre.log post.log -U 10000 > diff.log
 fi
-echo "Last retval must be zero for GA to pass..."
+rm pre.log && rm post.log
