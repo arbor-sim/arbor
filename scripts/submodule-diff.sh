d35ad24f9704d276529dcf23ddea113d57208fc9
Brent Huisman <brent@huisman.pl>
2021-11-30 11:54:54 +0100

    some review

--- scripts/submodule-diff.sh
+++ scripts/submodule-diff.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-git submodule foreach 'git describe HEAD --tags' > pre.log
+git submodule foreach 'git describe HEAD --tags' > pre.log | tee pre.log
 git submodule foreach 'git fetch'
-git submodule foreach 'git describe `git log --branches -1 --pretty=format:"%H"` --tags --abbrev=0' > post.log
+git submodule foreach 'git describe `git log --branches -1 --pretty=format:"%H"` --tags --abbrev=0' > post.log | tee post.log
 if diff pre.log post.log ; then
     echo "No submodule updates found"
 else
     diff pre.log post.log -U 10000 > diff.log
 fi
-rm pre.log && rm post.log
+rm pre.log
+rm post.log
