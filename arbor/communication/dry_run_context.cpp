0636bbfe8a26bac53eb3ac8d85d32ccc83e590ee
Nora Abi Akar <nora.abiakar@gmail.com>
2022-01-06 15:10:47 +0100

    Bug fix: properly partition networks containing one-sided gap-junction connections (#1774)
    Replace the `partition_load_balance` function assumed that gap-junction connections are always double-sided (i.e. if gid `x` has a gap-junction connection from peer gid `y`, then gid `y` must also have a gap-junction connection from peer gid `x`). This used to be a requirement that was checked prior to #1682. Since then, single-sided gap-junctions are in principle allowed, but `partition_load_balance` still operates under the bidirectional gap-junction connection assumption resulting in some gids being present in multiple cell-groups. 
    This PR modifies the `partition_load_balance` function to do the following: 
    1. On each rank, generate a gj_connection list per cell. (gj_connection list is a vector of gids that have an outgoing connection ending at the cell under consideration). 
    2. On each rank, gather all the gj_connection lists for each cell in the network. 
    3. On each rank, modify the global list of gj_connections in the network to make all gj_connections bidirectional. 
    4. On each rank, use the global list of gj_connections in the network to partition the graph. 
    
    Fixes #1767.

--- arbor/communication/dry_run_context.cpp
+++ arbor/communication/dry_run_context.cpp
@@ -70,6 +70,26 @@ struct dry_run_context_impl {
         return gathered_vector<cell_gid_type>(std::move(gathered_gids), std::move(partition));
     }
 
+    std::vector<std::vector<cell_gid_type>>
+    gather_gj_connections(const std::vector<std::vector<cell_gid_type>> & local_connections) const {
+        auto local_size = local_connections.size();
+        std::vector<std::vector<cell_gid_type>> global_connections;
+        global_connections.reserve(local_size*num_ranks_);
+
+        for (unsigned i = 0; i < num_ranks_; i++) {
+            util::append(global_connections, local_connections);
+        }
+
+        for (unsigned i = 0; i < num_ranks_; i++) {
+            for (unsigned j = i*local_size; j < (i+1)*local_size; j++){
+                for (auto& conn_gid: global_connections[j]) {
+                    conn_gid += num_cells_per_tile_*i;
+                }
+            }
+        }
+        return global_connections;
+    }
+
     cell_label_range gather_cell_label_range(const cell_label_range& local_ranges) const {
         cell_label_range global_ranges;
         for (unsigned i = 0; i < num_ranks_; i++) {
