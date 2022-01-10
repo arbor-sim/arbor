eb211c2c1c9b859721772a42fd1aeececa44840b
Brent Huisman <brent@huisman.pl>
2022-01-10 15:23:52 +0100

    add bundled key to config()

--- python/config.cpp
+++ python/config.cpp
@@ -44,6 +44,11 @@ pybind11::dict config() {
 #else
     dict[pybind11::str("neuroml")] = pybind11::bool_(false);
 #endif
+#ifdef ARB_BUNDLED_ENABLED
+    dict[pybind11::str("bundled")] = pybind11::bool_(true);
+#else
+    dict[pybind11::str("bundled")] = pybind11::bool_(false);
+#endif
     dict[pybind11::str("version")] = pybind11::str(ARB_VERSION);
     dict[pybind11::str("source")]  = pybind11::str(ARB_SOURCE_ID);
     dict[pybind11::str("arch")]    = pybind11::str(ARB_ARCH);
