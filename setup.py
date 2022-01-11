faa4395d3e2564183dd4648c32006ec354a7c017
Brent Huisman <brent@huisman.pl>
2022-01-11 14:15:04 +0100

    clearer cmake_args setting

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from sys import executable as python
 from skbuild import setup
-import os
+import os,platform
 
 # Hard coded options, because scikit-build does not do build options.
 # Override by instructing CMAKE, e.g.:
@@ -14,6 +14,20 @@ with_nml   = True
 use_libs   = True
 build_type = 'Release' # this is ok even for debugging, as we always produce info
 
+cmake_args = ['-DARB_WITH_PYTHON=on',
+                f'-DPYTHON_EXECUTABLE={python}',
+                f'-DARB_WITH_MPI={with_mpi}',
+                f'-DARB_VECTORIZE={with_vec}'
+                f'-DARB_ARCH={arch}',
+                f'-DARB_GPU={with_gpu}',
+                f'-DARB_WITH_NEUROML={with_nml}',
+                f'-DARB_USE_BUNDLED_LIBS={use_libs}',
+                f'-DCMAKE_BUILD_TYPE={build_type}']
+# For cibuildwheel builds, which build the Python wheels meant for PyPI, we use the static library of libxml2.
+# This gets us around this issue with skbuild: https://github.com/pypa/auditwheel/issues/363
+if 'linux' in str(platform.system()).lower() and os.environ.get('CIBUILDWHEEL') is not None:
+    cmake_args.append(f'-DARB_LIBXML=/usr/lib64/libxml2.a')
+
 # Find our dir; *should* be the arbor checkout
 here = Path(__file__).resolve().parent
 # Read version file
@@ -24,36 +38,26 @@ with open(here / 'python' / 'readme.md', encoding='utf-8') as fd:
     long_description = fd.read()
 
 setup(name='arbor',
-      version=arbor_version,
-      python_requires='>=3.6',
-      install_requires=['numpy'],
-      setup_requires=[],
-      zip_safe=False,
-      cmake_args=['-DARB_WITH_PYTHON=on',
-                  # if env var CIBUILDWHEEL set, then assume in PyPA image building wheels and use static libxml2
-                  f'-DARB_LIBXML=/usr/lib64/libxml2.a' if os.environ.get('CIBUILDWHEEL') is not None else '',
-                  f'-DPYTHON_EXECUTABLE={python}',
-                  f'-DARB_WITH_MPI={with_mpi}',
-                  f'-DARB_VECTORIZE={with_vec}'
-                  f'-DARB_ARCH={arch}',
-                  f'-DARB_GPU={with_gpu}',
-                  f'-DARB_WITH_NEUROML={with_nml}',
-                  f'-DARB_USE_BUNDLED_LIBS={use_libs}',
-                  f'-DCMAKE_BUILD_TYPE={build_type}',],
-      author='The Arbor dev team.',
-      url='https://github.com/arbor-sim/arbor',
-      description='High performance simulation of networks of multicompartment neurons.',
-      long_description=long_description,
-      long_description_content_type='text/markdown',
-      classifiers=['Development Status :: 5 - Production/Stable',
-                   'Intended Audience :: Science/Research',
-                   'License :: OSI Approved :: BSD License',
-                   'Programming Language :: Python :: 3.6',
-                   'Programming Language :: Python :: 3.7',
-                   'Programming Language :: Python :: 3.8',
-                   'Programming Language :: Python :: 3.9',
-                   'Programming Language :: Python :: 3.10',
-                   'Programming Language :: C++',],
-      project_urls={'Source': 'https://github.com/arbor-sim/arbor',
-                    'Documentation': 'https://docs.arbor-sim.org',
-                    'Bug Reports': 'https://github.com/arbor-sim/arbor/issues',},)
+    version=arbor_version,
+    python_requires='>=3.6',
+    install_requires=['numpy'],
+    setup_requires=[],
+    zip_safe=False,
+    cmake_args=cmake_args,
+    author='The Arbor dev team.',
+    url='https://arbor-sim.org',
+    description='High performance simulation of networks of multicompartment neurons.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=['Development Status :: 5 - Production/Stable',
+                'Intended Audience :: Science/Research',
+                'License :: OSI Approved :: BSD License',
+                'Programming Language :: Python :: 3.6',
+                'Programming Language :: Python :: 3.7',
+                'Programming Language :: Python :: 3.8',
+                'Programming Language :: Python :: 3.9',
+                'Programming Language :: Python :: 3.10',
+                'Programming Language :: C++',],
+    project_urls={'Source': 'https://github.com/arbor-sim/arbor',
+                'Documentation': 'https://docs.arbor-sim.org',
+                'Bug Reports': 'https://github.com/arbor-sim/arbor/issues',},)
