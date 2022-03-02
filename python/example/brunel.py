0e4917e744e59d37183e82779bcb80a721909b83
Thorsten Hater <24411438+thorstenhater@users.noreply.github.com>
2022-03-02 09:25:40 +0100

    Use one generator for both populations.
    Before we used two clones of the same generator, which meant
    that inh and exc connection sets were identical, which is wrong!
    As a nice benefit, this speeds up init by 30%

--- python/example/brunel.py
+++ python/example/brunel.py
@@ -22,11 +22,11 @@ Call with parameters, for example:
 
 # Samples m unique values in interval [start, end) - gid.
 # We exclude gid because we don't want self-loops.
-def sample_subset(gid, start, end, m):
+def sample_subset(gen, gid, start, end, m):
     idx = np.arange(start, end)
     if start <= gid < end:
         idx = np.delete(idx, gid - start)
-    RandomState(gid + 42).shuffle(idx)
+    gen.shuffle(idx)
     return idx[:m]
 
 class brunel_recipe (arbor.recipe):
@@ -61,12 +61,13 @@ class brunel_recipe (arbor.recipe):
         return arbor.cell_kind.lif
 
     def connections_on(self, gid):
+        gen = RandomState(gid + self.seed_)
         connections=[]
         # Add incoming excitatory connections.
-        for i in sample_subset(gid, 0, self.ncells_exc_, self.in_degree_exc_):
+        for i in sample_subset(gen, gid, 0, self.ncells_exc_, self.in_degree_exc_):
             connections.append(arbor.connection((i,"src"), "tgt", self.weight_exc_, self.delay_))
         # Add incoming inhibitory connections.
-        for i in sample_subset(gid, self.ncells_exc_, self.ncells_exc_ + self.ncells_inh_, self.in_degree_inh_):
+        for i in sample_subset(gen, gid, self.ncells_exc_, self.ncells_exc_ + self.ncells_inh_, self.in_degree_inh_):
             connections.append(arbor.connection((i,"src"), "tgt", self.weight_inh_, self.delay_))
         return connections
 
