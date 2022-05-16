755e6337407d52b88bdbf1514a54446e2c17d00f
Brent Huisman <brent@huisman.pl>
2022-05-16 12:31:11 +0200

    sigh

---
+++ scripts/where.py
@@ -0,0 +1,12 @@
+import sys,sysconfig
+
+pfx=sys.stdin.read()
+try:
+    #override scheme on debian/ubuntu py3.10, where 'posix_local' is set and malfunctioning.
+    if sysconfig.get_default_scheme()=='posix_local':
+        print(sysconfig.get_path('platlib',vars={} if pfx=='' else {'base':pfx,'platbase':pfx},scheme='posix_prefix'))
+        quit()
+except AttributeError:
+    #we're on Python <= 3.9, no scheme setting required and get_default_scheme does not exist.
+    pass
+print(sysconfig.get_path('platlib',vars={} if pfx=='' else {'base':pfx,'platbase':pfx}))
