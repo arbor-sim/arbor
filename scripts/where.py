625379704a1a444c8847c2a17d898677d1837275
Brent Huisman <brenthuisman@users.noreply.github.com>
2022-05-17 17:02:29 +0200

    Update scripts/where.py
    Co-authored-by: Simon Frasch <simon.frasch@cscs.ch>

--- scripts/where.py
+++ scripts/where.py
@@ -5,7 +5,7 @@ try:
     #override scheme on debian/ubuntu py3.10, where 'posix_local' is set and malfunctioning.
     if sysconfig.get_default_scheme()=='posix_local':
         print(sysconfig.get_path('platlib',vars={} if pfx=='' else {'base':pfx,'platbase':pfx},scheme='posix_prefix'))
-        quit()
+        sys.exit()
 except AttributeError:
     #we're on Python <= 3.9, no scheme setting required and get_default_scheme does not exist.
     pass
