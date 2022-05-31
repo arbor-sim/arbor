fb499792fb5dea247da5885121fae0d5fe48c82b
Thorsten Hater <24411438+thorstenhater@users.noreply.github.com>
2022-05-31 09:49:54 +0200

    Clean-up example some more.

--- example/diffusion/diffusion.cpp
+++ example/diffusion/diffusion.cpp
@@ -19,53 +19,53 @@
 #include <arbor/util/any_ptr.hpp>
 
 using namespace arborio::literals;
+using namespace arb;
 
-struct recipe: public arb::recipe {
-     recipe(double ext, double dx, double Xi, double beta): l{ext}, d{dx}, i{Xi}, b{beta} {
-        gprop.default_parameters = arb::neuron_parameter_defaults;
-        gprop.default_parameters.discretization = arb::cv_policy_max_extent{d};
+struct linear: public recipe {
+     linear(double ext, double dx, double Xi, double beta): l{ext}, d{dx}, i{Xi}, b{beta} {
+        gprop.default_parameters = neuron_parameter_defaults;
+        gprop.default_parameters.discretization = cv_policy_max_extent{d};
     }
 
-    arb::cell_size_type num_cells()                             const override { return 1; }
-    arb::cell_kind get_cell_kind(arb::cell_gid_type)            const override { return arb::cell_kind::cable; }
-    std::any get_global_properties(arb::cell_kind)              const override { return gprop; }
-    std::vector<arb::probe_info> get_probes(arb::cell_gid_type) const override { return {arb::cable_probe_ion_diff_concentration_cell{"na"}}; }
+    cell_size_type num_cells()                             const override { return 1; }
+    cell_kind get_cell_kind(cell_gid_type)            const override { return cell_kind::cable; }
+    std::any get_global_properties(cell_kind)              const override { return gprop; }
+    std::vector<probe_info> get_probes(cell_gid_type) const override { return {cable_probe_ion_diff_concentration_cell{"na"}}; }
 
-    arb::util::unique_any get_cell_description(arb::cell_gid_type) const override {
+    util::unique_any get_cell_description(cell_gid_type) const override {
         // Stick morphology
         // -----|-----
-        arb::segment_tree tree;
-        auto p = tree.append(arb::mnpos, { -l, 0, 0, 3}, {l, 0, 0, 3}, 1);
-        arb::morphology morph{tree};
+        segment_tree tree;
+        tree.append(mnpos, { -l, 0, 0, 3}, {l, 0, 0, 3}, 1);
         // Setup
-        arb::decor decor;
-        decor.set_default(arb::init_int_concentration{"na", i});
-        decor.set_default(arb::ion_diffusivity{"na", b});
-        decor.place("(location 0 0.5)"_ls, arb::synapse("inject/x=na", {{"alpha", 200.0*l}}), "Zap");
-        decor.paint("(all)"_reg, arb::density("decay/x=na"));
-        return arb::cable_cell(morph, {}, decor);
+        decor decor;
+        decor.set_default(init_int_concentration{"na", i});
+        decor.set_default(ion_diffusivity{"na", b});
+        decor.place("(location 0 0.5)"_ls, synapse("inject/x=na", {{"alpha", 200.0*l}}), "Zap");
+        decor.paint("(all)"_reg, density("decay/x=na"));
+        return cable_cell({tree}, {}, decor);
     }
 
-    std::vector<arb::event_generator> event_generators(arb::cell_gid_type gid) const override {
-        return {arb::explicit_generator({{{"Zap"}, 0.0, 0.005}})};
+    std::vector<event_generator> event_generators(cell_gid_type gid) const override {
+        return {explicit_generator({{{"Zap"}, 0.0, 0.005}})};
     }
 
-    arb::cable_cell_global_properties gprop;
+    cable_cell_global_properties gprop;
     double l, d, i, b;
 };
 
 std::ofstream out;
 
-void sampler(arb::probe_metadata pm, std::size_t n, const arb::sample_record* samples) {
-    auto ptr = arb::util::any_cast<const arb::mcable_list*>(pm.meta);
+void sampler(probe_metadata pm, std::size_t n, const sample_record* samples) {
+    auto ptr = util::any_cast<const mcable_list*>(pm.meta);
     assert(ptr);
     auto n_cable = ptr->size();
     out << "time,prox,dist,Xd\n"
         << std::fixed << std::setprecision(4);
     for (std::size_t i = 0; i<n; ++i) {
-        const auto& [val, _ig] = *arb::util::any_cast<const arb::cable_sample_range*>(samples[i].data);
+        const auto& [val, _ig] = *util::any_cast<const cable_sample_range*>(samples[i].data);
         for (unsigned j = 0; j<n_cable; ++j) {
-            arb::mcable loc = (*ptr)[j];
+            mcable loc = (*ptr)[j];
             out << samples[i].time << ',' << loc.prox_pos << ',' << loc.dist_pos << ',' << val[j] << '\n';
         }
     }
@@ -83,8 +83,6 @@ struct opt_t {
     std::string out = "log.csv";
     int gpu = -1;
 };
-
-std::ostream& operator<<(std::ostream& o, const opt_t&) { return o; }
 
 opt_t read_options(int argc, char** argv) {
     auto usage = "\n"
@@ -123,10 +121,10 @@ int main(int argc, char** argv) {
     auto O = read_options(argc, argv);
     out = std::ofstream{O.out};
     if (!out.good()) throw std::runtime_error("Could not open output file for writing.");
-    auto C = arb::make_context({1, O.gpu});
-    recipe R{O.L, O.dx, O.Xi, O.dX};
-    arb::simulation S(R, arb::partition_load_balance(R, C), C);
-    S.add_sampler(arb::all_probes, arb::regular_schedule(O.ds), sampler);
+    auto C = make_context({1, O.gpu});
+    auto R = linear{O.L, O.dx, O.Xi, O.dX};
+    simulation S(R, partition_load_balance(R, C), C);
+    S.add_sampler(all_probes, regular_schedule(O.ds), sampler);
     S.run(O.T, O.dt);
     out.close();
 }
