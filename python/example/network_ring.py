arbor-sim/arbor

commit 48ad369d8d32a08c70f11247754c17557cd7930c

Adopt Black for Python. (#1906)

python/example/network_ring.py
@@ -15,52 +15,65 @@ from math import sqrt
 #        \
 #         b2
 
+
 def make_cable_cell(gid):
     # (1) Build a segment tree
     tree = arbor.segment_tree()
 
     # Soma (tag=1) with radius 6 μm, modelled as cylinder of length 2*radius
-    s = tree.append(arbor.mnpos, arbor.mpoint(-12, 0, 0, 6), arbor.mpoint(0, 0, 0, 6), tag=1)
+    s = tree.append(
+        arbor.mnpos, arbor.mpoint(-12, 0, 0, 6), arbor.mpoint(0, 0, 0, 6), tag=1
+    )
 
     # Single dendrite (tag=3) of length 50 μm and radius 2 μm attached to soma.
     b0 = tree.append(s, arbor.mpoint(0, 0, 0, 2), arbor.mpoint(50, 0, 0, 2), tag=3)
 
     # Attach two dendrites (tag=3) of length 50 μm to the end of the first dendrite.
     # Radius tapers from 2 to 0.5 μm over the length of the dendrite.
-    b1 = tree.append(b0, arbor.mpoint(50, 0, 0, 2), arbor.mpoint(50+50/sqrt(2), 50/sqrt(2), 0, 0.5), tag=3)
+    b1 = tree.append(
+        b0,
+        arbor.mpoint(50, 0, 0, 2),
+        arbor.mpoint(50 + 50 / sqrt(2), 50 / sqrt(2), 0, 0.5),
+        tag=3,
+    )
     # Constant radius of 1 μm over the length of the dendrite.
-    b2 = tree.append(b0, arbor.mpoint(50, 0, 0, 1), arbor.mpoint(50+50/sqrt(2), -50/sqrt(2), 0, 1), tag=3)
+    b2 = tree.append(
+        b0,
+        arbor.mpoint(50, 0, 0, 1),
+        arbor.mpoint(50 + 50 / sqrt(2), -50 / sqrt(2), 0, 1),
+        tag=3,
+    )
 
     # Associate labels to tags
     labels = arbor.label_dict()
-    labels['soma'] = '(tag 1)'
-    labels['dend'] = '(tag 3)'
+    labels["soma"] = "(tag 1)"
+    labels["dend"] = "(tag 3)"
 
     # (2) Mark location for synapse at the midpoint of branch 1 (the first dendrite).
-    labels['synapse_site'] = '(location 1 0.5)'
+    labels["synapse_site"] = "(location 1 0.5)"
     # Mark the root of the tree.
-    labels['root'] = '(root)'
+    labels["root"] = "(root)"
 
     # (3) Create a decor and a cable_cell
     decor = arbor.decor()
 
     # Put hh dynamics on soma, and passive properties on the dendrites.
-    decor.paint('"soma"', arbor.density('hh'))
-    decor.paint('"dend"', arbor.density('pas'))
+    decor.paint('"soma"', arbor.density("hh"))
+    decor.paint('"dend"', arbor.density("pas"))
 
     # (4) Attach a single synapse.
-    decor.place('"synapse_site"', arbor.synapse('expsyn'), 'syn')
+    decor.place('"synapse_site"', arbor.synapse("expsyn"), "syn")
 
     # Attach a spike detector with threshold of -10 mV.
-    decor.place('"root"', arbor.spike_detector(-10), 'detector')
+    decor.place('"root"', arbor.spike_detector(-10), "detector")
 
     cell = arbor.cable_cell(tree, labels, decor)
 
     return cell
 
-# (5) Create a recipe that generates a network of connected cells.
-class ring_recipe (arbor.recipe):
 
+# (5) Create a recipe that generates a network of connected cells.
+class ring_recipe(arbor.recipe):
     def __init__(self, ncells):
         # The base C++ class constructor must be called first, to ensure that
         # all memory in the C++ class is initialized correctly.
@@ -84,17 +97,17 @@ class ring_recipe (arbor.recipe):
 
     # (8) Make a ring network. For each gid, provide a list of incoming connections.
     def connections_on(self, gid):
-        src = (gid-1)%self.ncells
-        w = 0.01 # 0.01 μS on expsyn
-        d = 5 # ms delay
-        return [arbor.connection((src,'detector'), 'syn', w, d)]
+        src = (gid - 1) % self.ncells
+        w = 0.01  # 0.01 μS on expsyn
+        d = 5  # ms delay
+        return [arbor.connection((src, "detector"), "syn", w, d)]
 
     # (9) Attach a generator to the first cell in the ring.
     def event_generators(self, gid):
-        if gid==0:
-            sched = arbor.explicit_schedule([1]) # one event at 1 ms
-            weight = 0.1 # 0.1 μS on expsyn
-            return [arbor.event_generator('syn', weight, sched)]
+        if gid == 0:
+            sched = arbor.explicit_schedule([1])  # one event at 1 ms
+            weight = 0.1  # 0.1 μS on expsyn
+            return [arbor.event_generator("syn", weight, sched)]
         return []
 
     # (10) Place a probe at the root of each cell.
@@ -103,6 +116,7 @@ class ring_recipe (arbor.recipe):
 
     def global_properties(self, kind):
         return self.props
+
 
 # (11) Instantiate recipe
 ncells = 4
@@ -121,19 +135,25 @@ handles = [sim.sample((gid, 0), arbor.regular_schedule(0.1)) for gid in range(nc
 
 # (15) Run simulation for 100 ms
 sim.run(100)
-print('Simulation finished')
+print("Simulation finished")
 
 # (16) Print spike times
-print('spikes:')
+print("spikes:")
 for sp in sim.spikes():
-    print(' ', sp)
+    print(" ", sp)
 
 # (17) Plot the recorded voltages over time.
 print("Plotting results ...")
 df_list = []
 for gid in range(ncells):
     samples, meta = sim.samples(handles[gid])[0]
-    df_list.append(pandas.DataFrame({'t/ms': samples[:, 0], 'U/mV': samples[:, 1], 'Cell': f"cell {gid}"}))
+    df_list.append(
+        pandas.DataFrame(
+            {"t/ms": samples[:, 0], "U/mV": samples[:, 1], "Cell": f"cell {gid}"}
+        )
+    )
 
-df = pandas.concat(df_list,ignore_index=True)
-seaborn.relplot(data=df, kind="line", x="t/ms", y="U/mV",hue="Cell",ci=None).savefig('network_ring_result.svg')
+df = pandas.concat(df_list, ignore_index=True)
+seaborn.relplot(data=df, kind="line", x="t/ms", y="U/mV", hue="Cell", ci=None).savefig(
+    "network_ring_result.svg"
+)

python/example/single_cell_allen.py
@@ -24,23 +24,23 @@ def load_allen_fit(fit):
 
     param = defaultdict(parameters)
     mechs = defaultdict(dict)
-    for block in fit['genome']:
-        mech   = block['mechanism'] or 'pas'
-        region = block['section']
-        name   = block['name']
-        value  = float(block['value'])
-        if name.endswith('_' + mech):
-            name = name[:-(len(mech) + 1)]
+    for block in fit["genome"]:
+        mech = block["mechanism"] or "pas"
+        region = block["section"]
+        name = block["name"]
+        value = float(block["value"])
+        if name.endswith("_" + mech):
+            name = name[: -(len(mech) + 1)]
         elif mech == "pas":
             # transform names and values
-            if name == 'cm':
+            if name == "cm":
                 # scaling factor NEURON -> Arbor
-                param[region].cm = value/100.0
-            elif name == 'Ra':
+                param[region].cm = value / 100.0
+            elif name == "Ra":
                 param[region].rL = value
-            elif name == 'Vm':
+            elif name == "Vm":
                 param[region].Vm = value
-            elif name == 'celsius':
+            elif name == "celsius":
                 param[region].tempK = value + 273.15
             else:
                 raise Exception(f"Unknown key: {name}")
@@ -53,31 +53,36 @@ def load_allen_fit(fit):
     mechs = [(r, m, vs) for (r, m), vs in mechs.items()]
 
     default = parameters(
-        tempK=float(fit['conditions'][0]['celsius']) + 273.15,
-        Vm=float(fit['conditions'][0]['v_init']),
-        rL=float(fit['passive'][0]['ra'])
+        tempK=float(fit["conditions"][0]["celsius"]) + 273.15,
+        Vm=float(fit["conditions"][0]["v_init"]),
+        rL=float(fit["passive"][0]["ra"]),
     )
 
     ions = []
-    for kv in fit['conditions'][0]['erev']:
-        region = kv['section']
+    for kv in fit["conditions"][0]["erev"]:
+        region = kv["section"]
         for k, v in kv.items():
-            if k == 'section':
+            if k == "section":
                 continue
             ion = k[1:]
             ions.append((region, ion, float(v)))
 
-    return default, regs, ions, mechs, fit['fitting'][0]['junction_potential']
+    return default, regs, ions, mechs, fit["fitting"][0]["junction_potential"]
+
 
 def make_cell(swc, fit):
     morphology = arbor.load_swc_neuron(swc)
     # (2) Label the region tags found in the swc with the names used in the parameter fit file.
     # In addition, label the midpoint of the somarbor.
-    labels = arbor.label_dict({'soma': '(tag 1)',
-                                'axon': '(tag 2)',
-                                'dend': '(tag 3)',
-                                'apic': '(tag 4)',
-                                'midpoint': '(location 0 0.5)'})
+    labels = arbor.label_dict(
+        {
+            "soma": "(tag 1)",
+            "axon": "(tag 2)",
+            "dend": "(tag 3)",
+            "apic": "(tag 4)",
+            "midpoint": "(location 0 0.5)",
+        }
+    )
 
     # (3) A function that parses the Allen parameter fit file into components for an arbor.decor
     dflt, regions, ions, mechanisms, offset = load_allen_fit(fit)
@@ -92,34 +97,37 @@ def make_cell(swc, fit):
     # (7) set reversal potentials
     for region, ion, e in ions:
         decor.paint(f'"{region}"', ion_name=ion, rev_pot=e)
-    decor.set_ion('ca', int_con=5e-5, ext_con=2.0, method=arbor.mechanism('nernst/x=ca'))
+    decor.set_ion(
+        "ca", int_con=5e-5, ext_con=2.0, method=arbor.mechanism("nernst/x=ca")
+    )
     # (8) assign ion dynamics
     for region, mech, values in mechanisms:
         nm = mech
         vs = {}
-        sp = '/'
+        sp = "/"
         for k, v in values.items():
-            if mech == 'pas' and k == 'e':
-                nm = f'{nm}{sp}{k}={v}'
-                sp = ','
+            if mech == "pas" and k == "e":
+                nm = f"{nm}{sp}{k}={v}"
+                sp = ","
             else:
                 vs[k] = v
         decor.paint(f'"{region}"', arbor.density(arbor.mechanism(nm, vs)))
     # (9) attach stimulus and spike detector
-    decor.place('"midpoint"', arbor.iclamp(200, 1000, 0.15), 'ic')
-    decor.place('"midpoint"', arbor.spike_detector(-40), 'sd')
+    decor.place('"midpoint"', arbor.iclamp(200, 1000, 0.15), "ic")
+    decor.place('"midpoint"', arbor.spike_detector(-40), "sd")
     # (10) discretisation strategy: max compartment length
     decor.discretization(arbor.cv_policy_max_extent(20))
 
     # (11) Create cell
     return arbor.cable_cell(morphology, labels, decor), offset
 
+
 # (12) Create cell, model
-cell, offset = make_cell('single_cell_allen.swc', 'single_cell_allen_fit.json')
+cell, offset = make_cell("single_cell_allen.swc", "single_cell_allen_fit.json")
 model = arbor.single_cell_model(cell)
 
 # (13) Set the probe
-model.probe('voltage', '"midpoint"', frequency=200)
+model.probe("voltage", '"midpoint"', frequency=200)
 
 # (14) Install the Allen mechanism catalogue.
 model.properties.catalogue.extend(arbor.allen_catalogue(), "")
@@ -128,14 +136,39 @@ model.properties.catalogue.extend(arbor.allen_catalogue(), "")
 model.run(tfinal=1400, dt=0.005)
 
 # (16) Load and scale reference
-reference = 1000.0*pandas.read_csv('single_cell_allen_neuron_ref.csv')['U/mV'].values[:-1] + offset
+reference = (
+    1000.0 * pandas.read_csv("single_cell_allen_neuron_ref.csv")["U/mV"].values[:-1]
+    + offset
+)
 
 # (17) Plot
 df_list = []
-df_list.append(pandas.DataFrame({'t/ms': model.traces[0].time, 'U/mV':model.traces[0].value, 'Simulator': "Arbor"}))
-df_list.append(pandas.DataFrame({'t/ms': model.traces[0].time, 'U/mV':reference, 'Simulator': "Neuron"}))
-df = pandas.concat(df_list,ignore_index=True)
-seaborn.relplot(data=df, kind="line", x="t/ms", y="U/mV",hue="Simulator",ci=None)
-plt.scatter(model.spikes, [-40]*len(model.spikes), color=seaborn.color_palette()[2], zorder=20)
-plt.bar(200, max(reference)-min(reference), 1000, min(reference), align='edge', label='Stimulus', color='0.9')
-plt.savefig('single_cell_allen_result.svg')
+df_list.append(
+    pandas.DataFrame(
+        {
+            "t/ms": model.traces[0].time,
+            "U/mV": model.traces[0].value,
+            "Simulator": "Arbor",
+        }
+    )
+)
+df_list.append(
+    pandas.DataFrame(
+        {"t/ms": model.traces[0].time, "U/mV": reference, "Simulator": "Neuron"}
+    )
+)
+df = pandas.concat(df_list, ignore_index=True)
+seaborn.relplot(data=df, kind="line", x="t/ms", y="U/mV", hue="Simulator", ci=None)
+plt.scatter(
+    model.spikes, [-40] * len(model.spikes), color=seaborn.color_palette()[2], zorder=20
+)
+plt.bar(
+    200,
+    max(reference) - min(reference),
+    1000,
+    min(reference),
+    align="edge",
+    label="Stimulus",
+    color="0.9",
+)
+plt.savefig("single_cell_allen_result.svg")

commit daeae7e9c0161f6ecbd2184358d28e114fcbf66b

- Fix line numbers in tutorials, fallout from #1906 - Mention arbor-contrib in a few relevant places - Correct some fallout from #1904 - A new hardware and profiling tutorial, and covers things moved out of other examples in #1904 - Various other documentation fixes

python/example/network_ring.py
@@ -2,8 +2,8 @@
 # This script is included in documentation. Adapt line numbers if touched.
 
 import arbor
-import pandas
-import seaborn
+import pandas # You may have to pip install these
+import seaborn # You may have to pip install these
 from math import sqrt
 
 # Construct a cell with the following morphology.
@@ -123,9 +123,9 @@ class ring_recipe(arbor.recipe):
 ncells = 4
 recipe = ring_recipe(ncells)
 
-# (12) Create an execution context using all locally available threads, domain decomposition and simulation
+# (12) Create an execution context using all locally available threads and simulation
 ctx = arbor.context("avail_threads")
-sim = arbor.simulation(recipe, context=ctx)
+sim = arbor.simulation(recipe, ctx)
 
 # (13) Set spike generators to record
 sim.record(arbor.spike_recording.all)

python/example/network_ring_gpu.py
@@ -2,10 +2,9 @@
 # This script is included in documentation. Adapt line numbers if touched.
 
 import arbor
-import pandas
+import pandas  # You may have to pip install these
+import seaborn  # You may have to pip install these
 from math import sqrt
-
-# Run with srun -n NJOBS python network_ring_mpi.py
 
 # Construct a cell with the following morphology.
 # The soma (at the root of the tree) is marked 's', and
@@ -121,43 +120,67 @@ class ring_recipe(arbor.recipe):
         return self.props
 
 
-# (11) Instantiate recipe
-ncells = 500
-recipe = ring_recipe(ncells)
-
-# (12) Create an MPI communicator, and use it to create a hardware context
-arbor.mpi_init()
-comm = arbor.mpi_comm()
-print(comm)
-context = arbor.context(mpi=comm)
+# (11) Set up the hardware context
+# gpu_id set to None will not use a GPU.
+# gpu_id=0 instructs Arbor to the first GPU present in your system
+context = arbor.context(
+    threads="avail_threads", gpu_id=None
+)
 print(context)
 
-# (13) Create a default domain decomposition and simulation
-sim = arbor.simulation(recipe, context)
+# (12) Set up and start the meter manager
+meters = arbor.meter_manager()
+meters.start(context)
 
-# (14) Set spike generators to record
+# (13) Instantiate recipe
+ncells = 50
+recipe = ring_recipe(ncells)
+meters.checkpoint("recipe-create", context)
+
+# (14) Define a hint at to the execution.
+hint = arbor.partition_hint()
+hint.prefer_gpu = True
+hint.gpu_group_size = 1000
+print(hint)
+hints = {arbor.cell_kind.cable: hint}
+
+# (15) Domain decomp
+decomp = arbor.partition_load_balance(recipe, context, hints)
+print(decomp)
+meters.checkpoint("load-balance", context)
+
+# (16) Simulation init and set spike generators to record
+sim = arbor.simulation(recipe, context, decomp)
 sim.record(arbor.spike_recording.all)
-
-# (15) Attach a sampler to the voltage probe on cell 0. Sample rate of 1 sample every ms.
-# Sampling period increased w.r.t network_ring.py to reduce amount of data
 handles = [sim.sample((gid, 0), arbor.regular_schedule(1)) for gid in range(ncells)]
+meters.checkpoint("simulation-init", context)
 
-# (16) Run simulation
+# (17) Run simulation
 sim.run(ncells * 5)
 print("Simulation finished")
+meters.checkpoint("simulation-run", context)
 
-# (17) Store the recorded voltages
-print("Storing results ...")
+# (18) Results
+# Print profiling information
+print(f"{arbor.meter_report(meters, context)}")
+
+# Print spike times
+print("spikes:")
+for sp in sim.spikes():
+    print(" ", sp)
+
+# Plot the recorded voltages over time.
+print("Plotting results ...")
 df_list = []
 for gid in range(ncells):
-    if len(sim.samples(handles[gid])):
-        samples, meta = sim.samples(handles[gid])[0]
-        df_list.append(
-            pandas.DataFrame(
-                {"t/ms": samples[:, 0], "U/mV": samples[:, 1], "Cell": f"cell {gid}"}
-            )
+    samples, meta = sim.samples(handles[gid])[0]
+    df_list.append(
+        pandas.DataFrame(
+            {"t/ms": samples[:, 0], "U/mV": samples[:, 1], "Cell": f"cell {gid}"}
         )
+    )
 
-if len(df_list):
-    df = pandas.concat(df_list, ignore_index=True)
-    df.to_csv(f"result_mpi_{context.rank}.csv", float_format="%g")
+df = pandas.concat(df_list, ignore_index=True)
+seaborn.relplot(data=df, kind="line", x="t/ms", y="U/mV", hue="Cell", ci=None).savefig(
+    "network_ring_gpu_result.svg"
+)

python/example/network_two_cells_gap_junctions.py
@@ -4,8 +4,8 @@ import arbor
 import argparse
 import numpy as np
 
-import pandas
-import seaborn  # You may have to pip install these.
+import pandas # You may have to pip install these.
+import seaborn # You may have to pip install these.
 import matplotlib.pyplot as plt
 
 

python/example/single_cell_allen.py
@@ -72,6 +72,7 @@ def load_allen_fit(fit):
 
 
 def make_cell(swc, fit):
+    # (1) Load the swc file pass into this function
     morphology = arbor.load_swc_neuron(swc)
     # (2) Label the region tags found in the swc with the names used in the parameter fit file.
     # In addition, label the midpoint of the somarbor.

python/example/single_cell_cable.py
@@ -40,8 +40,6 @@ class Cable(arbor.recipe):
         cv_policy_max_extent -- maximum extent of control volume in μm
         """
 
-        # (4.1) The base C++ class constructor must be called first, to ensure that
-        # all memory in the C++ class is initialized correctly.
         arbor.recipe.__init__(self)
 
         self.the_probes = probes
@@ -71,6 +69,13 @@ class Cable(arbor.recipe):
     def cell_kind(self, gid):
         assert gid == 0
         return arbor.cell_kind.cable
+
+    def probes(self, gid):
+        assert gid == 0
+        return self.the_probes
+
+    def global_properties(self, kind):
+        return self.the_props
 
     def cell_description(self, gid):
         """A high level description of the cell with global identifier gid.
@@ -110,13 +115,6 @@ class Cable(arbor.recipe):
         decor.discretization(policy)
 
         return arbor.cable_cell(tree, labels, decor)
-
-    def probes(self, gid):
-        assert gid == 0
-        return self.the_probes
-
-    def global_properties(self, kind):
-        return self.the_props
 
 
 def get_rm(g):

python/example/single_cell_detailed_recipe.py
@@ -80,15 +80,10 @@ decor.discretization(policy)
 
 cell = arbor.cable_cell(morph, labels, decor)
 
-# (5) Declare a probe.
-
-probe = arbor.cable_probe_membrane_voltage('"custom_terminal"')
-
-
-# (6) Create a class that inherits from arbor.recipe
+# (5) Create a class that inherits from arbor.recipe
 class single_recipe(arbor.recipe):
 
-    # (6.1) Define the class constructor
+    # (5.1) Define the class constructor
     def __init__(self, cell, probes):
         # The base C++ class constructor must be called first, to ensure that
         # all memory in the C++ class is initialized correctly.
@@ -105,44 +100,44 @@ class single_recipe(arbor.recipe):
         self.the_props.set_ion(ion="ca", int_con=5e-5, ext_con=2, rev_pot=132.5)
         self.the_props.catalogue.extend(arbor.allen_catalogue(), "")
 
-    # (6.2) Override the num_cells method
+    # (5.2) Override the num_cells method
     def num_cells(self):
         return 1
 
-    # (6.3) Override the num_targets method
+    # (5.3) Override the num_targets method
     def cell_kind(self, gid):
         return arbor.cell_kind.cable
 
-    # (6.4) Override the cell_description method
+    # (5.4) Override the cell_description method
     def cell_description(self, gid):
         return self.the_cell
 
-    # (6.5) Override the probes method
+    # (5.5) Override the probes method
     def probes(self, gid):
         return self.the_probes
 
-    # (6.6) Override the connections_on method
+    # (5.6) Override the connections_on method
     def connections_on(self, gid):
         return []
 
-    # (6.7) Override the gap_junction_on method
+    # (5.7) Override the gap_junction_on method
     def gap_junction_on(self, gid):
         return []
 
-    # (6.8) Override the event_generators method
+    # (5.8) Override the event_generators method
     def event_generators(self, gid):
         return []
 
-    # (6.9) Overrode the global_properties method
+    # (5.9) Overrode the global_properties method
     def global_properties(self, gid):
         return self.the_props
 
 
 # Instantiate recipe
 # Pass the probe in a list because that it what single_recipe expects.
-recipe = single_recipe(cell, [probe])
+recipe = single_recipe(cell, [arbor.cable_probe_membrane_voltage('"custom_terminal"')])
 
-# (7) Create a simulation
+# (6) Create a simulation
 sim = arbor.simulation(recipe)
 
 # Instruct the simulation to record the spikes and sample the probe
@@ -151,10 +146,10 @@ sim.record(arbor.spike_recording.all)
 probe_id = arbor.cell_member(0, 0)
 handle = sim.sample(probe_id, arbor.regular_schedule(0.02))
 
-# (8) Run the simulation
+# (7) Run the simulation
 sim.run(tfinal=100, dt=0.025)
 
-# (9) Print or display the results
+# (8) Print or display the results
 spikes = sim.spikes()
 print(len(spikes), "spikes recorded:")
 for s in spikes:

python/example/single_cell_recipe.py
@@ -8,13 +8,16 @@ import seaborn  # You may have to pip install these.
 # The corresponding generic recipe version of `single_cell_model.py`.
 
 # (1) Create a morphology with a single (cylindrical) segment of length=diameter=6 μm
+
 tree = arbor.segment_tree()
 tree.append(arbor.mnpos, arbor.mpoint(-3, 0, 0, 3), arbor.mpoint(3, 0, 0, 3), tag=1)
 
 # (2) Define the soma and its midpoint
+
 labels = arbor.label_dict({"soma": "(tag 1)", "midpoint": "(location 0 0.5)"})
 
 # (3) Create cell and set properties
+
 decor = arbor.decor()
 decor.set_property(Vm=-40)
 decor.paint('"soma"', arbor.density("hh"))
@@ -25,7 +28,6 @@ cell = arbor.cable_cell(tree, labels, decor)
 # (4) Define a recipe for a single cell and set of probes upon it.
 # This constitutes the corresponding generic recipe version of
 # `single_cell_model.py`.
-
 
 class single_recipe(arbor.recipe):
     def __init__(self, cell, probes):
@@ -56,15 +58,18 @@ class single_recipe(arbor.recipe):
         # (4.6) Override the global_properties method
         return self.the_props
 
-
 # (5) Instantiate recipe with a voltage probe located on "midpoint".
 
 recipe = single_recipe(cell, [arbor.cable_probe_membrane_voltage('"midpoint"')])
 
+# (6) Create simulation. When their defaults are sufficient, context and domain decomposition don't
+# have to be manually specified and the simulation can be create with just the recipe as argument.
+
+sim = arbor.simulation(recipe)
+
 # (7) Create and run simulation and set up 10 kHz (every 0.1 ms) sampling on the probe.
 # The probe is located on cell 0, and is the 0th probe on that cell, thus has probe_id (0, 0).
 
-sim = arbor.simulation(recipe)
 sim.record(arbor.spike_recording.all)
 handle = sim.sample((0, 0), arbor.regular_schedule(0.1))
 sim.run(tfinal=30)

scripts/run_python_examples.sh
@@ -15,9 +15,6 @@ $PREFIX python -m pip install -r python/example/example_requirements.txt
 $PREFIX python python/example/brunel.py -n 400 -m 100 -e 20 -p 0.1 -w 1.2 -d 1 -g 0.5 -l 5 -t 100 -s 1 -G 50 -S 123
 # $PREFIX python python/dynamic-catalogue.py # arbor-build-catalog is a test already
 $PREFIX python python/example/gap_junctions.py
-# $PREFIX python python/example/network_ring_mpi_plot.py # requires MPI
-# $PREFIX python python/example/network_ring_mpi.py # requires MPI
-$PREFIX python python/example/network_ring.py
 $PREFIX python python/example/single_cell_cable.py
 $PREFIX python python/example/single_cell_detailed_recipe.py python/example/single_cell_detailed.swc
 $PREFIX python python/example/single_cell_detailed.py python/example/single_cell_detailed.swc
@@ -27,5 +24,10 @@ $PREFIX python python/example/single_cell_nml.py python/example/morph.nml
 $PREFIX python python/example/single_cell_recipe.py
 $PREFIX python python/example/single_cell_stdp.py
 $PREFIX python python/example/single_cell_swc.py python/example/single_cell_detailed.swc
-$PREFIX python python/example/two_cell_gap_junctions.py
+$PREFIX python python/example/network_ring.py
+# $PREFIX python python/example/network_ring_mpi.py # requires MPI
+# $PREFIX python python/example/network_ring_mpi_plot.py # no need to test
+$PREFIX python python/example/network_ring_gpu.py # by default, gpu_id=None
+$PREFIX python python/example/network_two_cells_gap_junctions.py
+# $PREFIX python python/example/probe_lfpykit.py # maybe a bit heavy
 $PREFIX python python/example/diffusion.py