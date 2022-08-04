d12b56eae5b45973abb5d2c289ecf0c60def93f5
Thorsten Hater <24411438+thorstenhater@users.noreply.github.com>
2022-08-04 11:54:02 +0200

    Arborio reads from Stream objects now.

--- python/cable_cell_io.cpp
+++ python/cable_cell_io.cpp
@@ -19,14 +19,10 @@ namespace pyarb {
 namespace py = pybind11;
 
 arborio::cable_cell_component load_component(py::object fn) {
-    const auto fname = util::to_path(fn);
-    std::ifstream fid{fname};
-    if (!fid.good()) {
-        throw arb::file_not_found_error(fname);
-    }
-    auto component = arborio::parse_component(fid);
+    auto contents  = util::read_file_or_buffer(fn);
+    auto component = arborio::parse_component(contents);
     if (!component) {
-        throw pyarb_error("Error while trying to load component from \"" + fname + "\": " + component.error().what());
+        throw pyarb_error(std::string{"Error while trying to load component: "} + component.error().what());
     }
     return component.value();
 };
