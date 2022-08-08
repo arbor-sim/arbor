fd7661802aeffca72da3e9ca4ff01ffe3a284a50
Thorsten Hater <24411438+thorstenhater@users.noreply.github.com>
2022-08-08 16:33:11 +0200

    Fix exception translation.

--- python/pyarb.cpp
+++ python/pyarb.cpp
@@ -46,10 +46,6 @@ PYBIND11_MODULE(_arbor, m) {
     m.doc() = "arbor: multicompartment neural network models.";
     m.attr("__version__") = ARB_VERSION;
 
-    // Translate Arbor errors -> Python exceptions.
-    pybind11::register_exception<arb::file_not_found_error>(m, "FileNotFoundError", PyExc_FileNotFoundError);
-    pybind11::register_exception<arb::zero_thread_requested_error>(m, "ValueError", PyExc_ValueError);
-
     pyarb::register_cable_loader(m);
     pyarb::register_cable_probes(m, global_ptr);
     pyarb::register_cells(m);
@@ -69,14 +65,20 @@ PYBIND11_MODULE(_arbor, m) {
     pybind11::register_exception_translator([](std::exception_ptr p) {
         try {
             if (p) std::rethrow_exception(p);
-        } catch (const arb::arbor_exception& e) {
+        }
+        catch (const arb::arbor_exception& e) {
             std::stringstream msg;
             msg << e.what()
                 << "\n"
                 << e.where;
             PyErr_SetString(PyExc_RuntimeError, msg.str().c_str());
         }
-});
+    });
+
+    // Translate Arbor errors -> Python exceptions.
+    pybind11::register_exception<arb::file_not_found_error>(m, "FileNotFoundError", PyExc_FileNotFoundError);
+    pybind11::register_exception<arb::zero_thread_requested_error>(m, "ValueError", PyExc_ValueError);
+
 
     #ifdef ARB_MPI_ENABLED
     pyarb::register_mpi(m);
