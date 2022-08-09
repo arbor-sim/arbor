c0701f8b3af862ab6d4c2bebfd18b5e251e12c8c
Thorsten Hater <24411438+thorstenhater@users.noreply.github.com>
2022-08-09 17:32:24 +0200

    PANIC! Forgot to fix fvm type in GPU!!!.

--- arbor/backends/gpu/diffusion.cu
+++ arbor/backends/gpu/diffusion.cu
@@ -65,10 +65,10 @@ void solve_diffusion(
     T* __restrict__ const d,
     const T* __restrict__ const u,
     const level_metadata* __restrict__ const level_meta,
-    const fvm_index_type* __restrict__ const level_lengths,
-    const fvm_index_type* __restrict__ const level_parents,
-    const fvm_index_type* __restrict__ const block_index,
-    const fvm_index_type* __restrict__ const num_matrix) // number of packed matrices = number of cells
+    const arb_index_type* __restrict__ const level_lengths,
+    const arb_index_type* __restrict__ const level_parents,
+    const arb_index_type* __restrict__ const block_index,
+    const arb_index_type* __restrict__ const num_matrix) // number of packed matrices = number of cells
 {
     const auto tid = threadIdx.x;
     const auto bid = blockIdx.x;
@@ -213,18 +213,18 @@ void solve_diffusion(
 } // namespace kernels
 
 ARB_ARBOR_API void assemble_diffusion(
-    fvm_value_type* d,
-    fvm_value_type* rhs,
-    const fvm_value_type* invariant_d,
-    const fvm_value_type* concentration,
-    const fvm_value_type* voltage,
-    const fvm_value_type* current,
-    fvm_value_type q,
-    const fvm_value_type* conductivity,
-    const fvm_value_type* area,
-    const fvm_index_type* cv_to_intdom,
-    const fvm_value_type* dt_intdom,
-    const fvm_index_type* perm,
+    arb_value_type* d,
+    arb_value_type* rhs,
+    const arb_value_type* invariant_d,
+    const arb_value_type* concentration,
+    const arb_value_type* voltage,
+    const arb_value_type* current,
+    arb_value_type q,
+    const arb_value_type* conductivity,
+    const arb_value_type* area,
+    const arb_index_type* cv_to_intdom,
+    const arb_value_type* dt_intdom,
+    const arb_index_type* perm,
     unsigned n)
 {
     const unsigned block_dim = 128;
@@ -253,15 +253,15 @@ ARB_ARBOR_API void assemble_diffusion(
 // num_cells    = [2, 3, ...]
 // num_blocks   = level_start.size() - 1 = num_levels.size() = num_cells.size()
 ARB_ARBOR_API void solve_diffusion(
-    fvm_value_type* rhs,
-    fvm_value_type* d,                     // diagonal values
-    const fvm_value_type* u,               // upper diagonal (and lower diagonal as the matrix is SPD)
+    arb_value_type* rhs,
+    arb_value_type* d,                     // diagonal values
+    const arb_value_type* u,               // upper diagonal (and lower diagonal as the matrix is SPD)
     const level_metadata* level_meta,      // information pertaining to each level
-    const fvm_index_type* level_lengths,   // lengths of branches of every level concatenated
-    const fvm_index_type* level_parents,   // parents of branches of every level concatenated
-    const fvm_index_type* block_index,     // start index into levels for each gpu block
-    fvm_index_type* num_cells,             // the number of cells packed into this single matrix
-    fvm_index_type* padded_size,           // length of rhs, d, u, including padding
+    const arb_index_type* level_lengths,   // lengths of branches of every level concatenated
+    const arb_index_type* level_parents,   // parents of branches of every level concatenated
+    const arb_index_type* block_index,     // start index into levels for each gpu block
+    arb_index_type* num_cells,             // the number of cells packed into this single matrix
+    arb_index_type* padded_size,           // length of rhs, d, u, including padding
     unsigned num_blocks,                   // number of blocks
     unsigned blocksize)                    // size of each block
 {
