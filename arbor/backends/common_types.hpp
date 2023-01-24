6625b1d190996178aa36a2926615fa808c1006aa
Thorsten Hater <24411438+thorstenhater@users.noreply.github.com>
2023-01-24 10:37:36 +0100

    Shuffle.

--- arbor/backends/common_types.hpp
+++ arbor/backends/common_types.hpp
@@ -3,7 +3,8 @@
 #include <arbor/common_types.hpp>
 
 #include "util/range.hpp"
-#include "threshold_crossing.hpp"
+#include "backends/threshold_crossing.hpp"
+#include "execution_context.hpp"
 
 namespace arb {
 
@@ -11,6 +12,14 @@ struct fvm_integration_result {
     util::range<const threshold_crossing*> crossings;
     util::range<const arb_value_type*> sample_time;
     util::range<const arb_value_type*> sample_value;
+};
+
+struct fvm_detector_info {
+    arb_size_type count = 0;
+    std::vector<arb_index_type> cv;
+    std::vector<arb_value_type> threshold;
+    execution_context ctx
+    ;
 };
 
 }
