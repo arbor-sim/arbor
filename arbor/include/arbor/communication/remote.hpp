a03cc4c5d18b1aae8e22027615f4b0a9ceb49911
Thorsten Hater <24411438+thorstenhater@users.noreply.github.com>
2023-03-13 10:56:26 +0100

    Checked MPI, try to pass status.

--- arbor/include/arbor/communication/remote.hpp
+++ arbor/include/arbor/communication/remote.hpp
@@ -65,6 +65,16 @@ struct unexpected_message: remote_error {
     unexpected_message(): remote_error{"Arbor remote: Received unknown tag."} {}
 };
 
+struct mpi_error: remote_error {
+    mpi_error(const std::string& where, int rc):
+        remote_error{"MPI failed in " + where + " with error=" + std::to_string(rc)} {}
+};
+
+inline
+void mpi_checked(int rc, const std::string& where) {
+    if (rc != MPI_SUCCESS) throw mpi_error{where, rc};
+}
+
 // Prepare byte buffer for sending.
 inline
 std::vector<char> make_send_buffer(const ctrl_message& msg) {
@@ -127,28 +137,29 @@ inline
 ctrl_message exchange_ctrl(MPI_Comm comm, const ctrl_message& msg) {
     ctrl_message result = msg_null{};
     int rank = -1;
-    MPI_Comm_rank(comm, &rank);
+    mpi_checked(MPI_Comm_rank(comm, &rank), "echange ctrl block: comm rank");
     if (rank != ARB_REMOTE_ROOT) return result;
 
     auto send = make_send_buffer(msg);
     auto recv = std::vector<char>(ARB_REMOTE_MESSAGE_LENGTH, 0x0);
-
-    MPI_Sendrecv((const void*) send.data(), ARB_REMOTE_MESSAGE_LENGTH, MPI_CHAR, ARB_REMOTE_ROOT, ARB_REMOTE_MAGIC,
-                 (void*)       recv.data(), ARB_REMOTE_MESSAGE_LENGTH, MPI_CHAR, MPI_ANY_SOURCE,  ARB_REMOTE_MAGIC,
-                 comm,
-                 nullptr);
+    MPI_Status ignored;
+    mpi_checked(MPI_Sendrecv((const void*) send.data(), ARB_REMOTE_MESSAGE_LENGTH, MPI_CHAR, ARB_REMOTE_ROOT, ARB_REMOTE_MAGIC,
+                             (void*)       recv.data(), ARB_REMOTE_MESSAGE_LENGTH, MPI_CHAR, MPI_ANY_SOURCE,  ARB_REMOTE_MAGIC,
+                             comm,
+                             &ignored), "exchange control block: Sendrecv");
     return make_ctrl_message(recv);
 }
 
 template <typename T>
 std::vector<T> gather_all(const std::vector<T>& send, MPI_Comm comm) {
     int size = -1, rank = -1;
-    MPI_Comm_size(comm, &size);
-    MPI_Comm_rank(comm, &rank);
+    mpi_checked(MPI_Comm_size(comm, &size), "gather_all: comm size");
+    mpi_checked(MPI_Comm_rank(comm, &rank), "gather_all: comm rank");
     int send_count = send.size();
     std::vector<int> counts(size, 0);
     std::vector<int> displs(size, 0);
-    MPI_Allgather(&send_count, 1, MPI_INT, counts.data(), 1, MPI_INT, comm);
+    mpi_checked(MPI_Allgather(&send_count, 1, MPI_INT, counts.data(), 1, MPI_INT, comm),
+                "gather_all exchanging counts: Allgather");
     int recv_bytes = 0;
     int recv_count = 0;
     for (int ix = 0; ix < size; ++ix) {
@@ -160,9 +171,10 @@ std::vector<T> gather_all(const std::vector<T>& send, MPI_Comm comm) {
     }
     std::vector<T> recv(recv_count);
     auto send_bytes = send_count*sizeof(T);
-    MPI_Allgatherv(send.data(), send_bytes,                   MPI_BYTE, // send buffer
-                   recv.data(), counts.data(), displs.data(), MPI_BYTE, // recv buffer
-                   comm);
+    mpi_checked(MPI_Allgatherv(send.data(), send_bytes,                   MPI_BYTE, // send buffer
+                               recv.data(), counts.data(), displs.data(), MPI_BYTE, // recv buffer
+                               comm),
+                "gather_all exchanging payload: Allgatherv");
     return recv;
 }
 
