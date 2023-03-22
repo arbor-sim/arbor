1ec9eda816393e16637b0ffd6f6f4ed8291d362a
Thorsten Hater <24411438+thorstenhater@users.noreply.github.com>
2023-03-22 15:50:34 +0100

    Finish Merge.

--- arbor/communication/communicator.cpp
+++ arbor/communication/communicator.cpp
@@ -1,5 +1,6 @@
 #include <utility>
 #include <vector>
+#include <limits>
 
 #include <arbor/assert.hpp>
 #include <arbor/common_types.hpp>
@@ -11,7 +12,6 @@
 #include "communication/gathered_vector.hpp"
 #include "connection.hpp"
 #include "distributed_context.hpp"
-#include "epoch.hpp"
 #include "execution_context.hpp"
 #include "profile/profiler_macro.hpp"
 #include "threading/threading.hpp"
@@ -84,11 +84,17 @@ void communicator::update_connections(const connectivity& rec,
     // Build the connection information for local cells.
     PE(init:communicator:update:gid_connections);
     std::vector<cell_connection> gid_connections;
-    std::vector<size_t> part_connections; part_connections.reserve(num_local_cells_);
+    std::vector<ext_cell_connection> gid_ext_connections;
+    std::vector<size_t> part_connections;
+    part_connections.reserve(num_local_cells_);
     part_connections.push_back(0);
+    std::vector<size_t> part_ext_connections;
+    part_ext_connections.reserve(num_local_cells_);
+    part_ext_connections.push_back(0);
     std::vector<unsigned> src_domains;
     std::vector<cell_size_type> src_counts(num_domains_);
     for (const auto gid: gids) {
+        // Local
         const auto& conns = rec.connections_on(gid);
         for (const auto& conn: conns) {
             const auto sgid = conn.source.gid;
@@ -99,10 +105,17 @@ void communicator::update_connections(const connectivity& rec,
             gid_connections.emplace_back(conn);
         }
         part_connections.push_back(gid_connections.size());
+        // Remote
+        const auto& ext_conns = rec.external_connections_on(gid);
+        for (const auto& conn: ext_conns) {
+            gid_ext_connections.emplace_back(conn);
+        }
+        part_ext_connections.push_back(gid_ext_connections.size());
     }
 
     util::make_partition(connection_part_, src_counts);
-    std::size_t n_cons = gid_connections.size();
+    auto n_cons = gid_connections.size();
+    auto n_ext_cons = gid_ext_connections.size();
     PL();
 
     // Construct the connections. The loop above gave us the information needed
@@ -110,19 +123,32 @@ void communicator::update_connections(const connectivity& rec,
     // NOTE: The connections are partitioned by the domain of their source gid.
     PE(init:communicator:update:connections);
     connections_.resize(n_cons);
+    ext_connections_.resize(n_ext_cons);
     auto offsets = connection_part_; // Copy, as we use this as the list of current target indices to write into
+    std::size_t ext = 0;
     auto src_domain = src_domains.begin();
     auto target_resolver = resolver(&target_resolution_map);
     for (const auto index: util::make_span(num_local_cells_)) {
-        const auto gid = gids[index];
+        const auto tgt_gid = gids[index];
         auto source_resolver = resolver(&source_resolution_map);
         for (const auto cidx: util::make_span(part_connections[index], part_connections[index+1])) {
             const auto& conn = gid_connections[cidx];
+            auto src_gid = conn.source.gid;
+            if(is_external(src_gid)) throw arb::source_gid_exceeds_limit(tgt_gid, src_gid);
             auto src_lid = source_resolver.resolve(conn.source);
-            auto tgt_lid = target_resolver.resolve(gid, conn.dest);
+            auto tgt_lid = target_resolver.resolve(tgt_gid, conn.target);
             auto offset  = offsets[*src_domain]++;
             ++src_domain;
-            connections_[offset] = {{conn.source.gid, src_lid}, tgt_lid, conn.weight, conn.delay, index};
+            connections_[offset] = {{src_gid, src_lid}, tgt_lid, conn.weight, conn.delay, index};
+        }
+        for (const auto cidx: util::make_span(part_ext_connections[index], part_ext_connections[index+1])) {
+            const auto& conn = gid_ext_connections[cidx];
+            auto src = global_cell_of(conn.source);
+            auto src_gid = conn.source.rid;
+            if(is_external(src_gid)) throw arb::source_gid_exceeds_limit(tgt_gid, src_gid);
+            auto tgt_lid = target_resolver.resolve(tgt_gid, conn.target);
+            ext_connections_[ext] = {src, tgt_lid, conn.weight, conn.delay, index};
+            ++ext;
         }
     }
     PL();
