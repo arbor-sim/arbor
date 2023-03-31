90be868a99ea817fda355e28075e91844fb3795a
Thorsten Hater <24411438+thorstenhater@users.noreply.github.com>
2023-03-31 18:28:44 +0200

    Final(?) polish.

--- arbor/merge_events.cpp
+++ arbor/merge_events.cpp
@@ -41,10 +41,9 @@ void linear_merge_events(std::vector<event_span> sources, pse_vector& out) {
 
 // priority-queue based merge.
 void pqueue_merge_events(std::vector<event_span> sources, pse_vector& out) {
-    // Create a priority queue to keep track of the minimum element from each sorted vector
-    std::priority_queue<std::pair<spike_event, int>,
-                        std::vector<std::pair<spike_event, int>>,
-                        std::greater<>> heap;
+    // Min heap tracking the minimum element from each span
+    using kv_type = std::pair<spike_event, int>;
+    std::priority_queue<kv_type, std::vector<kv_type>, std::greater<>> heap;
 
     // Add the first element from each sorted vector to the min heap
     for (std::size_t ix = 0; ix < sources.size(); ++ix) {
@@ -53,17 +52,17 @@ void pqueue_merge_events(std::vector<event_span> sources, pse_vector& out) {
         source.left++;
     }
 
-    // Merge the sorted vectors by continually popping the minimum element from the min heap
+    // Merge by continually popping the minimum element from the min heap
     while (!heap.empty()) {
-        auto [value, idx] = heap.top();
+        auto [value, ix] = heap.top();
         heap.pop();
         out.emplace_back(value);
 
-        // If the sorted vector from which the minimum element was taken still has elements,
-        // add the next element to the min heap
-        auto& source = sources[idx];
+        // If the sorted vector from which the minimum element was taken still
+        // has elements, add the next smallest element to the heap
+        auto& source = sources[ix];
         if (!source.empty()) {
-            heap.emplace(source.front(), idx);
+            heap.emplace(source.front(), ix);
             source.left++;
         }
     }
@@ -77,7 +76,7 @@ void merge_events(std::vector<event_span> sources, pse_vector &out) {
     if (n_evts == 0) return;
     out.reserve(out.size() + n_evts);
     auto n_queues = sources.size();
-    if (n_queues < 20) { // MAGIC: Found by ubench/merge
+    if (n_queues < 20) { // NOTE: MAGIC NUMBER, found by ubench/merge
         linear_merge_events(std::move(sources), out);
     }
     else {
--- arbor/schedule.cpp
+++ arbor/schedule.cpp
@@ -16,20 +16,23 @@ namespace arb {
 // Regular schedule implementation.
 
 time_event_span regular_schedule_impl::events(time_type t0, time_type t1) {
-    times_.clear();
-
     t0 = std::max(t0, t0_);
     t1 = std::min(t1, t1_);
 
-    if (t1 > t0) {
-        times_.reserve(1 + std::size_t((t1 - t0)*oodt_));
+    if (t0 >= t1) return {};
 
-        long long n = t0*oodt_;
-        time_type t;
-        for (t = n*dt_; t < t0; t = (++n)*dt_) {}
-        for (         ; t < t1; t = (++n)*dt_) times_.push_back(t);
-    }
+    times_.clear();
+    times_.reserve(1 + std::size_t((t1 - t0)*oodt_));
 
+    // First estimate for the starting count; floor(t0/dt)
+    long long n = t0*oodt_;
+    // Step without generating unitl we reach the lower limit
+    // then start generating until upper limit.
+    time_type t;
+    for (t = n*dt_; t < t0; t = (++n)*dt_) {}
+    for (         ; t < t1; t = (++n)*dt_) times_.push_back(t);
+    // NOTE: To avoid accumulation errors or error correcting
+    // we use t = n*dt instead of t += dt here
     return as_time_event_span(times_);
 }
 
@@ -42,18 +45,17 @@ time_event_span explicit_schedule_impl::events(time_type t0, time_type t1) {
     return {lb, ub};
 }
 
+// Poisson implementation
 time_event_span poisson_schedule_impl::events(time_type t0, time_type t1) {
-    // if we start after the maximal allowed time, we have nothing to do
-    if (t0 >= tstop_) return {};
-
-    // Restart rng cache
-    index_ = 4;
-
-    times_.clear();
-
     // restrict by maximal allowed time
+    t0 = std::max(t0, tstart_);
     t1 = std::min(t1, tstop_);
-
+    // if we start after the end, we have nothing to do
+    if (t0 >= t1) return {};
+    // Reset state
+    index_ = 4;
+    times_.clear();
+    // Generate within bounds
     for (auto t = step(t0); t < t1; t = step(t)) times_.push_back(t);
 
     return as_time_event_span(times_);
