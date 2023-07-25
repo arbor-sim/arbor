52a47b78afd255afa9ee77245027e67728f75f30
Brent Huisman <brent@huisman.pl>
2023-07-25 10:45:31 +0200

    a-b-c verbose=off prints MORE info

--- scripts/test_executables.sh
+++ scripts/test_executables.sh
@@ -10,9 +10,9 @@ if [[ ! -z "$GITHUB_ACTIONS" ]]; then
   echo "### modcc: OK." >> $GITHUB_STEP_SUMMARY
 fi
 
-# echo "=== Executing a-b-c test ======================================"
-# arbor-build-catalogue -v cat python/example/cat
-# ./scripts/test-catalogue.py ./cat-catalogue.so
-# if [[ ! -z "$GITHUB_ACTIONS" ]]; then
-#   echo "### a-b-c: OK." >> $GITHUB_STEP_SUMMARY
-# fi
+echo "=== Executing a-b-c test ======================================"
+arbor-build-catalogue cat python/example/cat
+./scripts/test-catalogue.py ./cat-catalogue.so
+if [[ ! -z "$GITHUB_ACTIONS" ]]; then
+  echo "### a-b-c: OK." >> $GITHUB_STEP_SUMMARY
+fi
