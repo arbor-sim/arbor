23edbb64a84929e3280ca918e206fe4711e9d76b
Brent Huisman <brent@huisman.pl>
2023-07-25 14:26:07 +0200

    a-b-c: remove useless try block. force nonspack c++ compiler

--- scripts/test_executables.sh
+++ scripts/test_executables.sh
@@ -12,7 +12,7 @@ fi
 
 echo "=== Executing a-b-c test ======================================"
 arbor-build-catalogue cat python/example/cat
-./scripts/test-catalogue.py ./cat-catalogue.so
+./scripts/test-catalogue.py ./cat-catalogue.so --cxx=c++
 if [[ ! -z "$GITHUB_ACTIONS" ]]; then
   echo "### a-b-c: OK." >> $GITHUB_STEP_SUMMARY
 fi
