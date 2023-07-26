470f3586d6f89aa5752ec73d4e3b59023f703218
Thorsten Hater <24411438+thorstenhater@users.noreply.github.com>
2023-07-26 08:52:23 +0200

    Also fix for density states.

--- arbor/fvm_lowered_cell_impl.hpp
+++ arbor/fvm_lowered_cell_impl.hpp
@@ -826,15 +826,17 @@ void resolve_probe(const cable_probe_stimulus_current_cell& p, probe_resolution_
 
 template <typename B>
 void resolve_probe(const cable_probe_density_state& p, probe_resolution_data<B>& R) {
-    const arb_value_type* data = R.mechanism_state(p.mechanism, p.state);
+    const auto& mech = p.mechanism;
+    if (!R.mech_instance_by_name.count(mech)) return;
+    const arb_value_type* data = R.mechanism_state(mech, p.state);
     if (!data) return;
 
-    auto support = R.mechanism_support(p.mechanism);
+    auto support = R.mechanism_support(mech);
     for (mlocation loc: thingify(p.locations, R.cell.provider())) {
         if (!support.intersects(loc)) continue;
 
         arb_index_type cv = R.D.geometry.location_cv(R.cell_idx, loc, cv_prefer::cv_nonempty);
-        auto opt_i = util::binary_search_index(R.M.mechanisms.at(p.mechanism).cv, cv);
+        auto opt_i = util::binary_search_index(R.M.mechanisms.at(mech).cv, cv);
         if (!opt_i) continue;
 
         R.result.push_back(fvm_probe_scalar{{data+*opt_i}, loc});
