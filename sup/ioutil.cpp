63eb4e4a162bf253ab5d616c35b260f98ae0717a
Thorsten Hater <24411438+thorstenhater@users.noreply.github.com>
2023-08-02 21:12:24 +0200

    Use std::filesystem::path instead of homegrown path.

--- sup/ioutil.cpp
+++ sup/ioutil.cpp
@@ -2,22 +2,15 @@
 #include <utility>
 
 #include <sup/ioutil.hpp>
-#include <sup/path.hpp>
 #include <sup/strsub.hpp>
 
 namespace sup {
 
-ARB_SUP_API std::fstream open_or_throw(const path& p, std::ios_base::openmode mode, bool exclusive) {
-    if (exclusive && exists(p)) {
-        throw std::runtime_error(strsub("file % already exists", p));
-    }
-
+ARB_SUP_API std::fstream open_or_throw(const std::filesystem::path& p, std::ios_base::openmode mode, bool exclusive) {
+    if (exclusive && std::filesystem::exists(p)) throw std::runtime_error(strsub("file % already exists", p));
     std::fstream file;
     file.open(p, mode);
-    if (!file) {
-        throw std::runtime_error(strsub("unable to open file %", p));
-    }
-
+    if (!file) throw std::runtime_error(strsub("unable to open file %", p));
     return file;
 }
 
