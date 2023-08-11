c97e9c5c1a2781da0bc45494276bc1caab5b0662
Thorsten Hater <24411438+thorstenhater@users.noreply.github.com>
2023-08-11 13:01:57 +0200

    Remove super/regular cell split. Much simplification ensues.

--- arbor/partition_load_balance.cpp
+++ arbor/partition_load_balance.cpp
@@ -41,20 +41,11 @@ auto build_global_gj_connection_table(const recipe& rec) {
     }
     return res;
 }
-}
 
-ARB_ARBOR_API domain_decomposition partition_load_balance(const recipe& rec,
-                                                          context ctx,
-                                                          const partition_hint_map& hint_map) {
-    using util::make_span;
-
-    const auto global_gj_connection_table = build_global_gj_connection_table(rec);
-
+auto make_local_gid_range(context ctx, cell_gid_type num_global_cells) {
     const auto& dist = ctx->distributed;
     unsigned num_domains = dist->size();
     unsigned domain_id = dist->id();
-    const bool gpu_avail = ctx->gpu->has_gpu();
-    auto num_global_cells = rec.num_cells();
 
     auto dom_size = [&](unsigned dom) -> cell_gid_type {
         const cell_gid_type B = num_global_cells/num_domains;
@@ -62,22 +53,24 @@ ARB_ARBOR_API domain_decomposition partition_load_balance(const recipe& rec,
         return B + (dom < R);
     };
 
-    // Global load balance
     std::vector<cell_gid_type> gid_divisions;
-    auto gid_part = make_partition(gid_divisions, transform_view(make_span(num_domains), dom_size));
+    auto gid_part = util::make_partition(gid_divisions,
+                                         util::transform_view(util::make_span(num_domains), dom_size));
 
+    return gid_part[domain_id];
+}
 
-    // Local load balance
-
+auto build_components(const std::unordered_map<cell_gid_type, std::unordered_set<cell_gid_type>>& global_gj_connection_table,
+                      std::pair<cell_gid_type, cell_gid_type> local_gid_range) {
     std::vector<std::vector<cell_gid_type>> super_cells; // cells connected by gj
-    std::vector<cell_gid_type> reg_cells; //independent cells
+    std::vector<std::vector<cell_gid_type>> res;
 
-    // Map to track visited cells (cells that already belong to a group)
+    // track visited cells (cells that already belong to a group)
     std::unordered_set<cell_gid_type> visited;
 
     // Connected components algorithm using BFS
     std::queue<cell_gid_type> q;
-    for (auto gid: make_span(gid_part[domain_id])) {
+    for (auto gid: util::make_span(local_gid_range)) {
         if (global_gj_connection_table.count(gid)) {
             // If cell hasn't been visited yet, must belong to new super_cell
             // Perform BFS starting from that cell
@@ -98,45 +91,39 @@ ARB_ARBOR_API domain_decomposition partition_load_balance(const recipe& rec,
             }
         }
         else {
-            // If cell has no gap_junctions, put in separate group of independent cells
-            reg_cells.emplace_back(gid);
+            // If cell has no gap_junctions, put in front
+            res.push_back({gid});
         }
     }
 
     // Sort super_cell groups and only keep those where the first element in the group belongs to domain
-    super_cells.erase(std::remove_if(super_cells.begin(), super_cells.end(),
-            [gid_part, domain_id](std::vector<cell_gid_type>& cg)
-            {
-                std::sort(cg.begin(), cg.end());
-                return cg.front() < gid_part[domain_id].first;
-            }), super_cells.end());
-
-    // Collect local gids that belong to this rank, and sort gids into kind lists
-    // kind_lists maps a cell_kind to a vector of either:
-    // 1. gids of regular cells (in reg_cells)
-    // 2. indices of supercells (in super_cells)
-
-    struct cell_identifier {
-        cell_gid_type id;
-        bool is_super_cell;
-    };
-    std::vector<cell_gid_type> local_gids;
-    std::unordered_map<cell_kind, std::vector<cell_identifier>> kind_lists;
-
-    for (auto gid: reg_cells) {
-        local_gids.push_back(gid);
-        kind_lists[rec.get_cell_kind(gid)].push_back({gid, false});
+    for (auto& sc: super_cells) {
+        std::sort(sc.begin(), sc.end());
+        if (!sc.empty() && sc.front() >= local_gid_range.first) res.push_back(sc);
     }
+    return res;
+}
+}
 
-    for (unsigned i = 0; i < super_cells.size(); i++) {
-        auto kind = rec.get_cell_kind(super_cells[i].front());
-        for (auto gid: super_cells[i]) {
-            if (rec.get_cell_kind(gid) != kind) {
-                throw gj_kind_mismatch(gid, super_cells[i].front());
-            }
+ARB_ARBOR_API domain_decomposition partition_load_balance(const recipe& rec,
+                                                          context ctx,
+                                                          const partition_hint_map& hint_map) {
+    const auto global_gj_connection_table = build_global_gj_connection_table(rec);
+    const auto local_gid_range = make_local_gid_range(ctx, rec.num_cells());
+    const auto components = build_components(global_gj_connection_table, local_gid_range);
+
+    std::vector<cell_gid_type> local_gids;
+    std::unordered_map<cell_kind, std::vector<cell_gid_type>> kind_lists;
+
+    for (auto idx: util::make_span(components.size())) {
+        const auto& component = components[idx];
+        const auto& first_gid  = component.front();
+        auto kind = rec.get_cell_kind(first_gid);
+        for (auto gid: component) {
+            if (rec.get_cell_kind(gid) != kind) throw gj_kind_mismatch(gid, first_gid);
             local_gids.push_back(gid);
         }
-        kind_lists[kind].push_back({i, true});
+        kind_lists[kind].push_back((cell_gid_type) idx);
     }
 
     // Create a flat vector of the cell kinds present on this node,
@@ -150,13 +137,11 @@ ARB_ARBOR_API domain_decomposition partition_load_balance(const recipe& rec,
     // of cell group updates according to rules such as the back end on
     // which the cell group is running.
 
-    auto has_gpu_backend = [&ctx](cell_kind c) {
-        return cell_kind_supported(c, backend_kind::gpu, *ctx);
-    };
+    auto has_gpu_backend = [&ctx](cell_kind c) { return ctx->gpu->has_gpu() && cell_kind_supported(c, backend_kind::gpu, *ctx); };
 
     std::vector<cell_kind> kinds;
     for (auto l: kind_lists) {
-        kinds.push_back(cell_kind(l.first));
+        kinds.push_back(l.first);
     }
     std::partition(kinds.begin(), kinds.end(), has_gpu_backend);
 
@@ -166,36 +151,34 @@ ARB_ARBOR_API domain_decomposition partition_load_balance(const recipe& rec,
         if (auto opt_hint = util::value_by_key(hint_map, k)) {
             hint = opt_hint.value();
             if(!hint.cpu_group_size) {
-                throw arbor_exception(arb::util::pprintf("unable to perform load balancing because {} has invalid suggested cpu_cell_group size of {}", k, hint.cpu_group_size));
+                throw arbor_exception(arb::util::pprintf("unable to perform load balancing because {} has invalid suggested cpu_cell_group size of {}",
+                                                         k, hint.cpu_group_size));
             }
             if(hint.prefer_gpu && !hint.gpu_group_size) {
-                throw arbor_exception(arb::util::pprintf("unable to perform load balancing because {} has invalid suggested gpu_cell_group size of {}", k, hint.gpu_group_size));
+                throw arbor_exception(arb::util::pprintf("unable to perform load balancing because {} has invalid suggested gpu_cell_group size of {}",
+                                                         k, hint.gpu_group_size));
             }
         }
 
         backend_kind backend = backend_kind::multicore;
         std::size_t group_size = hint.cpu_group_size;
 
-        if (hint.prefer_gpu && gpu_avail && has_gpu_backend(k)) {
+        if (hint.prefer_gpu && has_gpu_backend(k)) {
             backend = backend_kind::gpu;
             group_size = hint.gpu_group_size;
         }
 
         std::vector<cell_gid_type> group_elements;
-        // group_elements are sorted such that the gids of all members of a super_cell are consecutive.
+        // group_elements are sorted such that the gids of all members of a component are consecutive.
         for (auto cell: kind_lists[k]) {
-            if (!cell.is_super_cell) {
-                group_elements.push_back(cell.id);
-            } else {
-                if (group_elements.size() + super_cells[cell.id].size() > group_size && !group_elements.empty()) {
-                    groups.emplace_back(k, std::move(group_elements), backend);
-                    group_elements.clear();
-                }
-                for (auto gid: super_cells[cell.id]) {
-                    group_elements.push_back(gid);
-                }
+            const auto& component = components[cell];
+            if (group_elements.size() + component.size() > group_size && !group_elements.empty()) {
+                groups.emplace_back(k, std::move(group_elements), backend);
+                group_elements.clear();
             }
-
+            for (auto gid: component) {
+                group_elements.push_back(gid);
+            }
             if (group_elements.size()>=group_size) {
                 groups.emplace_back(k, std::move(group_elements), backend);
                 group_elements.clear();
