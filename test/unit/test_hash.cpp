97ca7a4e6ef2e15715b3812d48c4ad5abac28c3a
Thorsten Hater <24411438+thorstenhater@users.noreply.github.com>
2023-11-22 14:45:29 +0100

    The missing test.

---
+++ test/unit/test_hash.cpp
@@ -0,0 +1,24 @@
+#include <gtest/gtest.h>
+
+#include <string>
+
+#include <arbor/util/hash_def.hpp>
+
+TEST(hash, string_eq) {
+    ASSERT_EQ(arb::hash_value("foobar"), arb::hash_value(std::string{"foobar"}));
+    ASSERT_EQ(arb::hash_value("foobar"), arb::internal_hash("foobar"));
+    ASSERT_NE(arb::hash_value("foobar"), arb::internal_hash("barfoo"));
+}
+
+TEST(hash, doesnt_compile) {
+    double foo = 42;
+    // Sadly we cannot check static assertions... this shoudln't compile
+    // EXPECT_ANY_THROW(arb::hash_value(&foo));
+    // this should
+    arb::hash_value((void*) &foo);
+}
+
+// check that we do not fall into the trap of the STL...
+TEST(hash, integral_is_not_identity) {
+    ASSERT_NE(arb::hash_value(42), 42);
+}
