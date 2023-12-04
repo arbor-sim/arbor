e8c3cc77b8fae2eb7c9aa7486eac943fdbaaf9af
Thorsten Hater <24411438+thorstenhater@users.noreply.github.com>
2023-12-04 19:48:46 +0100

    Now, submodules?

--- spack/package.py
+++ spack/package.py
@@ -14,9 +14,10 @@ class Arbor(CMakePackage, CudaPackage):
     git = "https://github.com/arbor-sim/arbor.git"
     url = "https://github.com/arbor-sim/arbor/releases/download/v0.8.1/arbor-v0.9.0-full.tar.gz"
     maintainers = ("thorstenhater", "brenthuisman", "haampie")
+    submodules = True
 
-    version("master", branch="master", submodules=True)
-    version("develop", submodules=True)
+    version("master", branch="master")
+    version("develop")
     version(
         "0.9.0",
         sha256="5f9740955c821aca81e23298c17ad64f33f635756ad9b4a0c1444710f564306a",
