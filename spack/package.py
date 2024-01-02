0ac1d817359092bd0260f8f97dd8e9b962200827
Thorsten Hater <24411438+thorstenhater@users.noreply.github.com>
2024-01-02 09:38:50 +0100

    Once more into the spack.

--- spack/package.py
+++ spack/package.py
@@ -17,7 +17,7 @@ class Arbor(CMakePackage, CudaPackage):
     submodules = True
 
     version("master", branch="master", submodules=True)
-    version("develop", submodules=True)
+    version("develop", branch="master", submodules=True)
     version(
         "0.9.0",
         sha256="5f9740955c821aca81e23298c17ad64f33f635756ad9b4a0c1444710f564306a",
