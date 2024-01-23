0e756151a5c68fc1d5c21a0a5f892b24801c6062
Simon Frasch <simon.frasch@cscs.ch>
2024-01-23 11:14:04 +0100

    fix python example

--- python/example/network_description.py
+++ python/example/network_description.py
@@ -1,53 +1,48 @@
 #!/usr/bin/env python3
 # This script is included in documentation. Adapt line numbers if touched.
 
-import arbor
+import arbor as A
+from arbor import units as U
 import pandas  # You may have to pip install these
 import seaborn  # You may have to pip install these
 from math import sqrt
-import math
-
-# Construct a cell with the following morphology.
-# The soma (at the root of the tree) is marked 's', and
-# the end of each branch i is marked 'bi'.
-#
-#         b1
-#        /
-# s----b0
-#        \
-#         b2
 
 
 def make_cable_cell(gid):
     # (1) Build a segment tree
-    tree = arbor.segment_tree()
-
+    # The dendrite (dend) attaches to the soma and has two simple segments
+    # attached.
+    #
+    #             left
+    #            /
+    # soma - dend
+    #            \
+    #             right
+    tree = A.segment_tree()
+    root = A.mnpos
     # Soma (tag=1) with radius 6 μm, modelled as cylinder of length 2*radius
-    s = tree.append(
-        arbor.mnpos, arbor.mpoint(-12, 0, 0, 6), arbor.mpoint(0, 0, 0, 6), tag=1
-    )
-
-    # (b0) Single dendrite (tag=3) of length 50 μm and radius 2 μm attached to soma.
-    b0 = tree.append(s, arbor.mpoint(0, 0, 0, 2), arbor.mpoint(0, 0, 50, 2), tag=3)
-
+    soma = tree.append(root, (-12, 0, 0, 6), (0, 0, 0, 6), tag=1)
+    # Single dendrite (tag=3) of length 50 μm and radius 2 μm attached to soma.
+    dend = tree.append(soma, (0, 0, 0, 2), (50, 0, 0, 2), tag=3)
     # Attach two dendrites (tag=3) of length 50 μm to the end of the first dendrite.
-    # (b1) Radius tapers from 2 to 0.5 μm over the length of the dendrite.
-    tree.append(
-        b0,
-        arbor.mpoint(0, 0, 50, 2),
-        arbor.mpoint(0, 50 / sqrt(2), 50 + 50 / sqrt(2), 0.5),
+    # Radius tapers from 2 to 0.5 μm over the length of the dendrite.
+    l = 50 / sqrt(2)
+    _ = tree.append(
+        dend,
+        (50, 0, 0, 2),
+        (50 + l, l, 0, 0.5),
         tag=3,
     )
-    # (b2) Constant radius of 1 μm over the length of the dendrite.
-    tree.append(
-        b0,
-        arbor.mpoint(0, 0, 50, 1),
-        arbor.mpoint(0, -50 / sqrt(2), 50 + 50 / sqrt(2), 1),
+    # Constant radius of 1 μm over the length of the dendrite.
+    _ = tree.append(
+        dend,
+        (50, 0, 0, 1),
+        (50 + l, -l, 0, 1),
         tag=3,
     )
 
     # Associate labels to tags
-    labels = arbor.label_dict(
+    labels = A.label_dict(
         {
             "soma": "(tag 1)",
             "dend": "(tag 3)",
@@ -60,29 +55,27 @@ def make_cable_cell(gid):
 
     # (3) Create a decor and a cable_cell
     decor = (
-        arbor.decor()
+        A.decor()
         # Put hh dynamics on soma, and passive properties on the dendrites.
-        .paint('"soma"', arbor.density("hh")).paint('"dend"', arbor.density("pas"))
+        .paint('"soma"', A.density("hh")).paint('"dend"', A.density("pas"))
         # (4) Attach a single synapse.
-        .place('"synapse_site"', arbor.synapse("expsyn"), "syn")
+        .place('"synapse_site"', A.synapse("expsyn"), "syn")
         # Attach a detector with threshold of -10 mV.
-        .place('"root"', arbor.threshold_detector(-10), "detector")
+        .place('"root"', A.threshold_detector(-10 * U.mV), "detector")
     )
 
-    return arbor.cable_cell(tree, decor, labels)
+    return A.cable_cell(tree, decor, labels)
 
 
 # (5) Create a recipe that generates a network of connected cells.
-class random_ring_recipe(arbor.recipe):
+class random_ring_recipe(A.recipe):
     def __init__(self, ncells):
-        # The base C++ class constructor must be called first, to ensure that
-        # all memory in the C++ class is initialized correctly.
-        arbor.recipe.__init__(self)
+        # Base class constructor must be called first for proper initialization.
+        A.recipe.__init__(self)
         self.ncells = ncells
-        self.props = arbor.neuron_cable_properties()
+        self.props = A.neuron_cable_properties()
 
-    # (6) The num_cells method that returns the total number of cells in the model
-    # must be implemented.
+    # (6) Returns the total number of cells in the model; must be implemented.
     def num_cells(self):
         return self.ncells
 
@@ -90,19 +83,11 @@ class random_ring_recipe(arbor.recipe):
     def cell_description(self, gid):
         return make_cable_cell(gid)
 
-    # The kind method returns the type of cell with gid.
-    # Note: this must agree with the type returned by cell_description.
-    def cell_kind(self, gid):
-        return arbor.cell_kind.cable
+    # Return the type of cell; must be implemented and match cell_description.
+    def cell_kind(self, _):
+        return A.cell_kind.cable
 
-    def cell_isometry(self, gid):
-        # place cells with equal distance on a circle
-        radius = 500.0  # μm
-        angle = 2.0 * math.pi * gid / self.ncells
-        return arbor.isometry.translate(
-            radius * math.cos(angle), radius * math.sin(angle), 0
-        )
-
+    # (8) Descripe network
     def network_description(self):
         seed = 42
 
@@ -134,66 +119,64 @@ class random_ring_recipe(arbor.recipe):
         # fixed delay
         d = "(scalar 5.0)"  # ms delay
 
-        return arbor.network_description(s, w, d, {})
+        return A.network_description(s, w, d, {})
 
     # (9) Attach a generator to the first cell in the ring.
     def event_generators(self, gid):
         if gid == 0:
-            sched = arbor.explicit_schedule([1])  # one event at 1 ms
+            sched = A.explicit_schedule([1 * U.ms])  # one event at 1 ms
             weight = 0.1  # 0.1 μS on expsyn
-            return [arbor.event_generator("syn", weight, sched)]
+            return [A.event_generator("syn", weight, sched)]
         return []
 
     # (10) Place a probe at the root of each cell.
     def probes(self, gid):
-        return [arbor.cable_probe_membrane_voltage('"root"')]
+        return [A.cable_probe_membrane_voltage('"root"', "Um")]
 
-    def global_properties(self, kind):
+    def global_properties(self, _):
         return self.props
 
 
 # (11) Instantiate recipe
-ncells = 20
+ncells = 4
 recipe = random_ring_recipe(ncells)
 
-sim = arbor.simulation(recipe)
+# (12) Create a simulation using the default settings:
+# - Use all threads available
+# - Use round-robin distribution of cells across groups with one cell per group
+# - Use GPU if present
+# - No MPI
+# Other constructors of simulation can be used to change all of these.
+sim = A.simulation(recipe)
 
 # (13) Set spike generators to record
-sim.record(arbor.spike_recording.all)
+sim.record(A.spike_recording.all)
 
 # (14) Attach a sampler to the voltage probe on cell 0. Sample rate of 10 sample every ms.
-handles = [sim.sample((gid, 0), arbor.regular_schedule(0.1)) for gid in range(ncells)]
+handles = [
+    sim.sample((gid, "Um"), A.regular_schedule(0.1 * U.ms)) for gid in range(ncells)
+]
 
-# (15) Inspect generated connections
-connections = arbor.generate_network_connections(recipe)
-
-print("connections:")
-for c in connections:
-    print(
-        f'({c.source.gid}, "{c.source.label}") -> ({c.target.gid}, "{c.target.label}")'
-    )
-
-# (16) Run simulation for 100 ms
-sim.run(100)
+# (15) Run simulation for 100 ms
+sim.run(100 * U.ms)
 print("Simulation finished")
 
-# (17) Print spike times
+# (16) Print spike times
 print("spikes:")
 for sp in sim.spikes():
     print(" ", sp)
 
-# (18) Plot the recorded voltages over time.
+# (17) Plot the recorded voltages over time.
 print("Plotting results ...")
-df_list = []
+dfs = []
 for gid in range(ncells):
     samples, meta = sim.samples(handles[gid])[0]
-    df_list.append(
+    dfs.append(
         pandas.DataFrame(
             {"t/ms": samples[:, 0], "U/mV": samples[:, 1], "Cell": f"cell {gid}"}
         )
     )
-
-df = pandas.concat(df_list, ignore_index=True)
+df = pandas.concat(dfs, ignore_index=True)
 seaborn.relplot(
     data=df, kind="line", x="t/ms", y="U/mV", hue="Cell", errorbar=None
-).savefig("network_ring_result.svg")
+).savefig("network_description_result.svg")
