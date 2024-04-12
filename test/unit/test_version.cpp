d6e722c1d48a3ae509887dc76302890974d3f28a
Thorsten Hater <24411438+thorstenhater@users.noreply.github.com>
2024-04-12 13:40:15 +0200

    Debug print.

--- test/unit/test_version.cpp
+++ test/unit/test_version.cpp
@@ -49,6 +49,7 @@ TEST(version, libmatch) {
 }
 
 TEST(version, sane_config) {
+    std::cerr << arb::build_config << "\n";
     EXPECT_TRUE(arb::build_config=="DEBUG"s
              || arb::build_config=="RELEASE"s
              || arb::build_config=="RELWITHDEBINFO"s
