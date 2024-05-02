e1d1544036290e1870ca609c685b3f5e0169367a
Thorsten Hater <24411438+thorstenhater@users.noreply.github.com>
2024-05-02 14:39:15 +0200

    Black.

--- python/example/hh/step-04.py
+++ python/example/hh/step-04.py
@@ -11,23 +11,23 @@ tree.append(A.mnpos, (-3, 0, 0, 3), (3, 0, 0, 3), tag=1)
 # Create (almost empty) decor
 decor = (
     A.decor()
-    .paint('(all)', A.density('hh04'))
-    .place('(location 0 0.5)', A.iclamp(10 * U.ms, 2 * U.ms, 0.8 * U.nA), "iclamp")
+    .paint("(all)", A.density("hh04"))
+    .place("(location 0 0.5)", A.iclamp(10 * U.ms, 2 * U.ms, 0.8 * U.nA), "iclamp")
 )
 
 # Run the model, extracting the membrane voltage
 model = A.single_cell_model(A.cable_cell(tree, decor))
-model.probe("voltage", '(location 0 0.5)', tag="Um", frequency=10 * U.kHz)
+model.probe("voltage", "(location 0 0.5)", tag="Um", frequency=10 * U.kHz)
 
 # add our catalogue
-model.properties.catalogue = A.load_catalogue('cat-catalogue.so')
+model.properties.catalogue = A.load_catalogue("cat-catalogue.so")
 
 model.run(tfinal=30 * U.ms)
 
 # Create a basic plot
 fg, ax = plt.subplots()
 ax.plot(model.traces[0].time, model.traces[0].value)
-ax.set_xlabel('t/ms')
-ax.set_ylabel('U/mV')
-plt.savefig('hh-04.pdf')
-plt.savefig('hh-04.svg')
+ax.set_xlabel("t/ms")
+ax.set_ylabel("U/mV")
+plt.savefig("hh-04.pdf")
+plt.savefig("hh-04.svg")
