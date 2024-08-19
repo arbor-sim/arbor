fa120716baba813ce41334d059a6d17b2101cf55
Han Lu <11597940+ErbB4@users.noreply.github.com>
2024-08-19 17:04:48 +0200

    added brunel network and cleaned Python format

--- python/example/brunel/arbor_brunel.py
+++ python/example/brunel/arbor_brunel.py
@@ -1,17 +1,16 @@
-#the code is adapted based on a previous version of Dr. Sebastian Schmidt
+# the code is adapted based on a previous version of Dr. Sebastian Schmidt
 # adaptation was made to match brunel network simulation in NEST simulator
 
-#load packages needed for this project
+# load packages needed for this project
 import arbor as A
 from arbor import units as U
 import numpy as np
 from numpy.random import RandomState
-import parameters
 from parameters import *
 
 
 class brunel_recipe(A.recipe):
-    #define recipe for brunel network
+    # define recipe for brunel network
     def __init__(
         self,
         NE,
@@ -28,7 +27,7 @@ class brunel_recipe(A.recipe):
         E_L,
         V_reset,
         C_m,
-        t_ref
+        t_ref,
     ):
         A.recipe.__init__(self)
 
@@ -46,26 +45,27 @@ class brunel_recipe(A.recipe):
         # set up network parameters
         self.ncells_exc_ = NE
         self.ncells_inh_ = NI
-        self.in_degree_exc_ = round(epsilon * NE) #fixed indegree based on connection probability
-        self.in_degree_inh_ = round(epsilon * NI) #fixed indegree based on connection probability
+        self.in_degree_exc_ = round(
+            epsilon * NE
+        )  # fixed indegree based on connection probability
+        self.in_degree_inh_ = round(
+            epsilon * NI
+        )  # fixed indegree based on connection probability
         self.delay_ = delay * U.ms
 
-        #set and scale synaptic weights based on C_m due to the conductance based property of LIF neuron in Arbor
-        self.weight_exc_ = weight * C_m 
+        # set and scale synaptic weights based on C_m due to the conductance based property of LIF neuron in Arbor
+        self.weight_exc_ = weight * C_m
         self.weight_inh_ = -1 * g * weight * C_m
         self.weight_ext_ = weight * C_m
 
         # set the firing rate of the Poissonian input
-        self.lambda_ = rate * U.Hz 
-
+        self.lambda_ = rate * U.Hz
 
     def num_cells(self):
         return self.ncells_exc_ + self.ncells_inh_
 
-
     def cell_kind(self, gid):
         return A.cell_kind.lif
-
 
     def cell_description(self, gid):
         return A.lif_cell(
@@ -79,23 +79,23 @@ class brunel_recipe(A.recipe):
             V_m=self.V_m,
             t_ref=self.t_ref,
         )
-    
 
     def sample_subset(self, gen, gid, start, end, m):
-        #this function is used to generate random connection based on fixed indegree
+        # this function is used to generate random connection based on fixed indegree
         idx = np.arange(start, end)
         if start <= gid < end:
             idx = np.delete(idx, gid - start)
         gen.shuffle(idx)
         return idx[:m]
 
-
     def connections_on(self, gid):
         gen = RandomState(gid + self.seed_)
         # Add incoming excitatory connections.
         connections = [
             A.connection((i, "src"), "tgt", self.weight_exc_, self.delay_)
-            for i in self.sample_subset(gen, gid, 0, self.ncells_exc_, self.in_degree_exc_)
+            for i in self.sample_subset(
+                gen, gid, 0, self.ncells_exc_, self.in_degree_exc_
+            )
         ]
         # Add incoming inhibitory connections.
         connections += [
@@ -111,14 +111,16 @@ class brunel_recipe(A.recipe):
 
         return connections
 
-
     def event_generators(self, gid):
-        #add poissonian input to each neuron
+        # add poissonian input to each neuron
         return [
             A.event_generator(
                 "tgt",
                 self.weight_ext_,
-                A.poisson_schedule(freq=self.lambda_, seed=gid + (self.ncells_exc_ + self.ncells_inh_) * self.seed_),
+                A.poisson_schedule(
+                    freq=self.lambda_,
+                    seed=gid + (self.ncells_exc_ + self.ncells_inh_) * self.seed_,
+                ),
             )
         ]
 
@@ -140,11 +142,8 @@ if __name__ == "__main__":
         E_L,
         V_reset,
         C_m,
-        t_ref
+        t_ref,
     )
-
-
-
 
     context = A.context()
     if A.config()["profiling"]:
@@ -157,27 +156,20 @@ if __name__ == "__main__":
     hint.cpu_group_size = 5000
     hints = {A.cell_kind.lif: hint}
     decomp = A.partition_load_balance(recipe, context, hints)
-    print(decomp)    
+    print(decomp)
 
     sim = A.simulation(recipe, context, decomp)
     sim.record(A.spike_recording.all)
     sim.run(tfinal * U.ms, dt * U.ms)
 
-
     print("This is simulating Brunel network in Arbor!")
-    print(A.config()) 
+    print(A.config())
 
-
-    #get data
+    # get data
     spikes = sim.spikes()
     sources = spikes["source"]["gid"]
     times = spikes["time"]
 
-    #save data
-    np.save("times.npy",times)
-    np.save("sources.npy",sources)
-
-
-
-
-
+    # save data
+    np.save("times.npy", times)
+    np.save("sources.npy", sources)
--- python/example/brunel/parameters.py
+++ python/example/brunel/parameters.py
@@ -11,49 +11,42 @@ epsilon = 0.1
 
 J = 0.1
 weight = J
-delay = 1.5 #ms
-theta = 20.
+delay = 1.5  # ms
+theta = 20.0
 
 CE = round(epsilon * NE)
 CI = round(epsilon * NI)
 
 #### define neuron parameter ####
-tau_m = 20 #ms
-V_th = theta #mV
-C_m = 250. #pF
-E_L = 0.0 #mV
-V_reset = 10.0 #mV
-V_m = 0.0 #mV
-t_ref = 2.0 #ms
+tau_m = 20  # ms
+V_th = theta  # mV
+C_m = 250.0  # pF
+E_L = 0.0  # mV
+V_reset = 10.0  # mV
+V_m = 0.0  # mV
+t_ref = 2.0  # ms
 
 
 ##### for nest #####
-neuron_model    = "iaf_psc_delta"
+neuron_model = "iaf_psc_delta"
 
-neuron_params   = {
-                    "C_m"       : C_m,
-                    "tau_m"     : tau_m,
-                    "t_ref"     : t_ref,
-                    "E_L"       : E_L,
-                    "V_reset"   : V_reset,
-                    "V_m"       : V_m,
-                    "V_th"      : theta
-                   }
-
+neuron_params = {
+    "C_m": C_m,
+    "tau_m": tau_m,
+    "t_ref": t_ref,
+    "E_L": E_L,
+    "V_reset": V_reset,
+    "V_m": V_m,
+    "V_th": theta,
+}
 
 
 #### define external input ####
-nu_th  = theta/(J*CE*tau_m)
-nu_ex  = eta*nu_th
-rate = 1000.0*nu_ex*CE #Hz
+nu_th = theta / (J * CE * tau_m)
+nu_ex = eta * nu_th
+rate = 1000.0 * nu_ex * CE  # Hz
 
 #### simulation parameters ####
-tfinal = 1000 #ms
-dt = 1 #ms
+tfinal = 1000  # ms
+dt = 1  # ms
 seed = 42
-
-
-
-
-
-
