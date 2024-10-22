5eab34508f36487a6a68572888dfddcc0c21d9b6
boeschf <48126478+boeschf@users.noreply.github.com>
2024-10-22 10:21:02 +0200

    event handling: fix a bug and clean house (#2421)
    - fix event stream when more than one cell in a cell group have same
    synapse
      - events would previously no longer necessarily be sorted by time
    - in order to simplify: also sort with respect to mechanism index (as
    was previously only required for the gpu backend)
      - add pertinent test
    - while cleaning up: overhauled the event related files and data
    structures
      - removed dead code
    - made event handling less generic (this feature was not used anywhere)

--- arbor/backends/event_stream_base.hpp
+++ arbor/backends/event_stream_base.hpp
@@ -2,9 +2,7 @@
 
 #include <vector>
 
-#include <arbor/generic_event.hpp>
 #include <arbor/mechanism_abi.h>
-
 
 #include "backends/event.hpp"
 #include "backends/event_stream_state.hpp"
@@ -18,15 +16,13 @@ namespace arb {
 
 template <typename Event>
 struct event_stream_base {
-    using size_type = std::size_t;
     using event_type = Event;
-    using event_time_type = ::arb::event_time_type<Event>;
-    using event_data_type = ::arb::event_data_type<Event>;
+    using event_data_type = decltype(event_data(std::declval<Event>()));
 
 protected: // members
     std::vector<event_data_type> ev_data_;
     std::vector<std::size_t> ev_spans_ = {0};
-    size_type index_ = 0;
+    std::size_t index_ = 0;
     event_data_type* base_ptr_ = nullptr;
 
 public:
@@ -62,24 +58,32 @@ public:
         index_ = 0;
     }
 
-    // Construct a mapping of mech_id to a stream s.t. streams are partitioned into
-    // time step buckets by `ev_span`
+  protected:
+    // backend specific initializations
+    virtual void init() = 0;
+};
+
+struct spike_event_stream_base : event_stream_base<deliverable_event> {
     template<typename EventStream>
-    static std::enable_if_t<std::is_base_of_v<event_stream_base, EventStream>>
-    multi_event_stream(const event_lane_subrange& lanes,
-                       const std::vector<target_handle>& handles,
-                       const std::vector<std::size_t>& divs,
-                       const timestep_range& steps,
-                       std::unordered_map<unsigned, EventStream>& streams) {
+    friend void initialize(const event_lane_subrange& lanes,
+                           const std::vector<target_handle>& handles,
+                           const std::vector<std::size_t>& divs,
+                           const timestep_range& steps,
+                           std::unordered_map<unsigned, EventStream>& streams) {
         arb_assert(lanes.size() < divs.size());
 
+        // reset streams and allocate sufficient space for temporaries
         auto n_steps = steps.size();
-        std::unordered_map<unsigned, std::vector<std::size_t>> dt_sizes;
         for (auto& [k, v]: streams) {
             v.clear();
-            dt_sizes[k].resize(n_steps, 0);
+            v.spike_counter_.clear();
+            v.spike_counter_.resize(steps.size(), 0);
+            v.spikes_.clear();
+            // ev_data_ has been cleared during v.clear(), so we use its capacity
+            v.spikes_.reserve(v.ev_data_.capacity());
         }
 
+        // loop over lanes: group events by mechanism and sort them by time
         auto cell = 0;
         for (const auto& lane: lanes) {
             auto div = divs[cell];
@@ -94,15 +98,70 @@ public:
                 if (step >= n_steps) break;
                 arb_assert(div + target < handles.size());
                 const auto& handle = handles[div + target];
-                streams[handle.mech_id].ev_data_.push_back({handle.mech_index, weight});
-                dt_sizes[handle.mech_id][step]++;
+                auto& stream = streams[handle.mech_id];
+                stream.spikes_.push_back(spike_data{step, handle.mech_index, time, weight});
+                // insertion sort with last element as pivot
+                // ordering: first w.r.t. step, within a step: mech_index, within a mech_index: time
+                auto first = stream.spikes_.begin();
+                auto last = stream.spikes_.end();
+                auto pivot = std::prev(last, 1);
+                std::rotate(std::upper_bound(first, pivot, *pivot), pivot, last);
+                // increment count in current time interval
+                stream.spike_counter_[step]++;
             }
         }
 
         for (auto& [id, stream]: streams) {
-            util::make_partition(stream.ev_spans_, dt_sizes[id]);
-            stream.init();
+            // copy temporary deliverable_events into stream's ev_data_
+            stream.ev_data_.reserve(stream.spikes_.size());
+            std::transform(stream.spikes_.begin(), stream.spikes_.end(), std::back_inserter(stream.ev_data_),
+                [](auto const& e) noexcept -> arb_deliverable_event_data {
+                return {e.mech_index, e.weight}; });
+            // scan over spike_counter_ and  written to ev_spans_
+            util::make_partition(stream.ev_spans_, stream.spike_counter_);
+            // delegate to derived class init: static cast necessary to access protected init()
+            static_cast<spike_event_stream_base&>(stream).init();
         }
+    }
+
+  protected: // members
+    struct spike_data {
+        arb_size_type step = 0;
+        cell_local_size_type mech_index = 0;
+        time_type time = 0;
+        float weight = 0;
+        auto operator<=>(spike_data const&) const noexcept = default;
+    };
+    std::vector<spike_data> spikes_;
+    std::vector<std::size_t> spike_counter_;
+};
+
+struct sample_event_stream_base : event_stream_base<sample_event> {
+    friend void initialize(const std::vector<std::vector<sample_event>>& staged,
+                           sample_event_stream_base& stream) {
+        // clear previous data
+        stream.clear();
+
+        // return if there are no timestep bins
+        if (!staged.size()) return;
+
+        // return if there are no events
+        auto num_events = util::sum_by(staged, [] (const auto& v) {return v.size();});
+        if (!num_events) return;
+
+        // allocate space for spans and data
+        stream.ev_spans_.reserve(staged.size() + 1);
+        stream.ev_data_.reserve(num_events);
+
+        // add event data and spans
+        for (const auto& v : staged) {
+            for (const auto& ev: v) stream.ev_data_.push_back(ev.raw);
+            stream.ev_spans_.push_back(stream.ev_data_.size());
+        }
+
+        arb_assert(num_events == stream.ev_data_.size());
+        arb_assert(staged.size() + 1 == stream.ev_spans_.size());
+        stream.init();
     }
 };
 
