36f2766d23ce70026bab38fa8149376b1cff2a85
Thorsten Hater <24411438+thorstenhater@users.noreply.github.com>
2025-01-27 20:22:37 +0100

    📊 Sort spikes. (#2433)
    Replace custom `introsort` with `std::sort`.

--- scripts/run_cpp_examples.sh
+++ scripts/run_cpp_examples.sh
@@ -31,9 +31,8 @@ all_examples=(
     "brunel -G 100000"
     "brunel --n-excitatory 10000 --n-inhibitory 2500 --tfinal 10 --in-degree-prop 0.1 --dt 0.01 --lambda 1.5 -g 0.8 --delay 1.0 --weight 0.00010 -G 1 --use-cable-cells"
     "brunel --n-excitatory 10000 --n-inhibitory 2500 --tfinal 10 --in-degree-prop 0.1 --dt 0.01 --lambda 1.5 -g 0.8 --delay 1.0 --weight 0.00010 -G 10 --use-cable-cells"
-    # TODO These need to be enabled when the group size issue is fixed.
-    # "brunel --n-excitatory 10000 --n-inhibitory 2500 --tfinal 10 --in-degree-prop 0.1 --dt 0.01 --lambda 1.5 -g 0.8 --delay 1.0 --weight 0.00010 -G 100 --use-cable-cells"
-    # "brunel --n-excitatory 10000 --n-inhibitory 2500 --tfinal 10 --in-degree-prop 0.1 --dt 0.01 --lambda 1.5 -g 0.8 --delay 1.0 --weight 0.00010 -G 100000 --use-cable-cells"
+    "brunel --n-excitatory 10000 --n-inhibitory 2500 --tfinal 10 --in-degree-prop 0.1 --dt 0.01 --lambda 1.5 -g 0.8 --delay 1.0 --weight 0.00010 -G 100 --use-cable-cells"
+    "brunel --n-excitatory 10000 --n-inhibitory 2500 --tfinal 10 --in-degree-prop 0.1 --dt 0.01 --lambda 1.5 -g 0.8 --delay 1.0 --weight 0.00010 -G 100000 --use-cable-cells"
     "gap_junctions"
     "generators"
     "lfp"
@@ -62,8 +61,8 @@ expected_outputs=(
     6998
     38956
     38956
-    # 38956
-    # 38956
+    38956
+    38956
     "30"
     ""
     ""
