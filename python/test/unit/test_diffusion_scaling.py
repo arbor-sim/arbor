dda7a857d9158551dfc1d5c36db3cb26d4edc0db
Jannik Luboeinski <mail@jlubo.net>
2025-02-26 23:52:36 +0100

    additional test for the scaling of the diffusive particle concentration; code cleanup

--- python/test/unit/test_diffusion_scaling.py
+++ python/test/unit/test_diffusion_scaling.py
@@ -9,8 +9,9 @@ from scipy.sparse.linalg import spsolve
 from .. import fixtures
 
 """
-Tests for the correct scaling of the amount of diffusive particles in Arbor, as compared to an
-independent SciPy implementation that solves the diffusion equation via the Crank-Nicolson method.
+Tests for the correct scaling of the amount _and_ concentration of diffusive particles in Arbor, as
+compared to an independent SciPy implementation that solves the diffusion equation via the Crank-Nicolson
+method.
 """
 
 class recipe(A.recipe):
@@ -43,7 +44,7 @@ class recipe(A.recipe):
         return self.the_props
 
     def event_generators(self, gid):
-        return [A.event_generator("syn" + str(i), 1., A.explicit_schedule(np.array([0])*U.ms)) for i in self.stim]
+        return [A.event_generator(f"syn_{i}", 1., A.explicit_schedule(np.array([0])*U.ms)) for i in self.stim]
 
 
 class TestDiffusionScaling(unittest.TestCase):
@@ -56,6 +57,9 @@ class TestDiffusionScaling(unittest.TestCase):
         self.L = 10.0   # length of the domain, in µm
         self.Nx = 100   # number of spatial points
         self.dx = self.L / self.Nx  # spatial step size, in µm
+        self.dendrite_radius = 1 # in µm
+        #self.cv_area = 2 * np.pi * self.dendrite_radius * self.dx # in µm^2
+        self.cv_volume = np.pi * self.dendrite_radius**2 * self.dx # in µm^3
 
         # temporal parameters
         self.T = 50.1  # runtime of the whole simulation in ms
@@ -67,34 +71,32 @@ class TestDiffusionScaling(unittest.TestCase):
 
         # other parameters
         self.stim = range(int(0.4 * self.Nx), int(0.6 * self.Nx))  # stimulated points
-        self.max_amount = 1.0  # maximum (initial) particle amount
-        self.dev_dyn = 0.20  # accepted relative deviation from maximum particle amount (in dynamic regime)
-        self.dev_ss = 0.05  # accepted relative deviation from maximum particle amount (in steady state)
+        self.inject_max_amount = 1.0  # maximum (initially injected) particle amount in µmol/l / 1e-21 mol
+        self.dev_dyn = 0.20  # accepted relative deviation from maximum particle amount / concentration (in dynamic regime)
+        self.dev_ss = 0.05  # accepted relative deviation from maximum particle amount/concentration (in steady state)
 
     # simulate_arbor
     # Method that sets up and simulates the diffusion of particles in Arbor.
-    # - return: the distribution of the particle amount over space and time
-    def simulate_arbor(self):
+    # - mech_name: the name of the injection point mechanism to be used
+    # - return: the distribution of the particle amount/concentration over space and time
+    def simulate_arbor(self, mech_name):
         # set up the morphology
         tree = A.segment_tree()
-        dendrite_radius = 1 # in µm
-        #cv_area = 2 * np.pi * dendrite_radius * self.dx # in µm^2
-        #cv_volume = np.pi * dendrite_radius**2 * self.dx # in µm^3
         #x = self.dx*np.arange(self.Nx+1)
         labels = A.label_dict({})
         for i in range(self.Nx):
             # the first segment: the root
             if i == 0:
-                locals()["dendrite0"] = tree.append(A.mnpos,
-                            A.mpoint(-self.L/2, 0, 0, dendrite_radius),
-                            A.mpoint(-self.L/2+self.dx, 0, 0, dendrite_radius), tag=0)
-                labels["dendrite0"] = "(tag 0)"
+                locals()["dendrite_0"] = tree.append(A.mnpos,
+                            A.mpoint(-self.L/2, 0, 0, self.dendrite_radius),
+                            A.mpoint(-self.L/2+self.dx, 0, 0, self.dendrite_radius), tag=0)
+                labels["dendrite_0"] = "(tag 0)"
             # all following segments
             else:
-                locals()[f"dendrite{i}"] = tree.append(locals()[f"dendrite{i-1}"],
-                            A.mpoint(-self.L/2+i*self.dx, 0, 0, dendrite_radius),
-                            A.mpoint(-self.L/2+(i+1)*self.dx, 0, 0, dendrite_radius), tag=i)
-                labels[f"dendrite{i}"] = f"(tag {i})"
+                locals()[f"dendrite_{i}"] = tree.append(locals()[f"dendrite_{i-1}"],
+                            A.mpoint(-self.L/2+i*self.dx, 0, 0, self.dendrite_radius),
+                            A.mpoint(-self.L/2+(i+1)*self.dx, 0, 0, self.dendrite_radius), tag=i)
+                labels[f"dendrite_{i}"] = f"(tag {i})"
         morph = A.morphology(tree)
 
         # set up decor
@@ -102,10 +104,11 @@ class TestDiffusionScaling(unittest.TestCase):
         decor.set_ion("my_ion", int_con=0.0*U.mM, diff=self.D*U.m2/U.s)
 
         # set up particle/ion injection
+        # NOTE 'inject_norm_concentration' mechanism will automatically normalize by the volume
         for i in self.stim:
-            mech_inject = A.mechanism("inject_norm_amount/x=my_ion", {"alpha": self.max_amount})
-            decor.place(f'(on-components 0.5 (region "dendrite{i}"))',
-                        A.synapse(mech_inject), "syn" + str(i))
+            mech_inject = A.mechanism(f"{mech_name}/x=my_ion", {"alpha": self.inject_max_amount})
+            decor.place(f'(on-components 0.5 (region "dendrite_{i}"))',
+                        A.synapse(mech_inject), f"syn_{i}")
 
         # set up probes
         probes = [A.cable_probe_ion_diff_concentration_cell("my_ion", "tag_my_ion")]
@@ -127,13 +130,17 @@ class TestDiffusionScaling(unittest.TestCase):
     # simulate_independent
     # Method that sets up and simulates the diffusion of particles with an independent SciPy implementation that
     # solves the diffusion equation via the Crank-Nicolson method.
-    # - return: the distribution of the particle amount over space and time
-    def simulate_independent(self):
+    # - concentration: flag that specifies if particle concentrations are considered (instead of amounts)
+    # - return: the distribution of the particle amount/concentration over space and time
+    def simulate_independent(self, concentration):
         # set Crank-Nicolson coefficient, initial conditions, stimulation
         alpha = self.D * (self.dt*1e-3) / (2 * (self.dx*1e-6)**2)
         #x = np.linspace(0, self.L*1e-6, self.Nx)
         u = np.zeros(self.Nx)
-        u[self.stim] = self.max_amount
+        if concentration:
+            u[self.stim] = self.inject_max_amount / self.cv_volume
+        else:
+            u[self.stim] = self.inject_max_amount
         data = np.zeros((self.Nt, self.Nx))
 
         # start to construct the matrices for the Crank-Nicolson method
@@ -172,26 +179,57 @@ class TestDiffusionScaling(unittest.TestCase):
         self, single_context
     ):
         # perform the simulations
-        data_arbor = self.simulate_arbor()
-        data_ind = self.simulate_independent()
+        data_arbor = self.simulate_arbor("inject_norm_amount")
+        data_ind = self.simulate_independent(concentration = False)
 
         # test initial state
         self.assertTrue(np.allclose(
                 data_arbor[0],
                 data_ind[0],
-                atol=self.dev_ss*self.max_amount
+                atol=self.dev_ss*self.inject_max_amount
             ), f"{data_arbor[0]} != {data_ind[0]}")
         
         # test final (equilibrium) state
         self.assertTrue(np.allclose(
                 data_arbor[-1],
                 data_ind[-1],
-                atol=self.dev_ss*self.max_amount
+                atol=self.dev_ss*self.inject_max_amount
             ), f"{data_arbor[-1]} != {data_ind[-1]}")
         
         # test whole dynamic time course
         self.assertTrue(np.allclose(
                 data_arbor,
                 data_ind,
-                atol=self.dev_dyn*self.max_amount
+                atol=self.dev_dyn*self.inject_max_amount
+            ), f"{data_arbor} != {data_ind}")
+        
+    # test_diffusion_scaling_concentration
+    # Test: compare the concentration of diffusive particles in Arbor and independent implementation
+    @fixtures.single_context()
+    def test_diffusion_scaling_concentration(
+        self, single_context
+    ):
+        # perform the simulations
+        data_arbor = self.simulate_arbor("inject_norm_concentration")
+        data_ind = self.simulate_independent(concentration = True)
+
+        # test initial state
+        self.assertTrue(np.allclose(
+                data_arbor[0],
+                data_ind[0],
+                atol=self.dev_ss*self.inject_max_amount/self.cv_volume
+            ), f"{data_arbor[0]} != {data_ind[0]}")
+        
+        # test final (equilibrium) state
+        self.assertTrue(np.allclose(
+                data_arbor[-1],
+                data_ind[-1],
+                atol=self.dev_ss*self.inject_max_amount/self.cv_volume
+            ), f"{data_arbor[-1]} != {data_ind[-1]}")
+        
+        # test whole dynamic time course
+        self.assertTrue(np.allclose(
+                data_arbor,
+                data_ind,
+                atol=self.dev_dyn*self.inject_max_amount/self.cv_volume
             ), f"{data_arbor} != {data_ind}")
