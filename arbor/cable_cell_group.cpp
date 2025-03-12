e9eec7013b8b90e7099cd5bad7a4e7232cd709ba
Thorsten Hater <24411438+thorstenhater@users.noreply.github.com>
2025-03-12 09:45:45 +0100

    Apple Clang, please.

--- arbor/cable_cell_group.cpp
+++ arbor/cable_cell_group.cpp
@@ -482,7 +482,7 @@ std::vector<probe_metadata> cable_cell_group::get_probe_metadata(const cell_addr
     result.reserve(data.size());
     unsigned index = 0;
     for (const auto& info: data) {
-        result.emplace_back(probeset_id, index, info.get_metadata_ptr());
+        result.push_back({probeset_id, index, info.get_metadata_ptr()});
         index++;
     }
     return result;
