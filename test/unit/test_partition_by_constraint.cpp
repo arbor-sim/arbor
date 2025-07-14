0a8d72067433b7ed703ae79fcbef7645e4625575
Thorsten Hater <24411438+thorstenhater@users.noreply.github.com>
2025-07-14 11:08:34 +0200

    make random test generic

--- test/unit/test_partition_by_constraint.cpp
+++ test/unit/test_partition_by_constraint.cpp
@@ -8,10 +8,7 @@
 
 using namespace arb;
 using iarray = multicore::iarray;
-constexpr unsigned vector_length = (unsigned) simd::simd_abi::native_width<arb_value_type>::value;
-using simd_value_type = simd::simd<arb_value_type, vector_length, simd::simd_abi::default_abi>;
-const int simd_width_ = simd::width<simd_value_type>();
-
+const int simd_width_ = 2;
 const int input_size_ = 1024;
 
 TEST(partition_by_constraint, partition_contiguous) {
@@ -100,38 +97,38 @@ TEST(partition_by_constraint, partition_none) {
 }
 
 TEST(partition_by_constraint, partition_random) {
-    iarray input_index(input_size_);
-    iarray expected_constant, expected_independent, expected_none, expected_simd_1;
+    auto check = [] (std::size_t simd_width, std::size_t input_size) {
+        // process input in quarters
+        // 1. no constraint
+        // 2. independent
+        // 3. constant
+        // 4. contiguous
+        iarray input_index(input_size);
+        for (unsigned i = 0;              i <   input_size/4; i++) input_index[i] = i/((simd_width + 1)/ 2);
+        for (unsigned i =   input_size/4; i <   input_size/2; i++) input_index[i] = i*2;
+        for (unsigned i =   input_size/2; i < 3*input_size/4; i++) input_index[i] = 5;
+        for (unsigned i = 3*input_size/4; i <   input_size;   i++) input_index[i] = i;
 
-    const int c = 5;
-    for (unsigned i = 0; i < input_size_; i++) {
-        input_index[i] = i<input_size_/4   ? i/((simd_width_ + 1)/ 2):
-                         i<input_size_/2   ? i*2:
-                         i<input_size_*3/4 ? c:
-                         i;
-        if (i < input_size_ / 4 && i % simd_width_ == 0) {
-            if (simd_width_ > 2) expected_none.push_back(i);
+        iarray expected_constant, expected_independent, expected_none, expected_contiguous;
+        for (unsigned i = 0;              i <   input_size/4; i += simd_width) expected_none.push_back(i);
+        for (unsigned i =   input_size/2; i < 3*input_size/4; i += simd_width) expected_constant.push_back(i);
+        for (unsigned i =   input_size/4; i <   input_size/2; i += simd_width) expected_independent.push_back(i);
+        expected_contiguous = {3*static_cast<int>(input_size)/4, static_cast<int>(input_size)};
+        // NOTE iff width == 2, the first 1/4 becomes either constant,
+        //      contiguous or independent instead of none.
+        //      Out choice here is contiguous.
+        if (simd_width == 2) {
+            expected_contiguous = {0, static_cast<int>(input_size)/4, 3*static_cast<int>(input_size)/4, static_cast<int>(input_size)};
+            expected_none       = {};
         }
-        else if (i < input_size_ / 2 && i % simd_width_ == 0) {
-            expected_independent.push_back(i);
-        }
-        else if (i < input_size_* 3/ 4 && i % simd_width_ == 0) {
-            expected_constant.push_back(i);
-        }
-        expected_simd_1.push_back(i);
-    }
 
-    auto output = multicore::make_constraint_partition(input_index, input_size_, simd_width_);
-
-    iarray expected_contiguous{0, input_size_/4, 3*input_size_/4, input_size_};
-    if (simd_width_ != 1) {
+        auto output = multicore::make_constraint_partition(input_index, input_size, simd_width);
         EXPECT_EQ(expected_contiguous, output.contiguous);
         EXPECT_EQ(expected_constant, output.constant);
         EXPECT_EQ(expected_independent, output.independent);
         EXPECT_EQ(expected_none, output.none);
-    }
-    else {
-        EXPECT_EQ(expected_simd_1, output.contiguous);
-    }
-
+    };
+    check(2, 128);
+    check(4, 2048);
+    check(8, 1024);
 }
